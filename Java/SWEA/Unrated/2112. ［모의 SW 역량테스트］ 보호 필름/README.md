c0ee55fe50f7ac29a02b51b6880bec5a092c5ba0
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-08-17 14:54:53 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 2,157 ms, Memory: 24,072 KB -BaekjoonHub

---
+++ Java/SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 보호 필름 - 2112 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5V1SYKAaUDFAWu) 
+
+### 성능 요약
+
+메모리: 24,072 KB, 시간: 2,157 ms, 코드길이: 2,273 Bytes
+
+### 제출 일자
+
+2024-08-17 14:54
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ Java/SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -0,0 +1,109 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static boolean[] changeLog; // D줄에 대한 교체 여부
+	static int D;
+	static int W;
+	static int K;
+	static boolean[][] map;
+	static boolean[] A;
+	static boolean[] B;
+	static boolean flag;
+	static int result;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int testCase = Integer.parseInt(br.readLine());
+		for(int t = 1 ; t <= testCase ; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			D = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			map = new boolean[D][W];
+			A = new boolean[W];
+			B = new boolean[W];
+			changeLog = new boolean[D];
+			for(int i = 0 ; i < D ; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0 ; j < W ; j++) {
+					B[j] = true;
+					if(st.nextToken().equals("1")) {
+						map[i][j] = true;
+					}
+					else {
+						map[i][j] = false;
+					}
+				}
+			}//map 설정 끝
+			flag = false;
+			result = Integer.MAX_VALUE;
+			subSet(0);		
+			sb.append('#').append(t).append(' ').append(result).append('\n');
+		}
+		System.out.println(sb);
+	}
+	private static void subSet(int row) {
+		if(row == D) {
+			if(isPosible()) {
+				int temp = 0;
+				for(boolean i : changeLog) if(i) temp++;
+				if(temp<result) result = temp;
+			}
+			return;
+		}
+		
+		subSet(row+1);
+		if(flag) return;
+		
+		boolean[] temp = map[row];
+		changeLog[row]=true;
+		
+		//A로 바꿈
+		map[row] = A;
+		subSet(row+1);
+		if(flag) return;
+		
+		//B로 바꿈
+		map[row] = B;
+		subSet(row+1);
+		if(flag) return;
+		
+		//복구
+		map[row] = temp;
+		changeLog[row] = false;
+		
+	}
+	
+	private static boolean isPosible() {
+		
+		for(int j = 0 ; j < W ; j++) {
+			boolean flag = false;
+			int trueCnt = 0, falseCnt = 0;
+			for(int i = 0 ; i < D ; i++) {
+				if(map[i][j]) {
+					trueCnt++;
+					falseCnt = 0;
+				}
+				else {
+					trueCnt=0;
+					falseCnt++;
+				}
+				if(trueCnt>=K||falseCnt>=K) {
+					flag = true;
+					break;
+				}
+			}
+			if(flag) {
+				continue;
+			}
+			return false;
+		}
+		return true;
+	}
+
+}
