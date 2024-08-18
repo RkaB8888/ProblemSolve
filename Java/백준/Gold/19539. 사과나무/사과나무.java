124033b2a66fea79889ce4ab40dd89054048fd49
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-08-19 02:09:49 +0900

    [Gold V] Title: 사과나무, Time: 88 ms, Memory: 12036 KB -BaekjoonHub

--- Java/백준/Gold/19539. 사과나무/사과나무.java
+++ Java/백준/Gold/19539. 사과나무/사과나무.java
@@ -1,29 +1,32 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		int N = readInt();
 		int[] cnt = {0,0};
-		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			int temp = Integer.parseInt(st.nextToken());
-			cnt[0]+=temp%2;
-			cnt[1]+=temp/2;
+			int temp = readInt();
+			cnt[0]+=temp&1;
+			cnt[1]+=temp>>1;
 		}
 		if(cnt[1]<cnt[0]) System.out.println("NO");
 		else if(cnt[1]==cnt[0]) System.out.println("YES");
 		else {
-			int diff = cnt[1]-cnt[0];
-			if(diff%3!=0) System.out.println("NO");
+			if((cnt[1]-cnt[0])%3!=0) System.out.println("NO");
 			else System.out.println("YES");
 		}
 	}
+	public static int readInt() throws Exception {
+	    int val = 0;
+	    int c = System.in.read();
+	    while (c <= ' ') {  // 공백 문자 건너뛰기
+	        c = System.in.read();
+	    }
+	    do {
+	        val = 10 * val + c - 48;  // 아스키 값으로 숫자 변환
+	    } while ((c = System.in.read()) >= 48 && c <= 57);
+
+	    return val;  // 양수 반환
+	}
+
 
 }
