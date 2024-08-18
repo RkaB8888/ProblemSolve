f63f38a5120c11de5b130ed364d7ced5bb7e8a48
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-08-19 01:40:56 +0900

    [Gold V] Title: 사과나무, Time: 444 ms, Memory: 23532 KB -BaekjoonHub

---
+++ Java/백준/Gold/19539. 사과나무/사과나무.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		int[] idealH = new int[N];
+		int[] cnt = {0,0};
+		boolean flag = false;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			idealH[i] = Integer.parseInt(st.nextToken());
+			cnt[0]+=idealH[i]%2;
+			cnt[1]+=idealH[i]/2;
+		}
+		if(cnt[1]<cnt[0]) flag = false;
+		else if(cnt[1]==cnt[0]) flag= true;
+		else {
+			while(cnt[1]>cnt[0]) {
+				cnt[1]--;
+				cnt[0]+=2;
+			}
+			if(cnt[0]==cnt[1]) flag = true;
+			else flag = false;
+		}
+		if (flag)
+			System.out.println("YES");
+		else
+			System.out.println("NO");
+	}
+
+}
