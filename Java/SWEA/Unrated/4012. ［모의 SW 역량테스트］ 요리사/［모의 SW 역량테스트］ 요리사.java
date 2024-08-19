5fd8f81c5675253619fefc7327761843c1764050
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-08-19 23:27:38 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 149 ms, Memory: 22,980 KB -BaekjoonHub

--- Java/SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ Java/SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,12 +1,9 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 /**
- * 메모리:29,712KB, 시간:1,733ms
- * 재귀를 통한 조합 생성을 2번 함
- * 모든 재료에 대해 A와 B가 나눠가지는 조합 1번
- * 나눠가진 재료에 대해서 궁합을 계산하기 위해 2개씩 고르는 조합 2번
- * DP가 없을 때 시간 초과가 걸렸고, DP를 넣고 돌리니 통과함
+ * 메모리:25,356KB, 시간:159ms
+ * 재귀를 통한 조합 생성을 1번 함
+ * -> 모든 재료에 대해 A와 B가 나눠가지는 조합
+ * 나눠가진 재료에 대해서 궁합을 계산하기 위해 2중 for문 사용
+ * DP를 통해 A와 B가 서로 반대로 고른 경우를 제외함
  */
 public class Solution {
 	static int N;// 전체 식재료 개수
@@ -18,21 +15,18 @@ public class Solution {
 	static int[] dp;
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
+		int T = readInt();
 		for (int test_case = 1; test_case <= T; test_case++) {
-			N = Integer.parseInt(br.readLine());
+			N = readInt();
 			H = N / 2;
 			table = new int[N][N];
 			isSelect = new boolean[N];
 			result = Integer.MAX_VALUE;
 			dp = new int[1 << N];
 			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
-					table[i][j] = Integer.parseInt(st.nextToken());
+					table[i][j] = readInt();
 				}
 			}
 			///////////////////// 입력 처리 끝//////////////////////////////////////
@@ -44,7 +38,7 @@ public class Solution {
 
 	// N개의 재료들 중에서 N/2개의 재료에 체크 표시(A가 사용하는 재료)
 	public static void combi1(int cnt, int str, int bitmask) {
-		if (cnt == H) {// N/2개 선택했다면 나눠진 재료를 토대로 점수 계산
+		if (cnt == H) {// N/2개 선택했다면 나눠진 재료를 토대로 점수 차이 계산
 			if (dp[bitmask] == 0) {
 				int diff = calcDiff();
 				if (result > diff)
@@ -63,7 +57,6 @@ public class Solution {
 	}
 
 	// 사용하기로 한 재료들 중에서 2개를 선택해서 궁합 점수를 산출한다.
-	// list는 선택된 2가지 재료를 담는 배열, aCheck가 true라면 A재료를 계산하는 중
 	public static int calcDiff() {
 		int a = 0; 
 		int b = 0;
@@ -80,6 +73,18 @@ public class Solution {
 			}
 		}
 		return Math.abs(a-b);
+	}
+	public static int readInt() throws Exception {
+	    int val = 0;
+	    int c = System.in.read();
+	    while (c <= ' ') {  // 공백 문자 건너뛰기
+	        c = System.in.read();
+	    }
+	    do {
+	        val = 10 * val + c - 48;  // 아스키 값으로 숫자 변환
+	    } while ((c = System.in.read()) >= 48 && c <= 57);
+
+	    return val;  // 양수 반환
 	}
 
 }
