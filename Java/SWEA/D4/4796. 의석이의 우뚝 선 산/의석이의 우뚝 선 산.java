2fe9f32a2a4de0ebbd892e6c7faabb8888ac89e7
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-08-20 23:05:30 +0900

    [D4] Title: 의석이의 우뚝 선 산, Time: 245 ms, Memory: 20,192 KB -BaekjoonHub

--- Java/SWEA/D4/4796. 의석이의 우뚝 선 산/의석이의 우뚝 선 산.java
+++ Java/SWEA/D4/4796. 의석이의 우뚝 선 산/의석이의 우뚝 선 산.java
@@ -1,6 +1,3 @@
-import java.io.IOException;
-import java.util.Scanner;
-
 /**
  * 메모리:108,832KB, 시간:879ms
  * @author SSAFY
@@ -9,40 +6,30 @@ import java.util.Scanner;
 
 public class Solution {
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
 		
 		StringBuilder sb = new StringBuilder();
-		int T = sc.nextInt();
+		int T = readInt();
 		for(int tc=1; tc<=T; tc++) {
-			int N = sc.nextInt();//산의 갯수
-			int preH = sc.nextInt();//이전 산의 높이
+			int N = readInt();//산의 갯수
+			int preH = readInt();//이전 산의 높이
 			int curH = 0;//현재 산의 높이
 			boolean riseFlag = false;//올라가는 중인지
 			int result = 0;//출력할 결과 저장
 			int riseCnt = 0;//얼마나 올라갔는지
 			for(int i = 1 ; i < N ; i++) {
-				curH = sc.nextInt();
+				curH = readInt();
 				if(preH<curH) {//값이 상승했다.
-					if(riseFlag) {//꾸준히 상승하는 구간이다.
-						riseCnt++;
-					}
-					else {//하락 구간을 벗어났다.
-//						riseCnt+=2; 이전의 하락 마지막 포인트까지 고려했으나
-//						꼭대기를 포함 안시키는 점을 고려해서 1만 추가
+					if(!riseFlag) {//하락 구간을 벗어났다.
 						riseCnt = 0;//이전 산의 것은 초기화
-						riseCnt++;
 						riseFlag= true;//상승의 시작
 					}
+					riseCnt++;
 				}
 				else {//값이 하락했다.
-					if(riseFlag) {//꼭대기에서 꺾였다.
+					if(riseFlag)//꼭대기에서 꺾였다.
 						riseFlag = false;
-						result+=riseCnt;
-					}
-					else {//계속 하락하는 구간
-						result+=riseCnt;
-					}
+					result+=riseCnt;
 				}
 				preH=curH;
 			}
@@ -50,7 +37,17 @@ public class Solution {
 			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
 		System.out.println(sb);
-		sc.close();
+	}
+	public static int readInt() throws Exception {
+		int val = 0;
+		int c = System.in.read();
+		while(c<=' ') {
+			c = System.in.read();
+		}
+		do {
+			val = 10*val+c-48;
+		}while((c=System.in.read()) >= 48 && c<=57);
+		return val;
 	}
 
 }
