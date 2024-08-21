3320adebb26731075a0056f4fdf60776a2efaa77
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-08-21 22:10:22 +0900

    [D5] Title: 현주가 좋아하는 제곱근 놀이, Time: 201 ms, Memory: 39,404 KB -BaekjoonHub

--- Java/SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
+++ Java/SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
@@ -7,28 +7,27 @@ import java.io.InputStreamReader;
  *
  */
 public class Solution {
-	static double N;
-	static long result;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		for(int tc=1; tc<=T; tc++) {
-			N = Double.parseDouble(br.readLine());
-			result = 0;
-			process();
+			double N = Double.parseDouble(br.readLine());
+			int result = process(N);
 			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
 		System.out.println(sb);
 	}
-	public static void process() {
+	public static int process(double N) {
+		int result = 0;
 		while(N>2) {
 			double tempCeil = Math.ceil(Math.sqrt(N));
 			double tempPow = Math.pow(tempCeil, 2);
 			result+=tempPow-N+1;
 			N=tempCeil;
 		}
+		return result;
 	}
 
 }
