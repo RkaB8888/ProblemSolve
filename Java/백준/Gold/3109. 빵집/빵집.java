d4db47e845833f109a653d61a0f137b7b33a6fb5
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-08-23 01:32:05 +0900

    [Gold II] Title: 빵집, Time: 260 ms, Memory: 46608 KB -BaekjoonHub

--- Java/백준/Gold/3109. 빵집/빵집.java
+++ Java/백준/Gold/3109. 빵집/빵집.java
@@ -3,52 +3,51 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * 메모리:37,020KB, 시간:288ms
- */
 public class Main {
-	static int R, C;
-	static boolean[][] map;
-	static int cnt;
-	static boolean flag;//생성된 경로 원복할지 결정
-	static int[] dr = {-1,0,1}; //우상 우 우하
- 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		map = new boolean[R][C];
-		for (int i = 0; i < R; i++) {
-			String temp = br.readLine();
-			for (int j = 0; j < C; j++) {
-				if (temp.charAt(j) == 'x')
-					map[i][j] = true;
-			}
-		}
-		C-=2;
-		for (int i = 0; i < R; i++) {
-			flag = false;
-			findPipePath(i, 0);
-		}
-		System.out.println(cnt);
+    static int R, C;
+    static char[][] map;
+    static int cnt;
+    static int[] dr = {-1, 0, 1}; // 우상, 우, 우하
+    static int endCol;
 
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        map = new char[R][C];
+        endCol = C - 2; // 마지막 열의 인덱스
 
-	public static void findPipePath(int row, int col) {
-		if(col==C) {
-			cnt++;
-			flag = true;
-			return;
-		}
-		int c = col+1;
-		for(int i = 0 ; i < 3 ; i++) {
-			int r = row+dr[i];
-			if(r<0||r>=R||map[r][c]) continue;
-			map[r][c]=true;
-			findPipePath(r,c);
-			if(flag) return;
-		}
-		
-	}
+        for (int i = 0; i < R; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
 
+        for (int i = 0; i < R; i++) {
+            if (findPipePath(i, 0)) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    public static boolean findPipePath(int row, int col) {
+        if (col == endCol) {
+            return true;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            int r = row + dr[i];
+            int c = col + 1;
+
+            if (r >= 0 && r < R && map[r][c] == '.') {
+                map[r][c] = 'x'; // 경로를 차지했음을 표시
+                if (findPipePath(r, c)) {
+                    return true;
+                }
+            }
+        }
+
+        return false; // 모든 경로가 실패하면 false를 반환
+    }
 }
