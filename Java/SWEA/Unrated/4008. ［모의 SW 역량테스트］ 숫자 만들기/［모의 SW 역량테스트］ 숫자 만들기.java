f002b513059f9d59b3042cd1ad52ee0cfc96ed3f
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-08-23 00:32:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 161 ms, Memory: 22,788 KB -BaekjoonHub

--- Java/SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ Java/SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -3,98 +3,82 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- *넥퍼를 이용한 연산
- *
- */
 public class Solution {
-	
-	static int N;
-	static int[] Operand;
-	static int[] Operator = new int[4]; // + - * /
-	static int[] Permutation;
-	static int PermutationLen;
-	static int max, min;
-	static int result;
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for(int tc=1; tc<=T; tc++) {
-			
-			N = Integer.parseInt(br.readLine());
-			Operand = new int[N];
-			PermutationLen = N;
-			Permutation = new int[PermutationLen];
-			max = -100000000;
-			min = 100000000;
-			
-			st = new StringTokenizer(br.readLine());
-			int pCnt = 1;
-			for(int i = 0 ; i < 4 ; i++) {
-				Operator[i] = Integer.parseInt(st.nextToken());
-				for(int j = 0, jEnd = Operator[i];j<jEnd;j++) {
-					Permutation[pCnt++] = i;
-				}
-			}
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0 ; i < N ; i++) {
-				Operand[i] = Integer.parseInt(st.nextToken());
-			}
-			int cnt;
-			int start = Operand[0];
-			do {
-				result = start;
-				cnt = 1;
-				while(cnt<PermutationLen) {
-					switch (Permutation[cnt]) {
-					case 0:
-						result+=Operand[cnt];
-						break;
-					case 1:
-						result-=Operand[cnt];
-						break;
-					case 2:
-						result*=Operand[cnt];
-						break;
-					case 3:
-						result/=Operand[cnt];
-						break;
-					}
-					cnt++;
-				}
-				if(result<min) min = result;
-				if(result>max) max = result;
-			}while(np());
-			sb.append('#').append(tc).append(' ').append(max-min).append('\n');
-		}
-		System.out.println(sb);
-	}
-	
-	public static boolean np() {
-		int i = PermutationLen-1;
-		while(i>1&&Permutation[i]<=Permutation[i-1]) {
-			i--;
-		}
-		if(i==1) return false; // 순열 생성 끝
-		int j = PermutationLen-1;
-		while(Permutation[i-1]>=Permutation[j]) {
-			j--;
-		}
-		swap(i-1,j);
-		j = PermutationLen-1;
-		while(i<j) {
-			swap(i,j);
-			i++;j--;
-		}
-		return true;
-		
-	}
-	public static void swap(int a, int b) {
-		Permutation[a]^=Permutation[b];
-		Permutation[b]^=Permutation[a];
-		Permutation[a]^=Permutation[b];
-	}
+
+    static int N;
+    static int[] Operand;
+    static int[] Operator = new int[4]; // + - * /
+    static int[] Permutation;
+    static int max, min;
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+
+            N = Integer.parseInt(br.readLine());
+            Operand = new int[N];
+            Permutation = new int[N - 1];
+            max = Integer.MIN_VALUE;
+            min = Integer.MAX_VALUE;
+
+            st = new StringTokenizer(br.readLine());
+            int pCnt = 0;
+            for (int i = 0; i < 4; i++) {
+                int count = Integer.parseInt(st.nextToken());
+                for (int j = 0; j < count; j++) {
+                    Permutation[pCnt++] = i;
+                }
+            }
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                Operand[i] = Integer.parseInt(st.nextToken());
+            }
+
+            do {
+                int result = Operand[0];
+                for (int i = 0; i < N - 1; i++) {
+                    result = calculate(result, Operand[i + 1], Permutation[i]);
+                }
+                max = Math.max(max, result);
+                min = Math.min(min, result);
+            } while (nextPermutation());
+
+            sb.append('#').append(tc).append(' ').append(max - min).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static int calculate(int a, int b, int op) {
+        switch (op) {
+            case 0: return a + b;
+            case 1: return a - b;
+            case 2: return a * b;
+            case 3: return a / b;
+            default: throw new IllegalArgumentException("Unknown operator");
+        }
+    }
+
+    public static boolean nextPermutation() {
+        int i = Permutation.length - 1;
+        while (i > 0 && Permutation[i - 1] >= Permutation[i]) i--;
+        if (i == 0) return false;
+
+        int j = Permutation.length - 1;
+        while (Permutation[i - 1] >= Permutation[j]) j--;
+        swap(i - 1, j);
+
+        j = Permutation.length - 1;
+        while (i < j) swap(i++, j--);
+
+        return true;
+    }
+
+    public static void swap(int a, int b) {
+        int temp = Permutation[a];
+        Permutation[a] = Permutation[b];
+        Permutation[b] = temp;
+    }
 }
