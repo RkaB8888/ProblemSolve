82bd5114d2e6bea036b58bba339ad3fae44388e2
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-08-25 19:43:18 +0900

    [Unrated] Title: [모의 SW 역량테스트] 무선 충전, Time: 176 ms, Memory: 21,932 KB -BaekjoonHub

--- Java/SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
+++ Java/SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
@@ -1,32 +1,44 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
+/**
+ * 메모리:24,468KB, 시간:133ms
+ */
 public class Solution {
-	static int M;// 시간
-	static int A;// BC개수
+	static final int[][] dr_dc = { { 0, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
+	static Position[][] map;
+	static int M; // 이동시간
+	static int A; // BC의 개수
+	static int[] aMove; // 상 우 하 좌
+	static int[] bMove;
 	static int result;
-	public static void main(String[] args) throws IOException {
+	static int[] aP = new int[2];
+	static int[] bP = new int[2];
 
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-///////////////////////입력 시작//////////////////////////////////////////
-		int testCase = Integer.parseInt(br.readLine());
-		for (int t = 1; t <= testCase; t++) {
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
+			map = new Position[11][11];
+			for (int i = 1; i < 11; i++) {
+				for (int j = 1; j < 11; j++) {
+					map[i][j] = new Position();
+				}
+			}
 			M = Integer.parseInt(st.nextToken());
 			A = Integer.parseInt(st.nextToken());
-			int[] aMove = new int[M];
-			int[] bMove = new int[M];
-			int[][] BCset = new int[A+1][4];
-			for(int i = 0 ; i < 4 ; i++) {
-				BCset[0][i] = 0;
-			}
+			aMove = new int[M];
+			bMove = new int[M];
+			aP[0] = 1;
+			aP[1] = 1;
+			bP[0] = 10;
+			bP[1] = 10;
+			result = 0;
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < M; i++) {
 				aMove[i] = Integer.parseInt(st.nextToken());
@@ -35,161 +47,102 @@ public class Solution {
 			for (int i = 0; i < M; i++) {
 				bMove[i] = Integer.parseInt(st.nextToken());
 			}
-			for (int i = 1; i <= A; i++) {
+			for (int i = 0; i < A; i++) {
 				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < 4; j++) {
-					BCset[i][j] = Integer.parseInt(st.nextToken());
-				}
+				setting(i, st);
 			}
-///////////////////////입력 끝//////////////////////////////////////////
-///////////////////////맵 설정 시작//////////////////////////////////////////
-			Position[][] map = new Position[11][11];
-			for (int i = 1; i <= 10; i++) {
-				for (int j = 1; j <= 10; j++) {
-					map[i][j] = new Position(i, j);
-				}
-			}
-			for (int i = 1; i <= A; i++) {
-				int x = BCset[i][0], y = BCset[i][1];
-				int range = BCset[i][2];
-				int perform = BCset[i][3];
-				map[x][y].addBC(perform,i);
-				for (int j = 1; j <= range; j++) {
-					if (x + j < 11)
-						map[x + j][y].addBC(perform,i);
-					if (x - j > 0)
-						map[x - j][y].addBC(perform,i);
-					if (y + j < 11)
-						map[x][y + j].addBC(perform,i);
-					if (y - j > 0)
-						map[x][y - j].addBC(perform,i);
-				}
-				for (int j = 1; j <= range; j++) {
-					for (int k = 1; k <= range; k++) {
-						if (j + k <= range) {
-							if (x + j < 11) {
-								if (y + k < 11)
-									map[x + j][y + k].addBC(perform,i);
-								if (y - k > 0)
-									map[x + j][y - k].addBC(perform,i);
-							}
-							if (x - j > 0) {
-								if (y + k < 11)
-									map[x - j][y + k].addBC(perform,i);
-								if (y - k > 0)
-									map[x - j][y - k].addBC(perform,i);
-							}
-						}
-					}
-				}
-			}
-///////////////////////맵 설정 끝//////////////////////////////////////////
-///////////////////////사람 이동 시작//////////////////////////////////////////
-			Person a = new Person(1,1);
-			Person b = new Person(10,10);
-			result = 0;
-			Position ap = map[a.i][a.j];
-			Position bp = map[b.i][b.j];
-			getCharge(ap,bp);
-			
+			calc();
 			for (int i = 0; i < M; i++) {
-				a.move(aMove[i]);
-				b.move(bMove[i]);
-				
-				ap = map[a.i][a.j];
-				bp = map[b.i][b.j];
-				getCharge(ap,bp);
+				// 충전 계산
+				move(aMove[i], bMove[i]);// 다음 좌표 계산
+				calc();
 			}
-///////////////////////사람 이동 끝//////////////////////////////////////////
-			sb.append('#').append(t).append(' ').append(result).append('\n');
+
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
 		System.out.println(sb);
 	}
-	public static void getCharge(Position ap, Position bp) {
-		if(ap.BCidx1==0&&bp.BCidx1==0) {
+
+	public static void calc() {
+		Position a = map[aP[0]][aP[1]];
+		Position b = map[bP[0]][bP[1]];
+		if (a.BC1key == b.BC1key) {
+			result += a.BC1P + Math.max(a.BC2P, b.BC2P);
+		} else {
+			result += a.BC1P + b.BC1P;
 		}
-		else if(ap.BCidx1==0) {
-			result+=bp.BCP1;
+	}
+
+	public static void move(int a, int b) {
+		aP[0] += dr_dc[a][0];
+		aP[1] += dr_dc[a][1];
+		bP[0] += dr_dc[b][0];
+		bP[1] += dr_dc[b][1];
+	}
+
+	public static void setting(int key, StringTokenizer st) {
+		int row = Integer.parseInt(st.nextToken());
+		int col = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int p = Integer.parseInt(st.nextToken());
+		map[row][col].setBC(key, p);
+		for (int j = 1; j <= c; j++) {
+			int mCol = col - j, pCol = col + j;
+			if(mCol>0) map[row][mCol].setBC(key, p);
+			if(pCol<11) map[row][pCol].setBC(key, p);
 		}
-		else if(bp.BCidx1==0) {
-			result+=ap.BCP1;
-		}
-		else {
-			if(ap.BCidx1==bp.BCidx1) { // 제일 큰게 같다면
-				if(ap.BCidx2==bp.BCidx2) { // 두번째도 같다면
-					result += ap.BCP1;
-					result += bp.BCP2;
-				}
-				else { // 두번째는 다르다면
-					result+=(ap.BCP2>bp.BCP2)?(ap.BCP2+bp.BCP1):(bp.BCP2+ap.BCP1);
+		for (int i = 1; i <= c; i++) {
+			int mRow = row - i, pRow = row + i;
+			if (mRow > 0) {
+				map[mRow][col].setBC(key, p);
+				for (int j = 1; j <= c; j++) {
+					if (i + j > c)
+						break;
+					int mCol = col - j, pCol = col + j;
+					if (pCol < 11) {
+						map[mRow][pCol].setBC(key, p);
+					}
+					if (mCol > 0) {
+						map[mRow][mCol].setBC(key, p);
+					}
 				}
 			}
-			else { // 제일 큰게 다르다면
-				result+= ap.BCP1;
-				result+=bp.BCP1;
+			if (pRow < 11) {
+				map[pRow][col].setBC(key, p);
+				for (int j = 1; j <= c; j++) {
+					if (i + j > c)
+						break;
+					int mCol = col - j, pCol = col + j;
+					if (pCol < 11) {
+						map[pRow][pCol].setBC(key, p);
+					}
+					if (mCol > 0) {
+						map[pRow][mCol].setBC(key, p);
+					}
+				}
 			}
 		}
 	}
 
-}
+	static class Position {
+		int BC1key = -1;// 해당 BC 고유 번호
+		int BC2key = -1;
+		int BC1P;// 충전량
+		int BC2P;
 
-class Position {
-	int i;
-	int j;
-	int BCidx1;// 그 위치의 BC 중 가장 perform이 높은 인덱스
-	int BCP1;
-	int BCidx2;
-	int BCP2;
-	List<int[]> BCP = new ArrayList<>();
+		public void setBC(int bCkey, int bCP) {
+			if (bCP > BC1P) {
+				BC2P = BC1P;
+				BC2key = BC1key;
+				BC1P = bCP;
+				BC1key = bCkey;
 
-	public Position(int i, int j) {
-		super();
-		this.i = i;
-		this.j = j;
-	}
-
-
-	public void addBC(int perform, int BCidx) {
-		if(BCP1<perform) {
-			BCP2 = BCP1;
-			BCidx2 = BCidx1;
-			BCP1 = perform;
-			BCidx1 = BCidx;
+			} else if (bCP > BC2P) {
+				BC2P = bCP;
+				BC2key = bCkey;
+			}
 		}
-		else if(BCP2<perform) {
-			BCP2 = perform;
-			BCidx2 = BCidx;
-		}
-	}
-}
 
-class Person {
-	int i;
-	int j;
-
-	public Person(int i, int j) {
-		super();
-		this.i = i;
-		this.j = j;
 	}
 
-	public void move(int path) {
-		switch (path) {
-		case 1://상
-			j--;
-			break;
-		case 2://우
-			i++;
-			break;
-		case 3://하
-			j++;
-			break;
-		case 4://좌
-			i--;
-			break;
-
-		default:
-			break;
-		}
-	}
 }
