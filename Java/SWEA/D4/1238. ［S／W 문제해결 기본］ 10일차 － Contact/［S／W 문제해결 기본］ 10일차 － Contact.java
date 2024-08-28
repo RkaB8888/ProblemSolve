7fc2430439f4038fe7bd986095d9c96f218ccb86
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-08-28 17:47:51 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 130 ms, Memory: 20,848 KB -BaekjoonHub

--- Java/SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
+++ Java/SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -2,68 +2,69 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
+
 /**
- * 메모리:19,100KB, 시간:118ms
- * bfs 탐색
- * 
+ * 메모리:19,328KB, 시간:125ms
  */
 public class Solution {
-	static int L; // 데이터의 길이
-	static int startNodeNum;
-	static int[][] adjMatrix;
-	static int maxNum;
+	static int L;
+	static int startNode;
+	static List<Integer>[] adjList;
 	static boolean[] Contact;
+	static int maxNum;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		int T = 10;	//테스트케이스
+		int T = 10; // 테스트케이스
 		for (int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
 			L = Integer.parseInt(st.nextToken());
-			L/=2;
-			startNodeNum = Integer.parseInt(st.nextToken());
+			startNode = Integer.parseInt(st.nextToken());
+			L /= 2;
+			adjList = new ArrayList[101];
+			for (int i = 0; i < 101; i++) {
+				adjList[i]= new ArrayList<Integer>();
+			}
 			Contact = new boolean[101];
-			adjMatrix = new int[101][101];
 			st = new StringTokenizer(br.readLine());
-			for(int i = 0 ; i < L ; i++) {
+			for (int i = 0; i < L; i++) {
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
-				if(adjMatrix[from][to]==0)adjMatrix[from][0]++;
-				adjMatrix[from][to]++;
+				adjList[from].add(to);
 			}
-			/////////입력 끝//////////////
+
 			bfs();
 			sb.append('#').append(tc).append(' ').append(maxNum).append('\n');
 		}
 		System.out.println(sb);
 	}
+
 	public static void bfs() {
 		Queue<Integer> q = new ArrayDeque<>();
-		
-		q.add(startNodeNum); // 마중물
-		Contact[startNodeNum] = true; // 해당 노드 방문 표시
-		
-		while(!q.isEmpty()) {
+		q.add(startNode);
+		Contact[startNode] = true;
+
+		while (!q.isEmpty()) {
 			int size = q.size();
 			int num = 0;
-			while(--size>=0) {//해당 레벨만 돌려본다.
+			while(--size>=0) {
 				int curNode = q.poll();
-				if(num<curNode) num = curNode;
-				int idx = 1;
-				while(adjMatrix[curNode][0]>0&&idx<101) {
-					if(adjMatrix[curNode][idx]>0&&Contact[idx]==false) {//idx 노드로 길이 있다면
-						Contact[idx] = true;
-						q.add(idx);
-						adjMatrix[curNode][idx] = 0;//지나간 간선 제거
-						adjMatrix[curNode][0]--;//해당 노드의 간선 수 차감
-					}
-					idx++;
+				if(curNode>num) num = curNode;
+				int adjSize = adjList[curNode].size();
+				while(--adjSize>=0) {
+					int nextNode = adjList[curNode].get(adjSize);
+					if(Contact[nextNode])continue;
+					q.add(nextNode);
+					Contact[nextNode] = true;
 				}
 			}
-			maxNum=num;
+			maxNum = num;
 		}
 	}
+
 }
