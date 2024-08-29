174258dae72f6f6c19acf79b3bca2dedbc8c8562
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-08-30 00:17:35 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 437 ms, Memory: 105,136 KB -BaekjoonHub

--- Java/SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ Java/SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -6,11 +6,14 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 /**
- * 메모리:52,216kb, 시간:1,408ms 큐에 넣어서 한 타임마다 size로 while문 돌림 큐에서 뽑기 전에 매번 정렬시켜서
- * 마지막에 칸을 차지하면 그 방향대로 크리쳐에는 방향과 좌표, 맵에는 양이 담겨 있다
+ * 메모리:100,620kb, 시간:469ms 
+ * 큐에 군집을 담고 크기 내림차순으로 정렬
+ * 맵2에 값이 존재하면 그 값에 추가만 해줌
  */
 public class Solution {
-	static int N, M, K, map1[][], map2[][];// 한변의 길이, 격리시간, 군집 수
+	static int N, M, K, preN = 10;
+	static int[][] map1 = new int[preN][preN];
+	static int[][] map2 = new int[preN][preN];;// 한변의 길이, 격리시간, 군집 수
 	static final int[][] dr_dc = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 	static final int[] mirror = { 1, 0, 3, 2 };
 	static List<creature> l1 = new ArrayList<>();
@@ -40,8 +43,11 @@ public class Solution {
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
-			map1 = new int[N][N];
-			map2 = new int[N][N];
+			if(N>preN) {
+				map1 = new int[N][N];
+				map2 = new int[N][N];
+				preN = N;
+			}
 			l1.clear();
 			for (int i = 0; i < K; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -64,6 +70,7 @@ public class Solution {
 
 	public static void simulation() {
 		int curTime = 0;
+		boolean[][] mapCheck = new boolean[N][N];
 		while (++curTime<=M&&!l1.isEmpty()) {
 			l1.sort((a, b) -> map1[b.i][b.j] - map1[a.i][a.j]);
 			for(creature creatureA : l1 ) {
@@ -71,11 +78,12 @@ public class Solution {
 				map1[creatureA.i][creatureA.j] = 0;
 				creatureA.i += dr_dc[creatureA.path][0];
 				creatureA.j += dr_dc[creatureA.path][1];
-				if (map2[creatureA.i][creatureA.j]>0) {// 이미 선점되어 있다면
+				if (mapCheck[creatureA.i][creatureA.j]) {// 이미 선점되어 있다면
 					map2[creatureA.i][creatureA.j] += amount;
 					continue;
 				}
-				map2[creatureA.i][creatureA.j] += amount;
+				mapCheck[creatureA.i][creatureA.j] = true;
+				map2[creatureA.i][creatureA.j] = amount;
 				if (creatureA.i == 0 || creatureA.j == 0 || creatureA.i == N - 1 || creatureA.j == N - 1) {
 					creatureA.path = mirror[creatureA.path];
 					map2[creatureA.i][creatureA.j] /= 2;
@@ -86,9 +94,12 @@ public class Solution {
 			l1.clear(); 
 			l1.addAll(l2);
 			l2.clear();
-
+			
+			int[][] tempMap = map1;
 			map1 = map2;
-			map2 = new int[N][N];
+			map2 = tempMap;
+			
+			mapCheck = new boolean[N][N];
 		}
 	}
 }
