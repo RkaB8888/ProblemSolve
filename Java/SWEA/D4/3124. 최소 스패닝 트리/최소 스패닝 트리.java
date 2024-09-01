ba5742da3b7902abcb334937c05556b441fc8548
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-01 20:53:03 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,162 ms, Memory: 184,584 KB -BaekjoonHub

--- Java/SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ Java/SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,16 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+
 /**
- * 메모리:110,496KB, 시간:1,974ms
- * KRUSKAL 알고리즘 사용
+ * 메모리:185,576, 시간:2,486ms
  */
 public class Solution {
-	static int V, E;
-	static int[] Groups = new int[100001];;
-	static int[][] Es;
+	static int V;
+	static int E;
+	static boolean[] Visited;
+	static List<int[]>[] list;
+	static int startNode;
+	static long result;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -20,46 +26,48 @@ public class Solution {
 			st = new StringTokenizer(br.readLine());
 			V = Integer.parseInt(st.nextToken());
 			E = Integer.parseInt(st.nextToken());
-			Es = new int[E][3];
-			makeSet();
+			list = new ArrayList[V+1]; 
+			Visited = new boolean[V+1];
+			result = 0;
+			
+			for (int i = 1; i <= V; i++) {
+                list[i] = new ArrayList<>();
+            }
+			
 			for(int i = 0 ; i < E ; i++) {
 				st = new StringTokenizer(br.readLine());
-				Es[i][0] = Integer.parseInt(st.nextToken());
-				Es[i][1] = Integer.parseInt(st.nextToken());
-				Es[i][2] = Integer.parseInt(st.nextToken());
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				int weight = Integer.parseInt(st.nextToken());
+				list[from].add(new int[] {to,weight});
+				list[to].add(new int[] {from,weight});
 			}
-			Arrays.sort(Es,(a,b)-> a[2]-b[2]);
-			sb.append("#").append(tc).append(" ").append(process()).append("\n");
+			Prim(1);
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
 		System.out.println(sb);
 	}
-	
-	public static long process() {
-		long minVal = 0;
-		for(int i = 0 ; i < E ; i++) {
-			if(union(Es[i][0],Es[i][1])) {
-				minVal+=Es[i][2];
-				if(Groups[findSet(Es[i][0])]==-V) break;
+	public static void Prim(int start) {
+		PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b) -> a[1]-b[1]);
+		pq.addAll(list[start]);
+		Visited[start] = true;
+		int nodeCnt = 1;
+		while(!pq.isEmpty()&&nodeCnt<V) {
+			int[] next = pq.poll();
+			int to = next[0];
+			int weight = next[1];
+			
+			if(Visited[to]) continue;
+			
+			Visited[to] = true;
+			nodeCnt++;
+			result+=weight;
+			for(int[] edge : list[to]) {
+				if(!Visited[edge[0]]) {
+					pq.add(edge);
+				}
 			}
 		}
-		
-		return minVal;
 	}
-	public static void makeSet() {
-		for(int i =1 ; i <= 100000 ; i++) {
-			Groups[i] = -1;
-		}
-	}
-	public static int findSet(int a) {
-		if(Groups[a]<0) return a;
-		return Groups[a] = findSet(Groups[a]);
-	}
-	public static boolean union(int a, int b) {
-		int aRoot = findSet(a);
-		int bRoot = findSet(b);
-		if(aRoot==bRoot) return false;
-		Groups[bRoot]+=Groups[aRoot];
-		Groups[aRoot] = bRoot;
-		return true;
-	}
+
 }
