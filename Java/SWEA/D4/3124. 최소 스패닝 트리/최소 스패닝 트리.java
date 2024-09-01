e01fb2aefcc5991dfb46d7fd2863c6f6bcd0cd63
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-01 21:41:50 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,502 ms, Memory: 188,844 KB -BaekjoonHub

--- Java/SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ Java/SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -8,81 +8,73 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /**
- * 메모리:231,244kb, 시간:3,071ms
- * 우선순위큐를 사용함
+ * 
  */
 public class Solution {
-	static int V, E;
-	static boolean[] Visited;
-	static List<Node>[] list;
+    static int V, E;
+    static boolean[] Visited;
+    static List<Node>[] list;
 
-	static class Node {
-		int to, w;
-		public Node(int to, int w) {
-			this.to = to;
-			this.w = w;
-		}
-		
-	}
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			st = new StringTokenizer(br.readLine());
-			V = Integer.parseInt(st.nextToken());
-			E = Integer.parseInt(st.nextToken());
-			list = new List[V];
-			Visited = new boolean[V];
+    static class Node {
+        int to, w;
+        public Node(int to, int w) {
+            this.to = to;
+            this.w = w;
+        }
+    }
 
-			for (int i = 0; i < V; i++) {
-				list[i] = new ArrayList<>();
-			}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(st.nextToken());
+            E = Integer.parseInt(st.nextToken());
+            list = new List[V];
+            Visited = new boolean[V];
 
-			for (int i = 0; i < E; i++) {
-				st = new StringTokenizer(br.readLine());
-				int from = Integer.parseInt(st.nextToken()) - 1;
-				int to = Integer.parseInt(st.nextToken()) - 1;
-				int weight = Integer.parseInt(st.nextToken());
-				list[from].add(new Node( to, weight ));
-				list[to].add(new Node( from, weight ));
-			}
-			sb.append("#").append(tc).append(" ").append(Prim(0)).append("\n");
-		}
-		System.out.println(sb);
-	}
+            for (int i = 0; i < V; i++) {
+                list[i] = new ArrayList<>();
+            }
 
-	public static long Prim(int start) {
-		long result = 0;
-		PriorityQueue<Node> pq = new PriorityQueue<Node>((a, b) -> a.w - b.w);
-		Visited[start] = true;
-		int nodeCnt = 1;
-		int[] minVal = new int[V];
-		Arrays.fill(minVal, Integer.MAX_VALUE);
-		minVal[start] = 0;
-		for (Node edge : list[start]) {
-			if(minVal[edge.to]<=edge.w) continue;
-			minVal[edge.to] = edge.w;
-			pq.offer(edge);
-		}
-		while (!pq.isEmpty() && nodeCnt < V) {
-			Node next = pq.poll();
+            for (int i = 0; i < E; i++) {
+                st = new StringTokenizer(br.readLine());
+                int from = Integer.parseInt(st.nextToken()) - 1;
+                int to = Integer.parseInt(st.nextToken()) - 1;
+                int weight = Integer.parseInt(st.nextToken());
+                list[from].add(new Node(to, weight));
+                list[to].add(new Node(from, weight));
+            }
+            sb.append("#").append(tc).append(" ").append(Prim(0)).append("\n");
+        }
+        System.out.println(sb);
+    }
 
-			if (Visited[next.to])
-				continue;
+    public static long Prim(int start) {
+        long result = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.w - b.w);
+        int[] minVal = new int[V];
+        Arrays.fill(minVal, Integer.MAX_VALUE);
+        minVal[start] = 0;
+        
+        pq.offer(new Node(start, 0));
+        
+        while (!pq.isEmpty()) {
+            Node next = pq.poll();
+            if (Visited[next.to]) continue;
 
-			Visited[next.to] = true;
-			nodeCnt++;
-			result += next.w;
-			for (Node edge : list[next.to]) {
-				if (Visited[edge.to]) continue;
-				if(minVal[edge.to]<=edge.w) continue;
-				minVal[edge.to] = edge.w;
-				pq.offer(edge);
-			}
-		}
-		return result;
-	}
+            Visited[next.to] = true;
+            result += next.w;
 
+            for (Node edge : list[next.to]) {
+                if (!Visited[edge.to] && minVal[edge.to] > edge.w) {
+                    minVal[edge.to] = edge.w;
+                    pq.offer(new Node(edge.to, edge.w));
+                }
+            }
+        }
+        return result;
+    }
 }
