6624b2ef2cae507263b9bb71ab554ca18bf80245
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-03 12:21:23 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 201 ms, Memory: 23,452 KB -BaekjoonHub

--- Java/SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ Java/SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,13 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 
 public class Solution {
 	static int N, map[][];
 	static final int[] dr = { -1, 1, 0, 0 };
 	static final int[] dc = { 0, 0, -1, 1 };
+	
+	static class Node{
+		int i,j,w;
+		Node next,pre;
 
+		public Node(int i, int j, int w, Node next, Node pre) {
+			super();
+			this.i = i;
+			this.j = j;
+			this.w = w;
+			this.next = next;
+			this.pre = pre;
+		}
+		
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -27,7 +41,6 @@ public class Solution {
 	}
 
 	static int getMinDistance(int startI, int startJ, int endI, int endJ) {
-		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
 		int[][] minDistance = new int[N][N];// 시작 정점에서 자신으로의 최소 거리
 		boolean[][] visited = new boolean[N][N];// 방문한 정점 관리
 		final int INF = Integer.MAX_VALUE;
@@ -38,20 +51,37 @@ public class Solution {
 			}
 		}
 		minDistance[startI][startJ] = 0;
-		pq.add(new int[] { startI, startJ, minDistance[startI][startJ] });
-		while (!pq.isEmpty()) {
-			int[] cur = pq.poll();
-			if (visited[cur[0]][cur[1]])
-				continue;
-			if (cur[0] == endI && cur[1] == endJ)
-				return cur[2];
+		Node nodes = new Node(startI,startJ,0,null,null);
+		
+		while (nodes!=null) {
+			Node node = null;
+			int minW = INF;
+			for(Node temp = nodes ; temp!=null ; temp = temp.next) {
+				if(!visited[temp.i][temp.j]&&temp.w<minW) {
+					node = temp;
+					minW=temp.w;
+				}
+			}
+			if(node==null)break;
+			
+			// 리스트에서 노드 제거
+	        if (node.pre != null) node.pre.next = node.next;
+	        if (node.next != null) node.next.pre = node.pre;
+	        if (nodes == node) nodes = node.next; // 헤드 노드 업데이트
+	        
+			visited[node.i][node.j] = true;
+			
+			if (node.i == endI && node.j == endJ)
+				return node.w;
+			
 			for (int i = 0; i < 4; i++) {
-				int nr = cur[0] + dr[i];
-				int nc = cur[1] + dc[i];
+				int nr = node.i + dr[i];
+				int nc = node.j + dc[i];
 				if (nr >= 0 && nr < N && nc >= 0 && nc < N && !visited[nr][nc]
-						&& minDistance[nr][nc] > cur[2] + map[nr][nc]) {
-					minDistance[nr][nc] = cur[2] + map[nr][nc];
-					pq.add(new int[] { nr, nc, minDistance[nr][nc] });
+						&& minDistance[nr][nc] > node.w + map[nr][nc]) {
+					minDistance[nr][nc] = node.w + map[nr][nc];
+					nodes = new Node(nr,nc,minDistance[nr][nc],nodes,null);
+					if(nodes.next!=null) nodes.next.pre = nodes;
 				}
 			}
 		}
