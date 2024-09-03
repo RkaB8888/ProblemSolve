3f80e2a4013c4e30e222c4b7772fe6b823a3064c
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-03 23:31:14 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 548 ms, Memory: 108260 KB -BaekjoonHub

--- Java/백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ Java/백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -6,8 +6,8 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
- * 메모리:111,368KB, 시간:924ms 맵에 저장하는 수치 - 한번 부수고 도달한 최소 거리 - 한번도 안 부수고 도달한 최소 거리
- * 이동하는 녀석은 - 벽을 부순 적이 있는지 - 이동 거리
+ * 메모리:108,684KB, 시간:600ms 이전에 부순적이 있을 때의 최소 거리와 없을 때의 최소 거리를 따로 별도로 기록 이동하는 녀석은
+ * - 벽을 부순 적이 있는지 - 이동 거리
  */
 public class Main {
 
@@ -24,8 +24,9 @@ public class Main {
 
 	}
 
-	static int N, M, map[][], dist[][][];
-	static final int INF = Integer.MAX_VALUE, drdc[][] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+	static int N, M, map[][];
+	static boolean visited[][][];
+	static final int drdc[][] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,13 +34,11 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		map = new int[N][M];
-		dist = new int[N][M][2];// 0: 부순 적 없음, 1: 부순 적 있음
+		visited = new boolean[N][M][2];// 0: 부순 적 없음, 1: 부순 적 있음
 		for (int i = 0; i < N; i++) {
 			char[] c = br.readLine().toCharArray();
 			for (int j = 0; j < M; j++) {
 				map[i][j] = c[j] - '0';
-				dist[i][j][0] = INF;
-				dist[i][j][1] = INF;
 			}
 		}
 
@@ -49,12 +48,15 @@ public class Main {
 	public static int bfs() {
 		Queue<Cursor> q = new ArrayDeque<>();
 		q.add(new Cursor(0, 0, 1, false));
-		dist[0][0][0] = 1;
+		visited[0][0][0] = true;
 
 		while (!q.isEmpty()) {
 			Cursor cursor = q.poll();
 			int r = cursor.i, c = cursor.j, d = cursor.dist;
 			boolean broke = cursor.broke;
+			int brokeIdx = broke?1:0;
+
+			visited[r][c][brokeIdx] = true;
 			if (r == N - 1 && c == M - 1)
 				return d;
 
@@ -64,17 +66,12 @@ public class Main {
 				if (nr < 0 || nr >= N || nc < 0 || nc >= M)
 					continue;
 
-				if (map[nr][nc] == 1 && !broke) { // 벽을 만났고, 부순 적 없음
-					if (dist[nr][nc][1] > d + 1) {
-						dist[nr][nc][1] = d + 1;
-						q.add(new Cursor(nr, nc, d + 1, true));
-					}
-				} else if (map[nr][nc] == 0) { // 벽이 아님
-					int brokeIndex = broke ? 1 : 0;
-					if (dist[nr][nc][brokeIndex] > d + 1) {
-						dist[nr][nc][brokeIndex] = d + 1;
-						q.add(new Cursor(nr, nc, d + 1, broke));
-					}
+				if (map[nr][nc] == 1 && !broke && !visited[nr][nc][1]) { // 벽을 만났고, 부순 적 없음
+					visited[nr][nc][1] = true;
+					q.add(new Cursor(nr, nc, d + 1, true));
+				} else if (map[nr][nc] == 0 && !visited[nr][nc][brokeIdx]) { // 벽이 아님
+					visited[nr][nc][brokeIdx] = true;
+					q.add(new Cursor(nr, nc, d + 1, broke));
 				}
 			}
 
