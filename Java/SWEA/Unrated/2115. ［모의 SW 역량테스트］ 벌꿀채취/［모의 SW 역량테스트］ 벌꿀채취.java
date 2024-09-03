5858e15921c0579a1f51e472958b553555922037
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-04 01:50:22 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 122 ms, Memory: 20,888 KB -BaekjoonHub

--- Java/SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ Java/SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- * 메모리:27,276KB , 시간:122ms
+ * 메모리:18,592KB , 시간:120ms
  * 벌통 M개를 선택한 방법 중 가장 큰 값을 vals에 저장
  * 각 인부의 합을 vals를 통해 출력
  */
@@ -25,46 +25,48 @@ public class Solution {
 			map = new int[N][N];
 			vals = new int[N][N];
 			max = 0;
+			
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
+			
 			valSet();
+			
 			findMax();
+			
 			sb.append('#').append(tc).append(' ').append(max).append('\n');
 		}
 		System.out.println(sb);
 	}
 	public static void findMax() {
-		for(int i = 0 ; i < N*N ; i++) {
-			int iR = i/N, iC = i%N;
-			if(iC+M>N) continue;
-			for(int j = i+M ; j < N*N ; j++) {
-				int jR = j/N, jC = j%N;
-				if(jC+M>N) continue;
-				max = Math.max(vals[iR][iC]+vals[jR][jC], max);
-			}
-		}
+		for (int i = 0; i < N; i++) {
+            for (int j = 0, jEnd = N-M; j <= jEnd; j++) {
+                for (int i2 = i; i2 < N; i2++) {
+                    for (int j2 = (i == i2 ? j + M : 0) ; j2 <= jEnd; j2++) {
+                        max = Math.max(max, vals[i][j] + vals[i2][j2]);
+                    }
+                }
+            }
+        }
 	}
 	public static void valSet() {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j <= N - M; j++) {
-				combinate(i,j,i,j,0,0,0);
+				vals[i][j] = combinate(i,j,0,0,0,0);
 			}
 		}
 	}
 
-	public static void combinate(int sr, int sc,int r, int c, int cnt, int sum, int val) {
-		if(sum>C) return;
-		if (cnt == M) {
-			if (val > vals[sr][sc])
-				vals[sr][sc] = val;
-			return;
-		}
-		int num = map[r][c];
-		combinate(sr,sc,r,c+1,cnt+1,sum+num,val+num*num);
-		combinate(sr,sc,r,c+1,cnt+1,sum,val);
+	public static int combinate(int r, int c, int cnt, int sum, int val, int maxVal) {
+		if(sum>C) return maxVal;
+		if(cnt == M) return Math.max(val, maxVal);
+		if(c>=N) return maxVal;
+		
+		int newVal = combinate(r,c+1,cnt+1,sum+map[r][c],val+map[r][c]*map[r][c],Math.max(val, maxVal));
+		int skipVal = combinate(r,c+1,cnt+1,sum,val,maxVal);
+		return Math.max(newVal, skipVal);
 	}
 }
