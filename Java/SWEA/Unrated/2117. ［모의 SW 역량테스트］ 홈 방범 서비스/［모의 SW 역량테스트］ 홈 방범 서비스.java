85b584a705f573c27010fc7b680fb72df4b28b0f
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-04 22:16:45 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 175 ms, Memory: 23,588 KB -BaekjoonHub

--- Java/SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
+++ Java/SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -10,8 +11,20 @@ import java.util.StringTokenizer;
  */
 public class Solution {
 	static int N, M, map[][];
+	static Home[] homes;
+	static int homeCnt;
 	static int result;
-	
+	static class Home {
+		int i, j;
+
+		public Home(int i, int j) {
+			super();
+			this.i = i;
+			this.j = j;
+		}
+
+	}
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -22,43 +35,46 @@ public class Solution {
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			map = new int[N][N];
+			homes = new Home[N * N];
+			homeCnt = 0;
 			result = 0;
-			
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
+					int num = Integer.parseInt(st.nextToken());
+					if (num == 1)
+						homes[homeCnt++] = new Home(i, j);
 				}
 			}
-			process();// K를 1부터 늘려가며 K = N까지 늘려가며 최대 이익 찾기
+			homes = Arrays.copyOf(homes, homeCnt);
+			process();// K를 1부터 늘려가며 K = N+1까지 늘려가며 최대 이익 찾기
 			sb.append('#').append(tc).append(' ').append(result).append('\n');
 		}
 		System.out.println(sb);
 	}
 
 	public static void process() {
+		int homeMax = 0;
 		for (int K = 1; K <=N+1; K++) {//K범위를 늘려감
-			int operationCost = K*K + (K-1)*(K-1);
-			for(int i = 0 ; i < N ; i++) {
-				for(int j = 0 ; j < N ; j++) {
-					int homeCnt = calcHomeCnt(i,j,K);
-					int profit = homeCnt*M - operationCost;
-					if(profit >= 0 && homeCnt > result) {
-						result = homeCnt;
+			int operate = K*K+(K-1)*(K-1);//색칠한 곳에서 가장 많이 겹치는 곳 = 집의 갯수
+			for (int HN = 0; HN < homeCnt; HN++) {
+				Home home = homes[HN];
+				int x = home.i, y = home.j;
+				for(int i = x-K+1 ; i < x+K ; i++) {
+					if(i<0||i>=N) continue;
+					for(int j = y-K+1 ; j < y+K ; j++) {
+						if(j<0||j>=N) continue;
+						if(Math.abs(x-i)+Math.abs(y-j)==K-1) {
+							map[i][j]++;
+							if(map[i][j]>homeMax) homeMax = map[i][j];
+						}
 					}
 				}
 			}
+			int get = M*homeMax;
+			if(get-operate>=0&&result<homeMax) result = homeMax;
+			
 		}
 	}
-	public static int calcHomeCnt(int x, int y, int K) {
-		int cnt = 0;
-		for(int i = 0 ; i < N ; i++) {
-			for(int j = 0 ; j < N ; j++) {
-				if (map[i][j] == 1 && Math.abs(i - x) + Math.abs(j - y) < K) {
-                    cnt++;
-                }
-			}
-		}
-		return cnt;
-	}
+
 }
