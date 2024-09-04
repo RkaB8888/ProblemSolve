ead77810c10de724642b2b0018bd4be11f5b4601
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-04 16:45:56 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 34700 KB -BaekjoonHub

--- Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -4,7 +4,10 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Queue;
-
+/**
+ * 메모리:34848KB, 시간:156ms
+ * 1부터 시작해서 bfs 탐색 dp에 그 숫자까지의 최소 연산 횟수 저장
+ */
 public class Main {
 	static int N;
 	static int[] dp;
@@ -12,25 +15,24 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		dp = new int[N+1];
-		Arrays.fill(dp, Integer.MAX_VALUE);
-		dp[0] = 0; dp[1] = 0;
 		Queue<Integer> q = new ArrayDeque<>();
 		q.add(1);
 		
 		while(!q.isEmpty()) {
 			int num = q.poll();
 			if(num==N) break;
-			if(num*3<=N&&dp[num*3]>dp[num]+1) {
-				dp[num*3] = dp[num]+1;
-				q.add(num*3);
+			int num1 = num+1, num2 = num*2, num3 = num*3, nCnt = dp[num]+1;
+			if(num3<=N&&dp[num3]==0) {
+				dp[num3] = nCnt;
+				q.add(num3);
 			}
-			if(num*2<=N&&dp[num*2]>dp[num]+1) {
-				dp[num*2] = dp[num]+1;
-				q.add(num*2);
+			if(num2<=N&&dp[num2]==0) {
+				dp[num2] = nCnt;
+				q.add(num2);
 			}
-			if(num+1<=N&&dp[num+1]>dp[num]+1) {
-				dp[num+1] = dp[num]+1;
-				q.add(num+1);
+			if(num1<=N&&dp[num1]==0) {
+				dp[num1] = nCnt;
+				q.add(num1);
 			}
 		}
 		System.out.println(dp[N]);
