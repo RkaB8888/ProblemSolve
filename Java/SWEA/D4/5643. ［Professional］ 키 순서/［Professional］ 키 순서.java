c2ab68f25578ae70a790fdcf257b5724d947c17b
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-06 10:15:08 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,334 ms, Memory: 93,352 KB -BaekjoonHub

--- Java/SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ Java/SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -4,10 +4,10 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /*
- * 메모리 94,988 kb 실행시간 2,052 ms
+ * 메모리 91,304 kb 실행시간 1,765 ms
  */
 public class Solution {
-	static int N, M, result, adjMatrix[][], cnt;
+	static int N, M, result, adjMatrix[][],radjMatrix[][], cnt;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,19 +19,21 @@ public class Solution {
 			N = Integer.parseInt(br.readLine());
 			M = Integer.parseInt(br.readLine());
 
-			adjMatrix = new int[N + 1][N + 1]; // 학생번호 1번부터 시작
+			adjMatrix = new int[N + 1][N + 1]; // 학생번호 1번부터 시작, 나보다 큰 학생 정보 행으로 유지
+			radjMatrix = new int[N + 1][N + 1]; // 학생번호 1번부터 시작, 나보다 작은 학생 정보 행으로 유지
 
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				adjMatrix[a][b] = 1;
+				radjMatrix[b][a] = 1;
 			}
 			result = 0;
 			for (int i = 1; i <= N; i++) {
 				cnt = 0;
-				ltDFS(i, new boolean[N+1]);// i녀석보다 작은 놈의 개수
-				gtDFS(i, new boolean[N+1]);
+				DFS(i, adjMatrix, new boolean[N+1]);// i녀석보다 작은 놈의 개수
+				DFS(i, radjMatrix, new boolean[N+1]);
 				if (cnt == N - 1)
 					result++;
 			}
@@ -40,21 +42,11 @@ public class Solution {
 		System.out.println(sb);
 	}
 
-	public static void gtDFS(int cur, boolean[] visited) {// 나보다 큰 녀석의 수를 찾는다.
+	public static void DFS(int cur,int[][] matrix ,boolean[] visited) {// 나보다 큰 녀석의 수를 찾는다.
 		visited[cur] = true;
 		for (int i = 1; i <= N; i++) {
-			if (!visited[i] && adjMatrix[cur][i] != 0) {
-				gtDFS(i, visited);
-				cnt++; // 나보다 큰 대상 카운팅
-			}
-		}
-	}
-
-	public static void ltDFS(int cur, boolean[] visited) {// 나보다 큰 녀석의 수를 찾는다.
-		visited[cur] = true;
-		for (int i = 1; i <= N; i++) {
-			if (!visited[i] && adjMatrix[i][cur] != 0) {
-				ltDFS(i, visited);
+			if (!visited[i] && matrix[cur][i] != 0) {
+				DFS(i, matrix,visited);
 				cnt++; // 나보다 큰 대상 카운팅
 			}
 		}
