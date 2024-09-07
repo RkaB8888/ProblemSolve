12a189099c0ee9e731508148b71d1d35a50d366d
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-08 00:30:38 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 68 ms, Memory: 11788 KB -BaekjoonHub

--- Java/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ Java/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,71 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
- * 메모리:12488kb, 시간:216ms
- * 
+ * 메모리:400220kb, 시간:676ms dp사용 가로는 대각선,가로에 가능 dp[i+1][j][0] += dp[i][j][0];
+ * dp[i+1][j+1][2] += dp[i][j][0]; 세로는 대각선, 세로에 가능 dp[i][j+1][1] += dp[i][j][1];
+ * dp[i+1][j+1][2] += dp[i][j][1]; 대각선은 대각선, 가로, 세로 가능 dp[i+1][j][0] +=
+ * dp[i][j][2]; dp[i][j+1][1] += dp[i][j][2]; dp[i+1][j+1][2] += dp[i][j][2];
  */
 public class Main {
-	static int N, result, map[][], dir[][] = {{0,1},{1,0},{1,1}};//가로, 세로, 대각선
-	static class Pipe{
-		int x, y, status, cnt;
-		//status는 0:가로 1:세로 2:대각선
-		public Pipe(int x, int y, int status) {
-			this.x = x;
-			this.y = y;
-			this.status = status;
-		}
-		
-	}
+	static int N, result, map[][], dir[][] = { { 0, 1 }, { 1, 0 }, { 1, 1 } };// 가로, 세로, 대각선
+	static int dp[][][];// i,j,type
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
 		map = new int[N][N];
-		for(int i = 0 ; i < N ; i++) {
+		dp = new int[N][N][N];
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0 ; j < N ; j++) {
+			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		bfs();
-		System.out.println(result);
-	}
-	public static void bfs() {
-		Queue<Pipe> q = new ArrayDeque<>();
-		q.add(new Pipe(0,1,0));
-		while(!q.isEmpty()) {
-			Pipe cur = q.poll();
-			if(cur.x==N-1&&cur.y==N-1) {
-				result++;
-				continue;
+		if (map[0][2] == 0) {
+			dp[0][2][0] = 1;
+			if (map[1][2] == 0 && map[1][1] == 0) {
+				dp[1][2][2] = 1;
 			}
-			boolean flag = true;//대각선 가능한지 체크
-			
-			int r = cur.x+dir[0][0], c = cur.y+dir[0][1];
-			if(r>=0&&r<N&&c>=0&&c<N&&map[r][c]!=1) {
-				if(cur.status!=1) {
-					q.add(new Pipe(r,c,0));
+			for (int i = 0, iEnd = N - 1; i < iEnd; i++) {
+				for (int j = 2, jEnd = N - 1; j < jEnd; j++) {
+					if (map[i][j] == 1)
+						continue;
+					if (map[i + 1][j] == 0 && map[i][j + 1] == 0 && map[i + 1][j + 1] == 0) {
+						dp[i + 1][j + 1][2] += dp[i][j][0];
+						dp[i + 1][j + 1][2] += dp[i][j][1];
+						dp[i + 1][j + 1][2] += dp[i][j][2];
+					}
+					if (map[i + 1][j] == 0) {
+						dp[i + 1][j][1] += dp[i][j][1];
+						dp[i + 1][j][1] += dp[i][j][2];
+					}
+					if (map[i][j + 1] == 0) {
+						dp[i][j + 1][0] += dp[i][j][0];
+						dp[i][j + 1][0] += dp[i][j][2];
+					}
 				}
 			}
-			else flag = false;
-			
-			r = cur.x+dir[1][0]; c = cur.y+dir[1][1];
-			if(r>=0&&r<N&&c>=0&&c<N&&map[r][c]!=1) {
-				if(cur.status!=0) {
-					q.add(new Pipe(r,c,1));
-				}
+			for (int i = 1, iEnd = N - 1, j = N - 1; i < iEnd; i++) {
+				if (map[i][j] == 1 || map[i + 1][j] == 1)
+					continue;
+				dp[i + 1][j][1] += dp[i][j][1] + dp[i][j][2];
 			}
-			else flag = false;
-			
-			r = cur.x+dir[2][0]; c = cur.y+dir[2][1];
-			if(flag&&map[r][c]!=1) {
-				q.add(new Pipe(r,c,2));
+			for (int j = 3, jEnd = N - 1, i = N - 1; j < jEnd; j++) {
+				if (map[i][j] == 1 || map[i][j + 1] == 1)
+					continue;
+				dp[i][j + 1][0] += dp[i][j][0] + dp[i][j][2];
 			}
 		}
+		System.out.println(dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2]);
 	}
 }
