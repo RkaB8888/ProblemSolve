c0464e0898c4eb524c04eb451da16e5bf6d73095
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-08 03:01:53 +0900

    [D4] Title: [Professional] 키 순서, Time: 502 ms, Memory: 91,964 KB -BaekjoonHub

--- Java/SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ Java/SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -3,11 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 /*
- * 메모리 93,528 kb 실행시간 672 ms
+ * 메모리 93,528 kb 실행시간 513 ms
  */
 public class Solution {
 	static int N, M, result;
-	static int map[][], rmap[][];
+	static int map[][];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,22 +19,17 @@ public class Solution {
 			N = Integer.parseInt(br.readLine());
 			M = Integer.parseInt(br.readLine());
 			map = new int[N + 1][N + 1];
-			rmap = new int[N + 1][N + 1];
 			for (int i = 1; i <= N; i++) {
 				map[i][0] = -1;
-				rmap[i][0] = -1;
 			}
 			result = 0;
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				map[a][b] = 1;
-				rmap[b][a] = 1;
+				map[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
 			}
 			for (int i = 1; i <= N; i++) {
 				if (map[i][0] == -1)
-					dfs1(i);
+					dfs(i);
 			}
 			for(int i = 1 ; i <=N ; i++) {
 				for(int j = 1 ; j <= N ; j++) {
@@ -49,13 +44,13 @@ public class Solution {
 		System.out.println(sb);
 	}
 
-	public static void dfs1(int str) {
+	public static void dfs(int str) {
 		map[str][0] = 0;
 		for (int i = 1; i <= N; i++) {
 			if (map[str][i] == 0)
 				continue;
 			if (map[i][0] == -1) {
-				dfs1(i);
+				dfs(i);
 			}
 			if (map[i][0] != 0) {
 				for (int j = 1; j <= N; j++) {
