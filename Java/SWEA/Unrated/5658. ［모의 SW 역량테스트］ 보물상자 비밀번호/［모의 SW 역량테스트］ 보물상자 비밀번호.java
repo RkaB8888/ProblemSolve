c8e9aed5a4fa25851cdb0c62f7c1bcfd0d10d7a2
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-07 20:16:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 98 ms, Memory: 18,632 KB -BaekjoonHub

--- Java/SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ Java/SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -1,57 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
+/**
+ * 메모리:27,288KB, 시간:181ms
+ *
+ */
 public class Solution {
-    static int N, K, sideLen;
-    static TreeSet<Integer> treeSet = new TreeSet<>((a, b) -> b - a);
+	static int N, K, sideLen, result[], arr[];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			sideLen = N/4;//한 변의 길이
+			char c[] = br.readLine().toCharArray();
+			arr = new int[N];
+			result = new int[N];
+			for(int i = 0 ; i < N ; i++) {//각 단위 10진수 변환
+				if(c[i]<='9') {
+					arr[i] = c[i]-'0';
+				}
+				else {
+					arr[i] = c[i]-'A'+10;
+				}
+			}
+			for(int i = 0; i < sideLen ; i++) {
+				result[0] = (result[0]<<4)|arr[i];
+			}
+			int leftShift = (sideLen-1)*4;
+			for(int i = 1, j = N-1 ; i<N&&j > 0 ; i++,j--) {
+				result[i]=result[i-1]>>4;
+				result[i]+=arr[j]<<leftShift;
+			}
+			Arrays.sort(result);
+			int preNum = 0, cnt = 0;
+			for(int i = N-1 ; i >= 0 ; i--) {
+				if(preNum!=result[i]) {
+					cnt++;
+					preNum = result[i];
+				}
+				if(cnt==K) break;
+			}
+			sb.append('#').append(tc).append(' ').append(preNum).append('\n');
+		}
+		System.out.println(sb);
+	}
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        
-        for (int tc = 1; tc <= T; tc++) {
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-            sideLen = N / 4; // 한 변의 길이
-            treeSet.clear();
-            
-            // 16진수로 이루어진 문자열을 배열로 변환
-            char[] c = br.readLine().toCharArray();
-            
-            // 각 회전마다 새로운 수를 추가
-            for (int start = 0; start < sideLen; start++) {
-                for (int i = 0; i < 4; i++) {  // 각 변을 10진수로 변환
-                    int num = 0;
-                    for (int j = 0; j < sideLen; j++) {
-                        char ch = c[(start + i * sideLen + j) % N];
-                        if (ch <= '9') {
-                            num = (num << 4) + (ch - '0'); // 숫자면
-                        } else {
-                            num = (num << 4) + (ch - 'A' + 10); // 문자면
-                        }
-                    }
-                    treeSet.add(num); // 회전한 후의 16진수를 treeSet에 추가
-                }
-            }
-
-            // TreeSet을 통해 내림차순으로 정렬된 상태에서 K번째로 큰 값을 찾음
-            int count = 0;
-            int result = 0;
-            for (int num : treeSet) {
-                count++;
-                if (count == K) {
-                    result = num;
-                    break;
-                }
-            }
-            sb.append('#').append(tc).append(' ').append(result).append('\n');
-        }
-        System.out.println(sb);
-    }
 }
