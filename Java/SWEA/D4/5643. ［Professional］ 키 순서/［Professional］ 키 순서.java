be63714ae2368017474e1fec421096cda00208fe
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-08 02:55:30 +0900

    [D4] Title: [Professional] 키 순서, Time: 940 ms, Memory: 92,768 KB -BaekjoonHub

--- Java/SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ Java/SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -2,90 +2,94 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/*
+ * 메모리 93,528 kb 실행시간 672 ms
+ */
 public class Solution {
-    static int N, M, result;
-    static int[][] map;
+	static int N, M, result;
+	static int map[][], rmap[][];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
-        int TC = Integer.parseInt(br.readLine()); // 테스트 케이스 수
-        for (int tc = 1; tc <= TC; tc++) {
-            N = Integer.parseInt(br.readLine());
-            M = Integer.parseInt(br.readLine());
-            map = new int[N + 1][N + 1];
+		int TC = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= TC; tc++) {
+			N = Integer.parseInt(br.readLine());
+			M = Integer.parseInt(br.readLine());
+			map = new int[N + 1][N + 1];
+			rmap = new int[N + 1][N + 1];
+			for (int i = 1; i <= N; i++) {
+				map[i][0] = -1;
+				rmap[i][0] = -1;
+			}
+			result = 0;
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				map[a][b] = 1;
+				rmap[b][a] = 1;
+			}
+			for (int i = 1; i <= N; i++) {
+				if (map[i][0] == -1)
+					dfs1(i);
+				if (rmap[i][0] == -1)
+					dfs2(i);
+				if (map[i][0] + rmap[i][0] == N - 1)
+					result++;
+			}
+//			for(int i = 0 ; i <= N ; i++) {
+//				System.out.println(Arrays.toString(map[i]));
+//			}
+//			System.out.println("//////////////////////////////////");
+//			for(int i = 0 ; i <= N ; i++) {
+//				System.out.println(Arrays.toString(rmap[i]));
+//			}
+			sb.append('#').append(tc).append(' ').append(result).append('\n');
+		}
+		System.out.println(sb);
+	}
 
-            // map 초기화: 행과 열의 0번 인덱스에 -1로 표시
-            for (int i = 1; i <= N; i++) {
-                map[i][0] = -1;
-                map[0][i] = -1;
-            }
+	public static void dfs1(int str) {
+		map[str][0] = 0;
+		for (int i = 1; i <= N; i++) {
+			if (map[str][i] == 0)
+				continue;
+			if (map[i][0] == -1) {
+				dfs1(i);
+			}
+			if (map[i][0] != 0) {
+				for (int j = 1; j <= N; j++) {
+					if (map[i][j] == 1)
+						map[str][j] = 1;
+				}
+			}
+		}
+		for (int i = 1; i <= N; i++) {
+			map[str][0] += map[str][i];
+		}
+	}
 
-            result = 0;
-
-            // M개의 간선 입력 받기
-            for (int i = 0; i < M; i++) {
-                st = new StringTokenizer(br.readLine());
-                int u = Integer.parseInt(st.nextToken());
-                int v = Integer.parseInt(st.nextToken());
-                map[u][v] = 1; // 연결된 간선 표시
-            }
-
-            // 각각의 행과 열에 대해 dfs 수행
-            for (int i = 1; i <= N; i++) {
-                if (map[i][0] == -1) {
-                    dfs(i, true);  // 행 기반 탐색
-                }
-                if (map[0][i] == -1) {
-                    dfs(i, false); // 열 기반 탐색
-                }
-                if (map[i][0] + map[0][i] == N - 1) {
-                    result++; // 모든 노드가 연결된 경우 결과 카운트 증가
-                }
-            }
-
-            sb.append('#').append(tc).append(' ').append(result).append('\n');
-        }
-        System.out.println(sb);
-    }
-
-    // 하나의 dfs 함수로 통합, isRow가 true이면 행을 탐색, false이면 열 탐색
-    public static void dfs(int idx, boolean isRow) {
-        if (isRow) {
-            map[idx][0] = 0;
-            for (int i = 1; i <= N; i++) {
-                if (map[idx][i] == 0) continue;
-                if (map[i][0] == -1) {
-                    dfs(i, true); // 행을 기준으로 dfs
-                }
-                if (map[i][0] != 0) {
-                    for (int j = 1; j <= N; j++) {
-                        map[idx][j] |= map[i][j];
-                    }
-                }
-            }
-            for (int i = 1; i <= N; i++) {
-                map[idx][0] += map[idx][i];
-            }
-        } else { // 열 기준 탐색
-            map[0][idx] = 0;
-            for (int i = 1; i <= N; i++) {
-                if (map[i][idx] == 0) continue;
-                if (map[0][i] == -1) {
-                    dfs(i, false); // 열을 기준으로 dfs
-                }
-                if (map[0][i] != 0) {
-                    for (int j = 1; j <= N; j++) {
-                        map[j][idx] |= map[j][i];
-                    }
-                }
-            }
-            for (int i = 1; i <= N; i++) {
-                map[0][idx] += map[i][idx];
-            }
-        }
-    }
+	public static void dfs2(int str) {
+		rmap[str][0] = 0;
+		for (int i = 1; i <= N; i++) {
+			if (rmap[str][i] == 0)
+				continue;
+			if (rmap[i][0] == -1) {
+				dfs2(i);
+			}
+			if (rmap[i][0] != 0) {
+				for (int j = 1; j <= N; j++) {
+					if (rmap[i][j] == 1)
+						rmap[str][j] = 1;
+				}
+			}
+			
+		}
+		for (int i = 1; i <= N; i++) {
+			rmap[str][0] += rmap[str][i];
+		}
+	}
 }
