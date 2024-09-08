a674031af9a90a5b395ac4084adef216e2aa64c2
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-08 22:24:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 원자 소멸 시뮬레이션, Time: 236 ms, Memory: 76,992 KB -BaekjoonHub

--- Java/SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.java
+++ Java/SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.java
@@ -1,11 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /*
- * 메모리 76,612KB 시간 297ms
+ * 메모리 88,232KB 시간 269ms
  */
 public class Solution {
 	static int N, result;
@@ -22,7 +23,6 @@ public class Solution {
 		double x, y;
 
 		public Atom(int i, int j, int dir, int k, int sn) {
-			super();
 			this.x = i;
 			this.y = j;
 			this.dir = dir;
@@ -32,7 +32,7 @@ public class Solution {
 	}
 
 	// 충돌이 발생했을 때 정보를 큐에 저장하기 위한 클래스
-	static class Collision implements Comparable<Collision>{
+	static class Collision implements Comparable<Collision> {
 		int startAtomNum;
 		int endAtomNum;
 		double collTime;
@@ -65,39 +65,38 @@ public class Solution {
 				atoms[i] = new Atom(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
 						Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), i);
 			}
-			//여기까지가 입력 받는 것
+			// 여기까지가 입력 받는 것
 			process();
 			sb.append('#').append(tc).append(' ').append(result).append('\n');
 		}
 		System.out.println(sb);
 	}
-	//각 원자의 충돌을 우선순위 큐에 담아서 하나씩 충돌 시킨다.
-	//이때 충돌 시간이 빠른 것을 먼저 처리하며, 이후에 나온 것에서 이미 충돌로 없어진 경우 그냥 넘어간다
+
+	// 각 원자의 충돌을 우선순위 큐에 담아서 하나씩 충돌 시킨다.
+	// 이때 충돌 시간이 빠른 것을 먼저 처리하며, 이후에 나온 것에서 이미 충돌로 없어진 경우 그냥 넘어간다
 	public static void process() {
-		Collision list[] = new Collision[N*(N-1)/2];
-		int size = 0;
-		for (int i = 0, iEnd = N - 1; i < iEnd; i++) {
+		List<Collision> collisionList = new ArrayList<>();
+		for (int i = 0; i < N - 1; i++) {
 			for (int j = i + 1; j < N; j++) {
 				Collision c = new Collision(i, j);
 				if (collTest(c)) {
-					list[size++] = c;
+					collisionList.add(c);
 				}
 			}
 		}
-		list = Arrays.copyOf(list, size);
-		Arrays.sort(list);
-		int cnt = 0;
-		while (cnt<size) {
-			Collision c = list[cnt];
+		collisionList.sort(null);
+		int cnt = 0, size = collisionList.size();
+		while (cnt < size) {
+			Collision c = collisionList.get(cnt);
 			cnt++;
-			int startNum = c.startAtomNum, endNum = c.endAtomNum;
-			if (is[startNum]&&c.collTime!=CT[startNum] || is[endNum]&&c.collTime!=CT[endNum]) {
-				//둘 중에 없는게 있고, 그 충돌 시간과 다른 경우
-					continue;
+			int startNum = c.startAtomNum;
+			int endNum = c.endAtomNum;
+			
+			if (is[startNum] && c.collTime != CT[startNum] || is[endNum] && c.collTime != CT[endNum]) {
+				// 둘 중에 없는게 있고, 그 충돌 시간과 다른 경우
+				continue;
 			}
-			Atom start = atoms[startNum];
-			Atom end = atoms[endNum];
-			result += (is[startNum]?0:start.K) + (is[endNum]?0:end.K);
+			result += (is[startNum] ? 0 : atoms[startNum].K) + (is[endNum] ? 0 : atoms[endNum].K);
 			is[startNum] = true;
 			is[endNum] = true;
 			CT[startNum] = c.collTime;
@@ -107,7 +106,7 @@ public class Solution {
 
 	// 두 개의 원자가 충돌이 일어날 지 검사하는 부분
 	// 충돌이 일어난다면 c에 충돌 시간을 담고 true를 반환한다.
-	public static boolean collTest(Collision c) { 
+	public static boolean collTest(Collision c) {
 		Atom a = atoms[c.startAtomNum];
 		Atom b = atoms[c.endAtomNum];
 		int aDirX = dirs[a.dir][0];// a의 x축 이동방향
@@ -115,23 +114,26 @@ public class Solution {
 		int bDirX = dirs[b.dir][0];// b의 x축 이동방향
 		int bDirY = dirs[b.dir][1];// b의 y축 이동방향
 
-		if (a.dir == b.dir)//두 원자의 이동방향이 같다면 충돌이 발생하지 않음
+		double deltaX = b.x - a.x;
+		double deltaY = b.y - a.y;
+		int dX = aDirX - bDirX;
+		int dY = aDirY - bDirY;
+
+		if (a.dir == b.dir)// 두 원자의 이동방향이 같다면 충돌이 발생하지 않음
 			return false;
-		//좌표의 벡터와 이동방향의 벡터의 부호가 같다면 -> 거리만 생각했을 때 충돌 가능성을 계산 가능
+		// 좌표의 벡터와 이동방향의 벡터의 부호가 같다면 -> 거리만 생각했을 때 충돌 가능성을 계산 가능
 		// 서로 부호가 다르면 원자가 충돌하기 위해 가야하는 방향과 서로의 이동 방향이 다르다는 의미
-		if (((b.x - a.x)<0&&(aDirX - bDirX)<0||(b.x - a.x)==0&&(aDirX - bDirX)==0||(b.x - a.x)>0&&(aDirX - bDirX)>0)&& 
-				((b.y - a.y)<0&&(aDirY - bDirY)<0||(b.y - a.y)==0&&(aDirY - bDirY)==0||(b.y - a.y)>0&&(aDirY - bDirY)>0)) {//가야할 방향이 같은 경우
-			if(b.x-a.x==0) {
-				c.collTime = Math.abs((b.y - a.y)/2);
-			}
-			else if(b.y - a.y==0) {
-				c.collTime = Math.abs((b.x - a.x)/2);
+		if ((deltaX<0&&dX<0||deltaX==0&&dX==0||deltaX>0&&dX>0)&& 
+				(deltaY<0&&dY<0||deltaY==0&&dY==0||deltaY>0&&dY>0)) {// 가야할 방향이 같은 경우
+			if (deltaX == 0) {
+				c.collTime = Math.abs(deltaY / 2);
+			} else if (deltaY == 0) {
+				c.collTime = Math.abs(deltaX / 2);
+			} else if (Math.abs(deltaX) == Math.abs(deltaY)) {
+				c.collTime = Math.abs(deltaX); // 대각선 충돌
 			}
 			else {
-				double xR=Math.abs(a.x-b.x);
-				double yR=Math.abs(a.y-b.y);
-				if(xR!=yR) return false;
-				c.collTime = xR;
+				return false;
 			}
 			return true;
 		}
