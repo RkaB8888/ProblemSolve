96ca7a8404133932ced603c3da946fcea4d51d52
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-09 16:27:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 188 ms, Memory: 53,112 KB -BaekjoonHub

--- Java/SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ Java/SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-//import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -32,9 +29,6 @@ public class Solution {
 					}
 				}
 			}
-//			for(int i = 0 ; i <= R ; i++) {
-//				System.out.println(Arrays.toString(map[i]));
-//			}
 			result = map[0][0];
 			dfs(0, map);
 			sb.append('#').append(tc).append(' ').append(result).append('\n');
@@ -58,25 +52,13 @@ public class Solution {
 			if (curMap[0][c] == 0)
 				continue;
 			int newMap[][] = shoot(c, curMap); // 해당 열에 공을 쐇을 때 결과
-//			System.out.println("/////////////");
-//			for(int i = 0 ; i <= R ; i++) {
-//				System.out.println(Arrays.toString(curMap[i]));
-//			}
-//			System.out.println(c+"열로 발사");
-//			for(int i = 0 ; i <= R ; i++) {
-//				System.out.println(Arrays.toString(newMap[i]));
-//			}
 			dfs(cnt + 1, newMap);
 		}
 	}
 
 	public static int[][] shoot(int col, int curMap[][]) {
-		int newMap[][] = new int[R + 1][C + 1];
-		for (int i = 0; i <= R; i++) {// 맵 복사
-			for (int j = 0; j <= C; j++) {
-				newMap[i][j] = curMap[i][j];
-			}
-		}
+		int newMap[][] = copyMap(curMap);
+		
 		for (int i = 1; i <= R; i++) {// 공이 발사됐을 때 맞을 블럭 탐색
 			if (newMap[i][col] == 0)
 				continue;
@@ -92,63 +74,61 @@ public class Solution {
 		return newMap;
 	}
 
+	public static int[][] copyMap(int[][] curMap) {
+		int[][] newMap = new int[R + 1][C + 1];
+		for (int i = 0; i <= R; i++) {
+			System.arraycopy(curMap[i], 0, newMap[i], 0, C + 1);
+		}
+		return newMap;
+	}
+
 	public static void downSort(int[][] map) {
-		List<Integer> list = new ArrayList<>();
 		for (int j = 1; j <= C; j++) {
-			list.clear();
+			int emptyRow = R; // 빈 칸을 가리키는 포인터
 			for (int i = R; i > 0; i--) {
 				if (map[i][j] != 0) {
-					list.add(map[i][j]);
-					map[i][j]=0;
+					map[emptyRow][j] = map[i][j]; // 빈칸으로 블록 이동
+					if (emptyRow != i)
+						map[i][j] = 0; // 기존 위치를 0으로
+					emptyRow--;
 				}
-			}
-			for (int i = R, k = 0, kEnd = list.size(); k < kEnd; i--, k++) {
-				map[i][j] = list.get(k);
 			}
 		}
 	}
 
 	public static void remove(int map[][], int r, int c, int curRange) {
 		for (int i = 1; i < curRange; i++) {// 해당 범위만큼 제거
-			if (c - i > 0) {
-				if (map[r][c - i] != 0) {
-					int nextRange = map[r][c - i];
-					map[r][c - i] = 0;
-					map[0][c - i]--;
-					map[0][0]--;
-					if (nextRange != 1)
-						remove(map, r, c - i, nextRange);
-				}
+			if (c - i > 0 && map[r][c - i] != 0) {
+				int nextRange = map[r][c - i];
+				map[r][c - i] = 0;
+				map[0][c - i]--;
+				map[0][0]--;
+				if (nextRange != 1)
+					remove(map, r, c - i, nextRange);
 			}
-			if (c + i <= C) {
-				if (map[r][c + i] != 0) {
-					int nextRange = map[r][c + i];
-					map[r][c + i] = 0;
-					map[0][c + i]--;
-					map[0][0]--;
-					if (nextRange != 1)
-						remove(map, r, c + i, nextRange);
-				}
+			if (c + i <= C && map[r][c + i] != 0) {
+				int nextRange = map[r][c + i];
+				map[r][c + i] = 0;
+				map[0][c + i]--;
+				map[0][0]--;
+				if (nextRange != 1)
+					remove(map, r, c + i, nextRange);
 			}
-			if (r - i > 0) {
-				if (map[r - i][c] != 0) {
-					int nextRange = map[r - i][c];
-					map[r - i][c] = 0;
-					map[0][c]--;
-					map[0][0]--;
-					if (nextRange != 1)
-						remove(map, r - i, c, nextRange);
-				}
+			if (r - i > 0 && map[r - i][c] != 0) {
+				int nextRange = map[r - i][c];
+				map[r - i][c] = 0;
+				map[0][c]--;
+				map[0][0]--;
+				if (nextRange != 1)
+					remove(map, r - i, c, nextRange);
 			}
-			if (r + i <= R) {
-				if (map[r + i][c] != 0) {
-					int nextRange = map[r + i][c];
-					map[r + i][c] = 0;
-					map[0][c]--;
-					map[0][0]--;
-					if (nextRange != 1)
-						remove(map, r + i, c, nextRange);
-				}
+			if (r + i <= R && map[r + i][c] != 0) {
+				int nextRange = map[r + i][c];
+				map[r + i][c] = 0;
+				map[0][c]--;
+				map[0][0]--;
+				if (nextRange != 1)
+					remove(map, r + i, c, nextRange);
 			}
 		}
 	}
