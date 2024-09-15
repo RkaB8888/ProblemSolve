e8ab5eb20f7d9e519d90c5e2f72ebb51c38c2465
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-15 23:26:48 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 1,107 ms, Memory: 41,512 KB -BaekjoonHub

---
+++ Java/SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
@@ -0,0 +1,123 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ *  ?kb ?ms
+ */
+public class Solution {
+	static int N, result, map[][];
+	static final int drdc[][]= {{-1,0},{1,0},{0,-1},{0,1}};
+	//블럭 1,2,3,4,5에 대한 방향에 따른 반사된 이둉방향 지정
+	static final int reflectDir[][] = {//block type,drdc
+			{},
+			{1,3,0,2},//1일 때
+			{3,0,1,2},//2일 때
+			{2,0,3,1},//3일 때
+			{1,2,3,0},//4일 때
+			{1,0,3,2},//5일 때
+	};
+	static WormHole wList[], WH[][];
+
+	static class Ball{
+		int dir, sr, sc, cr, cc, cnt;
+
+		public Ball(int dir, int sr, int sc, int cnt) {
+			this.dir = dir;
+			this.sr = sr;
+			this.sc = sc;
+			this.cr = sr;
+			this.cc = sc;
+			this.cnt = cnt;
+		}
+		
+	}
+	static class WormHole{
+		int r, c, nr, nc;
+
+		public WormHole(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+		public void warp(Ball ball) {
+			ball.cr = this.nr;
+			ball.cc = this.nc;
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		int TC = Integer.parseInt(br.readLine().trim());
+		for (int tc = 1; tc <= TC; tc++) {
+			result = 0;
+			N = Integer.parseInt(br.readLine().trim());
+			map = new int[N][N];
+			WH = new WormHole[N][N];
+			wList = new WormHole[5];
+			for(int i = 0 ; i < N ; i++) {
+				st = new StringTokenizer(br.readLine().trim());
+				for(int j = 0 ; j < N ; j++) {
+					int val = Integer.parseInt(st.nextToken());
+					map[i][j] = val;
+					if(val>=6) {//월홈을 조사함
+						WH[i][j] = new WormHole(i,j);
+						if(wList[val-6]!=null) {
+							wList[val-6].nr = i;
+							wList[val-6].nc = j;
+							WH[i][j].nr = wList[val-6].r;
+							WH[i][j].nc = wList[val-6].c;
+						}
+						else {
+							wList[val-6] = WH[i][j];
+						}
+					}
+				}
+			}
+			dfs();
+			sb.append('#').append(tc).append(' ').append(result).append('\n');
+//			System.out.println(result);
+		}
+		System.out.println(sb);
+	}
+	public static void dfs() {
+		for(int i = 0 ; i < N ; i++) {
+			for(int j = 0 ; j < N ; j++) {
+				if(map[i][j]!=0) continue;
+				goStraight(i,j);
+			}
+		}
+	}
+	public static void goStraight(int row, int col) {
+		for(int i = 0 ; i < 4 ; i++) {
+			Ball ball = new Ball(i,row,col,0);
+			ball.cr += drdc[ball.dir][0];
+			ball.cc += drdc[ball.dir][1];
+			while(true) {
+				if(ball.cr<0||ball.cr>=N||ball.cc<0||ball.cc>=N) {
+					//경계면 충돌은 5번 블럭 충돌과 같음
+					ball.cnt++;
+					ball.dir = reflectDir[5][ball.dir];
+				}
+				else if(map[ball.cr][ball.cc]==-1||ball.cr==ball.sr&&ball.cc==ball.sc) {
+					//블랙홀을 만나거나 원위치로 돌아온 경우
+					result = Math.max(result, ball.cnt);
+					break;
+				}
+				else if(map[ball.cr][ball.cc]>=6) {
+					//웜홀을 만나서 워프시켜 줌
+					WH[ball.cr][ball.cc].warp(ball);
+				}
+				else if(map[ball.cr][ball.cc]>=1) {
+					//벽에 부딪혀서 방향 바꿔줌
+					ball.cnt++;
+					ball.dir = reflectDir[map[ball.cr][ball.cc]][ball.dir];
+				}
+				//다음 칸으로 이동
+				ball.cr += drdc[ball.dir][0];
+				ball.cc += drdc[ball.dir][1];
+			}
+		}
+	}
+}
