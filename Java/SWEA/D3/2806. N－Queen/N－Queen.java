92101de0b04bf2bc3905b7cc0e08fe7d0a7c0d09
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-18 06:19:11 +0900

    [D3] Title: N-Queen, Time: 122 ms, Memory: 19,224 KB -BaekjoonHub

--- Java/SWEA/D3/2806. N－Queen/N－Queen.java
+++ Java/SWEA/D3/2806. N－Queen/N－Queen.java
@@ -3,46 +3,58 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /*
- * 18,268 KB 177 ms
- * 완전탐색
+ * 18,664 KB 90 ms
+ * 비트마스크를 이용한 최적화
  */
 public class Solution {
-	static int N, result[] = new int[11];
-	static boolean map[][], colCheck[], backSlash[], slash[];
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		for(int i = 1 ; i < 11 ; i++) {
-			N = i;
-			map = new boolean[N][N];
-			colCheck = new boolean[N];
-			backSlash = new boolean[2*N-1];//행 열 합이 0 ~ 2N-2
-			slash = new boolean[2*N-1];//행 열 차가 -N+1 ~ N-1 -> N-1을 더함
-			dfs(0);
-		}
-		int TC = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= TC; tc++) {
-			N = Integer.parseInt(br.readLine());
-			sb.append('#').append(tc).append(' ').append(result[N]).append('\n');
-		}
-		System.out.println(sb);
-	}
+    static int N, result[] = new int[11];
+    static int colCheck, backSlash, slash; // 비트마스크로 사용
 
-	public static void dfs(int cnt) {
-		if (cnt == N) {
-			result[N]++;
-			return;
-		}
-		for (int i = 0; i < N; i++) {
-			if(colCheck[i]||backSlash[cnt+i]||slash[cnt-i+N-1]) continue;
-			colCheck[i]=true;
-			backSlash[cnt+i]=true;
-			slash[cnt-i+N-1]=true;
-			dfs(cnt+1);
-			colCheck[i]=false;
-			backSlash[cnt+i]=false;
-			slash[cnt-i+N-1]=false;
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        for (int i = 1; i < 11; i++) {
+            N = i;
+            result[N] = 0;
+            colCheck = 0;
+            backSlash = 0;
+            slash = 0;
+            dfs(0);
+        }
+
+        int TC = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= TC; tc++) {
+            N = Integer.parseInt(br.readLine());
+            sb.append('#').append(tc).append(' ').append(result[N]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static void dfs(int row) {
+        if (row == N) {
+            result[N]++;
+            return;
+        }
+        
+        for (int col = 0; col < N; col++) {
+            // 열(col), 슬래시(\), 백슬래시(/) 체크
+            if ((colCheck & (1 << col)) != 0 ||
+                (backSlash & (1 << (row + col))) != 0 ||
+                (slash & (1 << (row - col + N - 1))) != 0) continue;
+            
+            // 비트마스크로 표시
+            colCheck |= (1 << col);
+            backSlash |= (1 << (row + col));
+            slash |= (1 << (row - col + N - 1));
+            
+            // 다음 행으로 진행
+            dfs(row + 1);
+            
+            // 백트래킹: 비트마스크 복구
+            colCheck &= ~(1 << col);
+            backSlash &= ~(1 << (row + col));
+            slash &= ~(1 << (row - col + N - 1));
+        }
+    }
 }
