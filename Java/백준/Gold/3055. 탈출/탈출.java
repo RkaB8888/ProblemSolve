18e1a4e2fccf5e05c19ad41fc8586e53e6a58f3f
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-17 19:09:21 +0900

    [Gold IV] Title: 탈출, Time: 112 ms, Memory: 15728 KB -BaekjoonHub

--- Java/백준/Gold/3055. 탈출/탈출.java
+++ Java/백준/Gold/3055. 탈출/탈출.java
@@ -2,21 +2,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 /*
- * 메모리 18,548 KB 시간 192 ms
- * BFS 구현
+ * 메모리 20,408 KB 시간 136 ms
+ * BFS 구현 최적화
  */
 public class Main {
 
 	static int R, C, hedgehog[], drdc[][] = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } }; // 위, 왼, 우, 하
 	static char[][] map;
-	static boolean[][][] visited;
-	static List<char[][]> mapList = new ArrayList<>();
+	static boolean[][] visited;
 
 	public static void main(String[] args) throws IOException {
 
@@ -26,69 +23,72 @@ public class Main {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		map = new char[R][C];
-		visited = new boolean[R][C][R * C];
-		for (int i = 0; i < R; i++) {
+		visited = new boolean[R][C];
+		int i = 0;
+		out:while(i<R) {
 			map[i] = br.readLine().toCharArray();
-			for (int j = 0; j < C; j++) {
+			int j = 0;
+			while(j<C) {
 				if (map[i][j] == 'S') {
 					hedgehog = new int [] {i,j};
+					i++;
+					break out;
 				}
+				j++;
 			}
+			i++;
 		}
-		mapList.add(map);
+		while(i<R) {
+			map[i] = br.readLine().toCharArray();
+			i++;
+		}
 		System.out.print(bfs());
 	}
 
 	public static String bfs() {
 		Queue<int[]> q = new ArrayDeque<>();
 		q.add(new int[] { hedgehog[0], hedgehog[1], 0 });
-		visited[hedgehog[0]][hedgehog[1]][0] = true;
+		visited[hedgehog[0]][hedgehog[1]] = true;
 
 		while (!q.isEmpty()) {
-
-			int[] cur = q.poll();
-
-			int r = cur[0], c = cur[1], time = cur[2], nTime = time + 1;
-
-			for (int i = 0; i < 4; i++) {
-				int nr = r + drdc[i][0];
-				int nc = c + drdc[i][1];
-				if (nr < 0 || nc < 0 || nr >= R || nc >= C || visited[nr][nc][nTime])
-					continue;
-				if (mapList.size() == nTime) {// 다음 지도는 준비되어 있지 않다는 뜻
-					createNextMap(time);
-				}
-				char[][] nMap = mapList.get(nTime);
-				if (nMap[nr][nc] == '*'||nMap[nr][nc] == 'X')
-					continue;
-				else if (nMap[nr][nc] == 'D')
-					return String.valueOf(nTime);
-				q.add(new int[] { nr, nc, nTime });
-				visited[nr][nc][nTime] = true;
+			//맵 설정
+			char[][] curMap = new char[R][C];
+			for(int i = 0 ; i < R ; i++) {
+				System.arraycopy(map[i], 0, curMap[i], 0, C);
 			}
-
-		}
-		return "KAKTUS";
-	}
-
-	public static void createNextMap(int curTime) {
-		char[][] curMap = mapList.get(curTime);
-		char[][] nextMap = new char[R][C];
-		for(int i = 0 ; i < R ; i++) {
-			System.arraycopy(curMap[i], 0, nextMap[i], 0, C);
-		}
-		for(int i = 0 ; i < R ; i++) {
-			for(int j = 0 ; j < C ; j++) {
-				if(curMap[i][j]=='*') {
-					for(int dir = 0 ; dir<4 ; dir++) {
-						int nr = i+drdc[dir][0];
-						int nc = j+drdc[dir][1];
-						if (nr < 0 || nc < 0 || nr >= R || nc >= C||curMap[nr][nc]=='X'||curMap[nr][nc]=='D') continue;
-						nextMap[nr][nc] = '*';
+			for(int i = 0 ; i < R ; i++) {
+				for(int j = 0 ; j < C ; j++) {
+					if(map[i][j]=='*') {
+						for(int dir = 0 ; dir<4 ; dir++) {
+							int nr = i+drdc[dir][0];
+							int nc = j+drdc[dir][1];
+							if (nr < 0 || nc < 0 || nr >= R || nc >= C||map[nr][nc]=='X'||map[nr][nc]=='D') continue;
+							curMap[nr][nc] = '*';
+						}
 					}
 				}
 			}
+			map = curMap;
+			
+			int size = q.size();
+			while(--size>=0) {
+				//갈 수 있는 곳 추가
+				int[] cur = q.poll();
+				int r = cur[0], c = cur[1], time = cur[2], nTime = time + 1;
+				for (int i = 0; i < 4; i++) {
+					int nr = r + drdc[i][0];
+					int nc = c + drdc[i][1];
+					if (nr < 0 || nc < 0 || nr >= R || nc >= C || visited[nr][nc])
+						continue;
+					if (map[nr][nc] == '*'||map[nr][nc] == 'X')
+						continue;
+					else if (map[nr][nc] == 'D')
+						return String.valueOf(nTime);
+					q.add(new int[] { nr, nc, nTime });
+					visited[nr][nc]= true;
+				}
+			}
 		}
-		mapList.add(nextMap);
+		return "KAKTUS";
 	}
 }
