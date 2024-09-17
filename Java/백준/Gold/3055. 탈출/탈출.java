ae825f05cce05cdaa2b15876ed1249d037823d1f
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-09-17 19:29:35 +0900

    [Gold IV] Title: 탈출, Time: 68 ms, Memory: 11768 KB -BaekjoonHub

--- Java/백준/Gold/3055. 탈출/탈출.java
+++ Java/백준/Gold/3055. 탈출/탈출.java
@@ -6,14 +6,16 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 /*
- * 메모리 20,408 KB 시간 136 ms
+ * 메모리 15,728 KB 시간 112 ms
  * BFS 구현 최적화
  */
 public class Main {
 
-	static int R, C, hedgehog[], drdc[][] = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } }; // 위, 왼, 우, 하
+	static int R, C, drdc[][] = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } }; // 위, 왼, 우, 하
 	static char[][] map;
 	static boolean[][] visited;
+	static Queue<int[]> waterQ = new ArrayDeque<>();
+	static Queue<int[]> hedgehogQ = new ArrayDeque<>();
 
 	public static void main(String[] args) throws IOException {
 
@@ -24,68 +26,55 @@ public class Main {
 		C = Integer.parseInt(st.nextToken());
 		map = new char[R][C];
 		visited = new boolean[R][C];
-		int i = 0;
-		out:while(i<R) {
+		for (int i = 0; i < R; i++) {
 			map[i] = br.readLine().toCharArray();
-			int j = 0;
-			while(j<C) {
+			for (int j = 0; j < C; j++) {
 				if (map[i][j] == 'S') {
-					hedgehog = new int [] {i,j};
-					i++;
-					break out;
+					hedgehogQ.add(new int[] { i, j, 0 });
+					visited[i][j] = true;
+					map[i][j] = '.';
+				} else if (map[i][j] == '*') {
+					waterQ.add(new int[] { i, j });
 				}
-				j++;
 			}
-			i++;
-		}
-		while(i<R) {
-			map[i] = br.readLine().toCharArray();
-			i++;
 		}
 		System.out.print(bfs());
 	}
 
 	public static String bfs() {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] { hedgehog[0], hedgehog[1], 0 });
-		visited[hedgehog[0]][hedgehog[1]] = true;
 
-		while (!q.isEmpty()) {
-			//맵 설정
-			char[][] curMap = new char[R][C];
-			for(int i = 0 ; i < R ; i++) {
-				System.arraycopy(map[i], 0, curMap[i], 0, C);
-			}
-			for(int i = 0 ; i < R ; i++) {
-				for(int j = 0 ; j < C ; j++) {
-					if(map[i][j]=='*') {
-						for(int dir = 0 ; dir<4 ; dir++) {
-							int nr = i+drdc[dir][0];
-							int nc = j+drdc[dir][1];
-							if (nr < 0 || nc < 0 || nr >= R || nc >= C||map[nr][nc]=='X'||map[nr][nc]=='D') continue;
-							curMap[nr][nc] = '*';
-						}
-					}
+		while (!hedgehogQ.isEmpty()) {
+			// 맵 설정
+			int waterSize = waterQ.size();
+			while (--waterSize >= 0) {
+				int[] water = waterQ.poll();
+				for (int dir = 0; dir < 4; dir++) {
+					int nr = water[0] + drdc[dir][0];
+					int nc = water[1] + drdc[dir][1];
+					if (nr < 0 || nc < 0 || nr >= R || nc >= C || map[nr][nc] == '*' || map[nr][nc] == 'X'
+							|| map[nr][nc] == 'D')
+						continue;
+					map[nr][nc] = '*';
+					waterQ.add(new int[] { nr, nc });
 				}
 			}
-			map = curMap;
-			
-			int size = q.size();
-			while(--size>=0) {
-				//갈 수 있는 곳 추가
-				int[] cur = q.poll();
-				int r = cur[0], c = cur[1], time = cur[2], nTime = time + 1;
+
+			int hedgeSize = hedgehogQ.size();
+			while (--hedgeSize >= 0) {
+				// 갈 수 있는 곳 추가
+				int[] cur = hedgehogQ.poll();
+				int r = cur[0], c = cur[1], time = cur[2];
 				for (int i = 0; i < 4; i++) {
 					int nr = r + drdc[i][0];
 					int nc = c + drdc[i][1];
 					if (nr < 0 || nc < 0 || nr >= R || nc >= C || visited[nr][nc])
 						continue;
-					if (map[nr][nc] == '*'||map[nr][nc] == 'X')
+					if (map[nr][nc] == 'D')
+						return String.valueOf(time + 1);
+					if (map[nr][nc] == '*' || map[nr][nc] == 'X')
 						continue;
-					else if (map[nr][nc] == 'D')
-						return String.valueOf(nTime);
-					q.add(new int[] { nr, nc, nTime });
-					visited[nr][nc]= true;
+					hedgehogQ.add(new int[] { nr, nc, time + 1 });
+					visited[nr][nc] = true;
 				}
 			}
 		}
