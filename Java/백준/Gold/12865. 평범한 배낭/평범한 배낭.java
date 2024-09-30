58a79cca3ecf0d27a6d735b89679581644961cbe
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-10-01 06:58:47 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 51352 KB -BaekjoonHub

--- Java/백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ Java/백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -3,10 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 /**
- * 
  * @author python98
  * DP
- * ? KB ? ms
+ * 51392 KB 140 ms
  */
 public class Main {
 	static int N, K, item[][], dp[][];
@@ -24,14 +23,6 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			item[i][0] = Integer.parseInt(st.nextToken());
 			item[i][1] = Integer.parseInt(st.nextToken());
-		}
-		
-		for(int i = 0 ; i <= N ; i++) {
-			dp[i][0] = 0;
-		}
-		
-		for(int i = 0 ; i <= K ; i++) {
-			dp[0][i] = 0;
 		}
 		
 		for(int i = 1 ; i <= N ; i++) {
