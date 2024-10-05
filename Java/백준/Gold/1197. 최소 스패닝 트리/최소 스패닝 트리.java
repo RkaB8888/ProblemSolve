028191e9e552ffc6118dbf01fda4df4d9f9954c2
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-10-05 16:42:39 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 436 ms, Memory: 52008 KB -BaekjoonHub

--- Java/백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ Java/백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -6,18 +6,18 @@ import java.util.StringTokenizer;
 
 /**
  * 
- * @author python98 MST ? KB ? ms
+ * @author python98 프림 ? KB ? ms
  */
 public class Main {
-	static int V, E, group[], result;
+	static int V, E, result;
+	static boolean visited[];
 	static Node[] adjList;
 
 	static class Node implements Comparable<Node> {
-		int from, to, val;
+		int to, val;
 		Node next;
 
-		public Node(int from, int to, int val, Node next) {
-			this.from = from;
+		public Node(int to, int val, Node next) {
 			this.to = to;
 			this.val = val;
 			this.next = next;
@@ -35,6 +35,7 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
+		visited = new boolean[V + 1];
 		adjList = new Node[V + 1];
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -42,10 +43,9 @@ public class Main {
 			int to = Integer.parseInt(st.nextToken());
 			int val = Integer.parseInt(st.nextToken());
 
-			adjList[from] = new Node(from, to, val, adjList[from]);
-			adjList[to] = new Node(to, from, val, adjList[to]);
+			adjList[from] = new Node(to, val, adjList[from]);
+			adjList[to] = new Node(from, val, adjList[to]);
 		}
-		makeSet();
 		process();
 
 		System.out.print(result);
@@ -53,42 +53,24 @@ public class Main {
 
 	private static void process() {
 		PriorityQueue<Node> pq = new PriorityQueue<Node>();
-		for(Node cur = adjList[1] ; cur!=null ; cur = cur.next) {
+		for (Node cur = adjList[1]; cur != null; cur = cur.next) {
 			pq.add(cur);
 		}
+		visited[1] = true;
 		int cnt = 1;
-		while(!pq.isEmpty()) {
-			if(cnt==V) return;
+		while (!pq.isEmpty()) {
+			if (cnt == V)
+				return;
 			Node cur = pq.poll();
-			if(union(cur.from,cur.to)) {
-				result+=cur.val;
-				cnt++;
-				for(Node node = adjList[cur.to] ; node!=null ; node = node.next) {
-					pq.add(node);
-				}
+			if (visited[cur.to])
+				continue;
+			visited[cur.to] = true;
+			result += cur.val;
+			cnt++;
+			for (Node node = adjList[cur.to]; node != null; node = node.next) {
+				pq.add(node);
+
 			}
 		}
-	}
-
-	private static void makeSet() {
-		group = new int[V+1];
-		for(int i = 1 ; i <= V ; i++) {
-			group[i] = i;
-		}
-	}
-
-	private static int find(int a) {
-		if(group[a]==a) return a;
-		return group[a]=find(group[a]);
-	}
-
-	private static boolean union(int a, int b) {
-		int rootA = find(a);
-		int rootB = find(b);
-		
-		if(rootA==rootB) return false;
-		
-		group[rootA] = rootB;
-		return true;
 	}
 }
