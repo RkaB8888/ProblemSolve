56bfe21199c5bf055faa396dcaeab9a847375e00
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-10-21 17:37:52 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 448 ms, Memory: 165616 KB -BaekjoonHub

--- Java/백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
+++ Java/백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -1,51 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
-
-/*
- * 메모리 ? KB 시간 ? ms
- * 한번 정렬 후 탐색
+/**
+ * 메모리 ? KB 시간 ? ms 다시 풀어봄
+ * 메모이제이션
+ * @author python98
  */
 public class Main {
-
-	static int N;
-	static int arr[];
-	static List<Integer> list = new ArrayList<>();
-
-	public static void main(String[] args) throws IOException {
-
+	static int N, arr[];
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-
 		N = Integer.parseInt(br.readLine());
 		arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-
-		list.add(Integer.MIN_VALUE);
-		for (int i = 0; i < N; i++) {
-			if (arr[i] > list.get(list.size() - 1)) {
-				list.add(arr[i]);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr[0] = Integer.parseInt(st.nextToken());
+		int cnt = 1;
+		for(int i = 1 ; i < N ; i++) {
+			int nextNum = Integer.parseInt(st.nextToken());
+			if(arr[cnt-1]<nextNum) {
+				arr[cnt++] = nextNum;
 			} else {
-				int left = 1, right = list.size() - 1;
-				while (left < right) {
-					int mid = (left + right) >> 1;
-					if (list.get(mid) >= arr[i]) {
-						right = mid;
-					}
-					else {
-						left = mid+1;
-					}
-				}
-				list.set(right, arr[i]);
+				int idx = bSearch(0,cnt,nextNum);
+				arr[idx] = nextNum;
 			}
 		}
-		System.out.print(list.size()-1);
+		System.out.print(cnt);
+	}
+	private static int bSearch(int str, int end, int nextNum) {
+		while(str<end) {
+			int mid = (str+end)>>1;
+			if(arr[mid]<nextNum) {
+				str = mid+1;
+			} else {
+				end = mid;
+			}
+		}
+		return str;
 	}
 
 }
