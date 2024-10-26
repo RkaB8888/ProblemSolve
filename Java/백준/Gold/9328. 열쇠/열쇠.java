8f103b2b8559c5b248dab1a9fe17269dde97e318
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-10-27 00:16:23 +0900

    [Gold I] Title: 열쇠, Time: 148 ms, Memory: 18880 KB -BaekjoonHub

--- Java/백준/Gold/9328. 열쇠/열쇠.java
+++ Java/백준/Gold/9328. 열쇠/열쇠.java
@@ -1,160 +1,128 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
-/**
- * FloodFill DFS 메모리 ? KB 시간 ? ms
- * 
- * @author python98
- */
 public class Main {
-	static final int DIFF = 'a' - 'A';
-	static BufferedReader br;
-	static StringTokenizer st;
-	static int R, C, result, drdc[][] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-	static char map[][];
-	static int searchMap[][];
-	static List<Door> doors[];
-	static boolean usedKey[], getKey[];
+    static final int DIFF = 'a' - 'A';
+    static int R, C, result;
+    static char[][] map;
+    static boolean usedKey[], visited[][];
+    static List<Door>[] doors;
+    static int[][] drdc = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+    static Queue<int[]> queue;
+    static class Door {
+        int r, c;
+        public Door(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
 
-	static class Door {
-		int r, c;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		public Door(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
+        int T = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
 
-		@Override
-		public String toString() {
-			return "(r=" + r + ", c=" + c + ")";
-		}
+        while (T-- > 0) {
+            input(br);
+            bfs();
+            sb.append(result).append('\n');
+        }
+        System.out.print(sb);
+    }
 
-	}
+    private static void input(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken()) + 2;
+        C = Integer.parseInt(st.nextToken()) + 2;
 
-	public static void main(String[] args) throws IOException {
-		StringBuilder sb = new StringBuilder();
-		br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			input();
-			int cnt = 1;
-			while (floodFill(0, 0, cnt)) {
-				//탐색하면서 키를 먹었다면 해당 문을 열고 재탐색
-				for(int i = 0 ; i < 26 ; i++) {
-					if(getKey[i]) {
-						getKey[i] = false;
-						openDoors(i);
-						usedKey[i] = true;
-					}
-				}
-				cnt++;
-			}
-			sb.append(result).append('\n');
-		}
-		System.out.print(sb);
-	}
+        map = new char[R][C];
+        visited = new boolean[R][C];
+        doors = new List[26]; // 각 문을 저장하는 리스트
+        for (int i = 0; i < 26; i++) doors[i] = new ArrayList<>();
+        usedKey = new boolean[26];
+        queue = new ArrayDeque<>();
+        result = 0;
 
-	// 탐색 도중 새로운 키를 먹으면 true
-	private static boolean floodFill(int r, int c, int cnt) {
-		boolean flag = false;
+        // 외곽을 빈 공간으로 초기화
+        Arrays.fill(map[0], '.');
+        Arrays.fill(map[R - 1], '.');
 
-		if (map[r][c] == '*'||searchMap[r][c] == cnt)
-			return false;
-		else if (map[r][c] >= 'A' && map[r][c] <= 'Z') { // 열 수 없는 문
-			return false;
-		} else if (map[r][c] >= 'a' && map[r][c] <= 'z') {
-			if (usedKey[map[r][c] - 'a'] == false) { // 사용해본 적 없는 키
-				getKey[map[r][c] - 'a'] = true;
-				flag = true;
-			} else { // 사용한 적 있는 키
-				map[r][c] = '.';
-			}
-		} else if (map[r][c]=='$') { // 문서 획득
-			result++;
-			map[r][c] = '.';
-		}
+        for (int i = 1; i < R - 1; i++) {
+            char[] row = br.readLine().toCharArray();
+            map[i][0] = map[i][C - 1] = '.';
+            System.arraycopy(row, 0, map[i], 1, row.length);
 
-		searchMap[r][c] = cnt;
-		for (int i = 0; i < 4; i++) {
-			int nr = r + drdc[i][0], nc = c + drdc[i][1];
-			if (nr < 0 || nr >= R || nc < 0 || nc >= C)
-				continue;
-			flag = floodFill(nr, nc, cnt) ? true : flag;
-		}
+            for (int j = 1; j < C - 1; j++) {
+                if (map[i][j] >= 'A' && map[i][j] <= 'Z') {
+                    doors[map[i][j] - 'A'].add(new Door(i, j)); // 문 위치 저장
+                }
+            }
+        }
 
-		return flag;
-	}
+        char[] initialKeys = br.readLine().toCharArray();
+        if (initialKeys[0] != '0') {
+            for (char key : initialKeys) {
+                usedKey[key - 'a'] = true; // 초기 키 저장
+                openDoors(key - 'a'); // 초기 키로 열 수 있는 문 열기
+            }
+        }
+    }
 
-	public static void openDoors(int doorCode) {
-		for (Door door : doors[doorCode]) {
-			map[door.r][door.c] = '.';
-		}
-	}
+    private static void bfs() {
+        queue.add(new int[] { 0, 0 }); // 시작점
+        visited[0][0] = true;
 
-	public static void input() throws IOException {
-		st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken()) + 2;
-		C = Integer.parseInt(st.nextToken()) + 2;
-		result = 0;
-		map = new char[R][C];
-		searchMap = new int[R][C];
-		doors = new List[26];
-		for (int i = 0; i < 26; i++) {
-			doors[i] = new ArrayList<Door>();
-		}
-		usedKey = new boolean[26];
-		getKey = new boolean[26];
-		Arrays.fill(map[0], '.');
-		Arrays.fill(map[R - 1], '.');
-		for (int i = 1, iEnd = R - 2; i <= iEnd; i++) {
-			char[] temp = br.readLine().toCharArray();
-			map[i][0] = '.';
-			map[i][C - 1] = '.';
-			System.arraycopy(temp, 0, map[i], 1, temp.length);
-			for (int j = 1, jEnd = C - 2; j <= jEnd; j++) {
-				if (map[i][j] >= 'A' && map[i][j] <= 'Z') {
-					doors[map[i][j] - 'A'].add(new Door(i, j));
-				}
-			}
-		}
-		char[] initKey = br.readLine().toCharArray();
-		if (initKey[0] == '0')
-			return;
-		for (char c : initKey) {
-			usedKey[c - 'a'] = true;
-			openDoors(c - DIFF-'A');
-		}
-	}
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int r = cur[0], c = cur[1];
 
-//	static void output() {
-//		System.out.println("지도 정보");
-//		for (char[] row : map) {
-//			for (char c : row) {
-//				System.out.print(c);
-//			}
-//			System.out.println();
-//		}
-//		System.out.println("문 정보");
-//		for (int i = 0; i < 26; i++) {
-//			System.out.print((char) (i + 'A') + ":");
-//			System.out.println(doors[i].toString());
-//		}
-//		System.out.println("키 정보");
-//		System.out.print("사용된 키 : ");
-//		for (int i = 0; i < 26; i++) {
-//			if(usedKey[i]) System.out.print((char) (i + 'a') + ", ");
-//		}
-//		System.out.println();
-//		System.out.print("획득한 키 : ");
-//		for (int i = 0; i < 26; i++) {
-//			if(getKey[i]) System.out.print((char) (i + 'a') + ",");
-//		}
-//		System.out.println();
-//		System.out.println("///////////////");
-//	}
+            for (int[] d : drdc) {
+                int nr = r + d[0], nc = c + d[1];
+                if (nr < 0 || nr >= R || nc < 0 || nc >= C || visited[nr][nc] || map[nr][nc] == '*') 
+                    continue;
+
+                char cell = map[nr][nc];
+                visited[nr][nc] = true;
+
+                if (cell == '$') {
+                    result++; // 문서 획득
+                    map[nr][nc] = '.';
+                } else if (cell >= 'a' && cell <= 'z') { // 키를 발견한 경우
+                    int key = cell - 'a';
+                    if (!usedKey[key]) { // 새 키라면
+                        usedKey[key] = true;
+                        openDoors(key); // 해당 키로 열 수 있는 문 열기
+                    }
+                    map[nr][nc] = '.';
+                } else if (cell >= 'A' && cell <= 'Z') { // 문을 만난 경우
+                    int door = cell - 'A';
+                    if (!usedKey[door]) continue; // 열쇠가 없으면 무시
+                    map[nr][nc] = '.';
+                }
+                queue.add(new int[] { nr, nc });
+            }
+        }
+    }
+
+    private static void openDoors(int key) {
+        for (Door door : doors[key]) {
+            map[door.r][door.c] = '.';
+            if(check(door.r, door.c)) {
+            	queue.add(new int[] {door.r, door.c});
+            }
+        }
+    }
+    
+    private static boolean check(int r, int c) {
+    	for(int i = 0 ; i < 4 ; i++) {
+    		int nr = r + drdc[i][0], nc = c + drdc[i][1];
+    		if(nr<0||nc<0||nr>=R||nc>=C) continue;
+    		if(visited[nr][nc]) return true;
+    	}
+    	return false;
+    }
 }
