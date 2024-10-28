d8252989024a83b6853df6cb6913116270a50d82
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-10-29 01:54:57 +0900

    [Bronze V] Title: 아스키 코드, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 메모리 ? KB 시간 ? ms
+ * 
+ * @author python98
+ */
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print((int)br.readLine().charAt(0));
+	}
+
+}
