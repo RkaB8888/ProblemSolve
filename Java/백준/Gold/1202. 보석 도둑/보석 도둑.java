893cc7e2309549c67fc8899012e702173608d6c8
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-10-31 08:58:01 +0900

    [Gold II] Title: 보석 도둑, Time: 1580 ms, Memory: 131952 KB -BaekjoonHub

--- Java/백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ Java/백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -2,127 +2,57 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+/**
+ * 메모리 ? KB 시간 ? ms 1.보석을 무게 오름차순으로 정렬 2.가방을 무게 오름차순으로 정렬 3.PQ 생성 (가치를 내림차순으로
+ * 정렬) 4.가장 작은 가방의 무게보다 보석의 무게가 더 작으면 PQ에 넣음 5.가방의 무게보다 보석의 무게가 더 크면 이전까지 PQ에
+ * 담았던 보석 중에서 가장 큰 가치를 저장 6. 다음 가방으로 4번부터 반복
+ * 
+ * @author python98
+ */
 public class Main {
-    static final int inf = Integer.MAX_VALUE;
-    static int N, K;
-    static long result;
-    static int[][] jewel;
-    static int[] bag;
-    static int[][] seg;
-    static int segSize;
+	static int N, K;
+	static long result;
+	static int[][] jewel;
+	static int[] bag;
+	static PriorityQueue<Integer> pq;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 
-        jewel = new int[N][2];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            jewel[i][0] = Integer.parseInt(st.nextToken());
-            jewel[i][1] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(jewel, (a, b) -> a[0] - b[0]);
+		jewel = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			jewel[i][0] = Integer.parseInt(st.nextToken());
+			jewel[i][1] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(jewel, (a, b) -> a[0] - b[0]);
 
-        bag = new int[K];
+		bag = new int[K];
+		for (int i = 0; i < K; i++) {
+			bag[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(bag);
+
+		pq = new PriorityQueue<>((a, b) -> b-a);
+
+		int j = 0;
         for (int i = 0; i < K; i++) {
-            bag[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(bag);
+            while (j < N && jewel[j][0] <= bag[i]) {
+                pq.offer(jewel[j][1]);
+                j++;
+            }
 
-        segSize = 1;
-        while (segSize < N) segSize *= 2;
-        segSize *= 2;
-        seg = new int[segSize][3];
-        setSeg(0, 0, N - 1);
-
-        for (int i = 0; i < K; i++) {
-            int len = bSearch(bag[i], 0, N);
-            int[] getJewel = findSeg(0, 0, len, 0, N - 1);
-            if (getJewel[1] > 0) {
-                result += getJewel[1];
-                updateSeg(0, 0, N - 1, getJewel[2]);
+            if (!pq.isEmpty()) {
+                result += pq.poll();
             }
         }
 
-        System.out.print(result);
-    }
-
-    private static void updateSeg(int idx, int segStr, int segEnd, int target) {
-        if (segStr == segEnd) {
-            seg[idx] = new int[] { 0, 0, -1 };
-            return;
-        }
-
-        int mid = (segStr + segEnd) >> 1;
-        if (target <= mid) {
-            updateSeg(2 * idx + 1, segStr, mid, target);
-        } else {
-            updateSeg(2 * idx + 2, mid + 1, segEnd, target);
-        }
-
-        int[] left = seg[2 * idx + 1];
-        int[] right = seg[2 * idx + 2];
-        if (left[1] > right[1]) {
-            seg[idx] = left;
-        } else if (left[1] < right[1]) {
-            seg[idx] = right;
-        } else {
-            seg[idx] = left[0] > right[0] ? left : right;
-        }
-    }
-
-    private static int[] setSeg(int idx, int str, int end) {
-        if (str == end) {
-            return seg[idx] = new int[] { jewel[str][0], jewel[str][1], str };
-        }
-
-        int mid = (str + end) >> 1;
-        int[] left = setSeg(2 * idx + 1, str, mid);
-        int[] right = setSeg(2 * idx + 2, mid + 1, end);
-
-        if (left[1] > right[1]) {
-            return seg[idx] = left;
-        } else if (left[1] < right[1]) {
-            return seg[idx] = right;
-        } else {
-            return seg[idx] = (left[0] > right[0]) ? left : right;
-        }
-    }
-
-    private static int bSearch(int w, int str, int end) {
-        while (str < end) {
-            int mid = (str + end) >> 1;
-            if (jewel[mid][0] <= w) {
-                str = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-        return str - 1;
-    }
-
-    private static int[] findSeg(int idx, int qStr, int qEnd, int segStr, int segEnd) {
-        if (qEnd < segStr || qStr > segEnd) {
-            return new int[] { 0, 0, -1 };
-        }
-        if (qStr <= segStr && qEnd >= segEnd) {
-            return seg[idx];
-        }
-
-        int mid = (segStr + segEnd) >> 1;
-        int[] left = findSeg(2 * idx + 1, qStr, qEnd, segStr, mid);
-        int[] right = findSeg(2 * idx + 2, qStr, qEnd, mid + 1, segEnd);
-
-        if (left[1] > right[1]) {
-            return left;
-        } else if (left[1] < right[1]) {
-            return right;
-        } else {
-            return (left[0] > right[0]) ? left : right;
-        }
-    }
+		System.out.print(result);
+	}
 }
