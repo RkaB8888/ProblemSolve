b538e584d5289e91f3539a57681cc8111111b198
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-11-13 12:09:37 +0900

    [Silver I] Title: RGB거리, Time: 72 ms, Memory: 12036 KB -BaekjoonHub

--- Java/백준/Silver/1149. RGB거리/RGB거리.java
+++ Java/백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- * DP ? KB 시간 ? ms
+ * DP 11,932 KB 시간 76 ms
  */
 public class Main {
     static int N;
@@ -22,20 +21,13 @@ public class Main {
         DP[0] = Integer.parseInt(st.nextToken());
         DP[1] = Integer.parseInt(st.nextToken());
         DP[2] = Integer.parseInt(st.nextToken());
-        int r, g, b;
         for(int i = 1 ; i < N ; i++) {
-//        	System.out.println("DP : "+Arrays.toString(DP));
         	st = new StringTokenizer(br.readLine());
-        	r=Integer.parseInt(st.nextToken());
-        	g=Integer.parseInt(st.nextToken());
-        	b=Integer.parseInt(st.nextToken());
         	System.arraycopy(DP, 0, preDP, 0, 3);
-//        	System.out.println("preDP : "+Arrays.toString(preDP));
-        	DP[0] = Math.min(preDP[1], preDP[2])+r;
-        	DP[1] = Math.min(preDP[0], preDP[2])+g;
-        	DP[2] = Math.min(preDP[1], preDP[0])+b;
+        	DP[0] = Math.min(preDP[1], preDP[2])+Integer.parseInt(st.nextToken());
+        	DP[1] = Math.min(preDP[0], preDP[2])+Integer.parseInt(st.nextToken());
+        	DP[2] = Math.min(preDP[1], preDP[0])+Integer.parseInt(st.nextToken());
         }
-//        System.out.println("DP : "+Arrays.toString(DP));
         System.out.print(Math.min(DP[0],Math.min(DP[1], DP[2])));
     }
 }
