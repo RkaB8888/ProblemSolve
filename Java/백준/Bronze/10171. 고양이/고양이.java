8890f8fc8d06c08dafca65e5d1230d6f59bea868
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-11-15 14:58:59 +0900

    [Bronze V] Title: 고양이, Time: 60 ms, Memory: 11372 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * 구현 메모리 ? KB 시간 ? ms
+ *
+ */
+public class Main {
+
+	public static void main(String[] args){
+		StringBuilder sb = new StringBuilder();
+		sb.append("\\    /\\").append('\n').append(" )  ( ')").append('\n').append("(  /  )").append('\n').append(" \\(__)|");
+		System.out.print(sb);	
+	}
+}
