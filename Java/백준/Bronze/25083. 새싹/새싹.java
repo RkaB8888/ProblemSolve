fe1214d2788443bd3a9905ea9f3ed3273bbc32db
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-11-18 08:30:33 +0900

    [Bronze V] Title: 새싹, Time: 60 ms, Memory: 11368 KB -BaekjoonHub

---
+++ Java/백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 입출력 메모리 ? KB 시간 ? ms
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("         ,r'\"7").append('\n').append("r`-_   ,'  ,/").append('\n').append(" \\. \". L_r'")
+				.append('\n').append("   `~\\/").append('\n').append("      |").append('\n').append("      |");
+		System.out.print(sb);
+	}
+}
