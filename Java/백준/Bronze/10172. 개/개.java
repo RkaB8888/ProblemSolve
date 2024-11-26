762b0856ec219ff2de78b1647212b580df5144c5
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-11-26 17:38:59 +0900

    [Bronze V] Title: 개, Time: 64 ms, Memory: 11204 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 입출력 메모리 ? KB 시간 ? ms
+ *
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("|\\_/|").append('\n').append("|q p|   /}").append('\n').append("( 0 )\"\"\"\\").append('\n').append("|\"^\"`    |").append('\n').append("||_/=\\\\__|");
+		System.out.print(sb);
+		
+	}
+}
