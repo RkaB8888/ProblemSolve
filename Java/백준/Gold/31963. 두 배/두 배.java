74ac7b2a8e8486b341af220365ff9da699cd8e7f
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-12-02 00:09:28 +0900

    [Gold IV] Title: 두 배, Time: 292 ms, Memory: 43296 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Gold/31963. 두 배/두 배.java
+++ Java/백준/Gold/31963. 두 배/두 배.java
@@ -2,31 +2,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
 /**
  * 
- * 그리디 메모리 ? KB 시간 ? ms
+ * 그리디 메모리 37,664 KB 시간 256 ms
+ * 
  * @author python98
  *
  */
 public class Main {
-	static int N, result;
-	
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        double preVal = 0;
-        for(int i = 0 ; i < N ; i++) {
-        	double curVal = Math.log(Integer.parseInt(st.nextToken()))/Math.log(2);
-        	if(preVal<=curVal) {
-        		preVal = curVal;
-        	} else {
-        		double diff = Math.ceil(preVal - curVal);
-        		result+=diff;
-        		preVal = curVal+diff;
-        	}
-        }
-        System.out.print(result);
-    }
+	static int N;
+	static long result;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int[] arr = new int[N];
+		int[] multiplyCount = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		multiplyCount[0] = 0;
+
+		for (int i = 1; i < N; i++) {
+			int neededMultiplier = (int) Math.ceil(Math.log((double) arr[i - 1] / arr[i]) / Math.log(2));
+			multiplyCount[i] = Math.max(neededMultiplier + multiplyCount[i - 1], 0);
+
+			// 결과에 곱셈 횟수를 누적
+			result += multiplyCount[i];
+		}
+
+		System.out.print(result);
+	}
 
 }
