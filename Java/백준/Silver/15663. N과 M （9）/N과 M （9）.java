ea9725ad7cf2d2d49d1cff2ada5de6592fb9d3c3
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-12-03 03:01:12 +0900

    [Silver II] Title: N과 M (9), Time: 116 ms, Memory: 17280 KB -BaekjoonHub

--- Java/백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ Java/백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -2,61 +2,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 /**
- * 
- * 순열 메모리 ? KB 시간 ? ms
- * 
- * @author python98
+ * 최적화된 순열 생성 코드 메모리 및 시간 절약
+ * 중복 체크 방식을 개선하여 메모리 사용을 줄이고 효율성을 높임
  *
+ * @author python98
  */
 public class Main {
-	static int N, M, arr1[], arr2[];
-	static Set<String> set;
-	static boolean[] selected;	
-	static StringBuilder sb1;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr1 = new int[N];
-		arr2 = new int[M];
-		set = new HashSet<>();
-		selected = new boolean[N];
-		sb1 = new StringBuilder();
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0 ; i < N ; i++) {
-			arr1[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr1);
-		process(0);
-		System.out.print(sb1);
-	}
-	
-	private static void process(int depth) {
-		if(depth == M) {
-			StringBuilder sb2 = new StringBuilder();
-			for(int i = 0 ; i < M ; i++) {
-				sb2.append(arr2[i]).append(' ');
-			}
-			if(!set.contains(sb2.toString())) {
-				set.add(sb2.toString());
-				sb1.append(sb2).append('\n');
-			}
-			return;
-		}
-		
-		for(int i = 0 ; i < N ; i++) {
-			if(selected[i]) continue;
-			selected[i] = true;
-			arr2[depth] = arr1[i];
-			process(depth+1);
-			selected[i] = false;
-		}
-	}
+    static int N, M, arr1[];
+    static boolean[] selected;
+    static StringBuilder resultBuilder;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr1 = new int[N];
+        selected = new boolean[N];
+        resultBuilder = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr1);
+        process(0, new int[M]);
+        System.out.print(resultBuilder);
+    }
+
+    private static void process(int depth, int[] arr2) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                resultBuilder.append(arr2[i]).append(' ');
+            }
+            resultBuilder.append('\n');
+            return;
+        }
+
+        int prev = -1;
+        for (int i = 0; i < N; i++) {
+            if (!selected[i] && arr1[i] != prev) {
+                selected[i] = true;
+                arr2[depth] = arr1[i];
+                process(depth + 1, arr2);
+                selected[i] = false;
+                prev = arr1[i];
+            }
+        }
+    }
 }
