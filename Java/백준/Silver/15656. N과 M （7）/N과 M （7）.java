95dd7614da8fae01e678c8d8d4e2710a19faf0ca
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-12-06 02:37:23 +0900

    [Silver III] Title: N과 M (7), Time: 512 ms, Memory: 219984 KB -BaekjoonHub

---
+++ Java/백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+/**
+ * 
+ * ? 메모리 ? KB 시간 ? ms
+ * 
+ * @author python98
+ *
+ */
+public class Main {
+	static int N, M;
+	static int[] arr1, arr2;
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr1 = new int[N];
+		arr2 = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < N ; i++) {
+			arr1[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr1);
+		permutation(0);
+		System.out.print(sb);
+	}
+	private static void permutation(int depth) {
+		if(depth==M) {
+			for(int num : arr2) {
+				sb.append(num).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		for(int i = 0 ; i < N ; i++) {
+			arr2[depth] = arr1[i];
+			permutation(depth+1);
+		}
+	}
+}
