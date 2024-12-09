be1024c9867c7d7cf007270123cce15c6fea8dbd
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-12-10 07:07:35 +0900

    [Platinum V] Title: 배열에서 이동, Time: 372 ms, Memory: 35144 KB -BaekjoonHub

--- Java/백준/Platinum/1981. 배열에서 이동/배열에서 이동.java
+++ Java/백준/Platinum/1981. 배열에서 이동/배열에서 이동.java
@@ -2,82 +2,85 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Queue;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 /**
- * BFS, PQ 메모리 ? KB 시간 ? ms
+ * 투포인터, bfs 메모리 ? KB 시간 ? ms
  * 
  * @author python98
  */
 public class Main {
-	static final int DIR[][] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } }, INF = Integer.MAX_VALUE;
-	static int N, maxVal = 0, minVal = INF, result = INF;
+	static final int DIR[][] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+	static int N, minH, maxH, result = 200;
 	static int[][] map;
-
+	static List<Integer> hList;
+	static boolean[][] visited;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 
 		map = new int[N][N];
+		Set<Integer> heights = new HashSet<>();
+		
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				int val = Integer.parseInt(st.nextToken());
 				map[i][j] = val;
-				minVal = Math.min(minVal, val);
-				maxVal = Math.max(maxVal, val);
+				heights.add(val);
 			}
 		}
 
-		int minDiff = 0, maxDiff = maxVal - minVal;
-		while (minDiff <= maxDiff) {
-			int midDiff = (minDiff + maxDiff) >> 1;
-//			System.out.println("돌아감0 midDiff:"+midDiff);
-			if (isPosible(midDiff)) {
-				result = midDiff;
-				maxDiff = midDiff - 1;
+		hList = new ArrayList<>(heights);
+		hList.sort(Integer::compareTo);
+		
+		int lowIdx = 0, highIdx = 0;
+		int temp = Math.max(map[0][0],map[N-1][N-1]);
+		while (hList.get(highIdx) < temp) highIdx++;
+		
+		int highLimit = hList.size(), lowLimit = 0;
+		
+		temp = Math.min(map[0][0],map[N-1][N-1]);
+		while (hList.get(lowLimit) <= temp) lowLimit++;
+		
+		visited = new boolean[N][N];
+		while (lowIdx < lowLimit && highIdx < highLimit) {
+			minH = hList.get(lowIdx);
+			maxH = hList.get(highIdx);
+			if (isPosible()) {
+				result = Math.min(result, maxH - minH);
+				lowIdx++;
 			} else {
-				minDiff = midDiff + 1;
+				highIdx++;
 			}
 		}
 		System.out.print(result);
 	}
 
-	// diff 이하의 차이로 도달할 수 있는지 확인
-	private static boolean isPosible(int diff) {
-		for (int i = minVal; i <= maxVal - diff; i++) {
-//			System.out.println("돌아감1");
-			if (bfs(i, i + diff)) {
-//				System.out.println("돌아감2");
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private static boolean bfs(int min, int max) {
-		if (map[0][0] < min || max < map[0][0])
-			return false;
+	private static boolean isPosible() {
 		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] { 0, 0 });
-		boolean[][] visited = new boolean[N][N];
+		q.add(new int[] {0,0});
+		for(int i = 0 ; i < N ; i++) Arrays.fill(visited[i], false);
 		visited[0][0] = true;
-		while (!q.isEmpty()) {
-//			System.out.println("돌아감3");
+		
+		while(!q.isEmpty()) {
 			int[] cur = q.poll();
 			int row = cur[0], col = cur[1];
-			if (row == N - 1 && col == N - 1)
-				return true;
-			for (int i = 0; i < 4; i++) {
-				int nr = row + DIR[i][0];
-				int nc = col + DIR[i][1];
-				if (nr < 0 || nc < 0 || nr >= N || nc >= N || visited[nr][nc])
-					continue;
+			
+			for(int i = 0 ; i < 4 ; i++) {
+				int nr = row + DIR[i][0], nc = col + DIR[i][1];
+				if(nr<0||nc<0||nr>=N||nc>=N||visited[nr][nc]) continue;
+				if(nr==N-1&&nc==N-1) return true;
 				visited[nr][nc] = true;
-				if (map[nr][nc] < min || map[nr][nc] > max)
-					continue;
-				q.add(new int[] { nr, nc });
+				if(map[nr][nc]<minH||map[nr][nc]>maxH) continue;
+				q.add(new int[] {nr,nc});
 			}
 		}
 		return false;
