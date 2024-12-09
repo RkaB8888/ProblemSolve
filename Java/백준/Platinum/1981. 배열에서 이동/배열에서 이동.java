985a1ba6b295500da7b5d0bd88a557fe20c5a795
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-12-10 07:16:19 +0900

    [Platinum V] Title: 배열에서 이동, Time: 256 ms, Memory: 20716 KB -BaekjoonHub

--- Java/백준/Platinum/1981. 배열에서 이동/배열에서 이동.java
+++ Java/백준/Platinum/1981. 배열에서 이동/배열에서 이동.java
@@ -1,17 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Queue;
 import java.util.Set;
 import java.util.StringTokenizer;
 
 /**
- * 투포인터, bfs 메모리 ? KB 시간 ? ms
+ * 투포인터, DFS 메모리 ? KB 시간 ? ms
  * 
  * @author python98
  */
@@ -21,14 +18,14 @@ public class Main {
 	static int[][] map;
 	static List<Integer> hList;
 	static boolean[][] visited;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 
 		map = new int[N][N];
 		Set<Integer> heights = new HashSet<>();
-		
+
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
@@ -39,22 +36,25 @@ public class Main {
 		}
 
 		hList = new ArrayList<>(heights);
-		hList.sort(Integer::compareTo);
-		
+		hList.sort((a,b)->a-b);
+
 		int lowIdx = 0, highIdx = 0;
-		int temp = Math.max(map[0][0],map[N-1][N-1]);
-		while (hList.get(highIdx) < temp) highIdx++;
-		
+		int temp = Math.max(map[0][0], map[N - 1][N - 1]);
+		while (hList.get(highIdx) < temp)
+			highIdx++;
+
 		int highLimit = hList.size(), lowLimit = 0;
+
+		temp = Math.min(map[0][0], map[N - 1][N - 1]);
+		while (hList.get(lowLimit) <= temp)
+			lowLimit++;
+
 		
-		temp = Math.min(map[0][0],map[N-1][N-1]);
-		while (hList.get(lowLimit) <= temp) lowLimit++;
-		
-		visited = new boolean[N][N];
 		while (lowIdx < lowLimit && highIdx < highLimit) {
 			minH = hList.get(lowIdx);
 			maxH = hList.get(highIdx);
-			if (isPosible()) {
+			visited = new boolean[N][N];
+			if (isPosible(0,0)) {
 				result = Math.min(result, maxH - minH);
 				lowIdx++;
 			} else {
@@ -64,24 +64,16 @@ public class Main {
 		System.out.print(result);
 	}
 
-	private static boolean isPosible() {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] {0,0});
-		for(int i = 0 ; i < N ; i++) Arrays.fill(visited[i], false);
-		visited[0][0] = true;
-		
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			int row = cur[0], col = cur[1];
-			
-			for(int i = 0 ; i < 4 ; i++) {
-				int nr = row + DIR[i][0], nc = col + DIR[i][1];
-				if(nr<0||nc<0||nr>=N||nc>=N||visited[nr][nc]) continue;
-				if(nr==N-1&&nc==N-1) return true;
-				visited[nr][nc] = true;
-				if(map[nr][nc]<minH||map[nr][nc]>maxH) continue;
-				q.add(new int[] {nr,nc});
-			}
+	private static boolean isPosible(int row, int col) {
+		if(row==N-1&&col==N-1) {
+			return true;
+		}
+		for (int i = 0; i < 4; i++) {
+			int nr = row + DIR[i][0], nc = col + DIR[i][1];
+			if (nr < 0 || nc < 0 || nr >= N || nc >= N || visited[nr][nc]) continue;
+			visited[nr][nc] = true;
+			if (map[nr][nc] < minH || map[nr][nc] > maxH) continue;
+			if(isPosible(nr,nc)) return true;
 		}
 		return false;
 	}
