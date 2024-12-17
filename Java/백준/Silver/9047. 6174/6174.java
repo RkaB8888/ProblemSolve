b2de19910ebef6118df4eb4672aabb59aa8c50e4
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-12-17 10:38:08 +0900

    [Silver V] Title: 6174, Time: 64 ms, Memory: 11556 KB -BaekjoonHub

--- Java/백준/Silver/9047. 6174/6174.java
+++ Java/백준/Silver/9047. 6174/6174.java
@@ -1,44 +1,49 @@
 import java.io.*;
-import java.util.Arrays;
 /**
- * 구현 메모리 ? KB 시간 ? ms
+ * 구현 메모리 12,348 KB 시간 72 ms
  * 
  * @author python98
  */
 public class Main {
-	static char[] numC;
-	static int[] num;
+	static final int GOAL = 6174; // 목표 값
 	static int result;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc = 1 ; tc <= T ; tc++) {
-			numC = br.readLine().toCharArray();
-			num = new int[4];
-			for(int i = 0 ; i < 4 ; i++) {
-				num[i] = numC[i]-'0';
-			}
+
+		for (int tc = 1; tc <= T; tc++) {
+			int num = Integer.parseInt(br.readLine()); // 숫자를 정수로 읽음
 			result = 0;
-			while(num[0]!=6||num[1]!=1||num[2]!=7||num[3]!=4) {
-				Arrays.sort(num);
-				int large = 0, small = 0, unit = 1;
-				for(int i = 0 ; i < 4 ; i++) {
-					small*=10;
-					small+=num[i];
-					large += unit*num[i];
-					unit*=10;
-				}
-				int temp = large - small;
-				for(int i = 3 ; i >= 0 ; i--) {
-					num[i] = temp%10;
-					temp/=10;
-				}
+
+			while (num != GOAL) {
+				num = process(num);
 				result++;
 			}
+
 			sb.append(result).append('\n');
 		}
 		System.out.print(sb);
+	}
+
+	private static int process(int num) {
+		// 카운팅 정렬
+		int[] count = new int[10];
+		for (int i = 0; i < 4; i++) {
+			count[num % 10]++;
+			num /= 10;
+		}
+
+		int large = 0, small = 0, multiplier = 1;
+		for (int i = 9; i >= 0; i--) {
+			while (count[i]-- > 0) {
+				large = large * 10 + i;
+				small += i * multiplier;
+				multiplier *= 10;
+			}
+		}
+
+		return large - small;
 	}
 }
