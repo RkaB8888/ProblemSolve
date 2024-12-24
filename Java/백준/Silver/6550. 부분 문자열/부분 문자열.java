75cdde0df0af94791bb200b683a25ab189bea428
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2024-12-24 09:48:05 +0900

    [Silver V] Title: 부분 문자열, Time: 96 ms, Memory: 15444 KB -BaekjoonHub

--- Java/백준/Silver/6550. 부분 문자열/부분 문자열.java
+++ Java/백준/Silver/6550. 부분 문자열/부분 문자열.java
@@ -1,37 +1,31 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 문자열 메모리 15,468 KB 시간 100 ms 하나씩 비교
- * 
- * @author python98
- */
 public class Main {
-	static int N, K, i, j, iEnd, jEnd;
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	static String line;
-	static char[] s, t;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while ((line = br.readLine())!=null) {
-			st = new StringTokenizer(line);
-			s = st.nextToken().toCharArray();
-			t = st.nextToken().toCharArray();
-			i = 0; j = 0; iEnd = s.length; jEnd = t.length - iEnd + 1;
-			while (i < iEnd && j < jEnd) {
-				if (s[i] == t[j]) {
-					i++;
-					jEnd++;
-				}
-					j++;
-			}
-			if(i==iEnd) {
-				sb.append("Yes\n");
-			} else {
-				sb.append("No\n");
-			}
-		}
-		System.out.print(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line);
+            char[] s = st.nextToken().toCharArray();
+            char[] t = st.nextToken().toCharArray();
+
+            int i = 0, j = 0;
+            while (i < s.length && j < t.length) {
+                if (s[i] == t[j]) {
+                    i++;
+                }
+                j++;
+            }
+
+            if (i == s.length) {
+                sb.append("Yes\n");
+            } else {
+                sb.append("No\n");
+            }
+        }
+        System.out.print(sb);
+    }
 }
