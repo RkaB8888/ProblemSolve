69b7003d17536de9ce11ddb90ff2ca815e5a8aba
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-01-02 06:48:32 +0900

    [Silver IV] Title: The Game of Death, Time: 124 ms, Memory: 17284 KB -BaekjoonHub

--- Java/백준/Silver/11558. The Game of Death/The Game of Death.java
+++ Java/백준/Silver/11558. The Game of Death/The Game of Death.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 /**
  * @description
@@ -10,29 +9,28 @@ import java.util.*;
 public class Main {
 	static int T, N;
 	static int[] target;
-	static boolean[] check;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
 		T = Integer.parseInt(br.readLine());
 		target = new int[10001];
-		check = new boolean[10001];
+		
 		test:for(int t = 1 ; t <= T ; t++) {
 			N = Integer.parseInt(br.readLine());
 			for(int i = 1 ; i <= N ; i++) {
 				target[i] = Integer.parseInt(br.readLine());
 			}
-			int curNum = 1, K = 0;
-			Arrays.fill(check, 1, N+1, false);
-			while(!check[curNum]) {
-				check[curNum] = true;
-				curNum = target[curNum];
-				K++;
+			int curNum = 1, nextNum = 0, K = 0;
+			while((nextNum = target[curNum])!=0) {
 				if(curNum==N) {
 					sb.append(K).append('\n');
 					continue test;
 				}
+				target[curNum] = 0;
+				curNum = nextNum;
+				K++;
+				
 			}
 			sb.append("0\n");
 		}
