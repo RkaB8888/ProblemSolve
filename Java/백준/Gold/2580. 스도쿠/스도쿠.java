7d391e289d3d211a9bcb97d6d3d0cf830d7be2a2
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-01-07 09:55:54 +0900

    [Gold IV] Title: 스도쿠, Time: 216 ms, Memory: 14044 KB -BaekjoonHub

--- Java/백준/Gold/2580. 스도쿠/스도쿠.java
+++ Java/백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,80 +1,93 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * @description 조합, 완전탐색
- * @performance 메모리: ? KB, 동작시간: ? ms
- * @author python98
- */
 public class Main {
-	static int blankCnt;
-	static int[][] map, blanks;
-	static boolean flag;
-	static boolean[][] rowCheck, colCheck;
-	static boolean[][][] nineCheck;
+    static int[][] map;
+    static List<int[]> blanks;
+    static int[] rowCheck, colCheck, nineCheck;
+    static boolean solved;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		map = new int[9][9];
-		rowCheck = new boolean[9][10];
-		colCheck = new boolean[9][10];
-		nineCheck = new boolean[3][3][10];
-		blanks = new int[81][2];
-		StringTokenizer st;
-		for (int i = 0; i < 9; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 9; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if (map[i][j] == 0) {
-					blanks[blankCnt][0] = i;
-					blanks[blankCnt++][1] = j;
-				}
-			}
-		}
-		checkPossible();
-		combination(0);
-		for(int i = 0 ; i < 9 ; i++) {
-			for(int j = 0 ; j < 9 ; j++) {
-				sb.append(map[i][j]).append(" ");
-			}
-			sb.append("\n");
-		}
-		System.out.print(sb);
-	}
+        map = new int[9][9];
+        blanks = new ArrayList<>();
+        rowCheck = new int[9];
+        colCheck = new int[9];
+        nineCheck = new int[9];
 
-	private static void combination(int depth) {
-		if (depth == blankCnt) {
-			flag = true;
-			return;
-		}
-		for (int i = 1; i <= 9; i++) {
-			if (!rowCheck[blanks[depth][0]][i] && !colCheck[blanks[depth][1]][i] && !nineCheck[blanks[depth][0] / 3][blanks[depth][1] / 3][i]) {
-				rowCheck[blanks[depth][0]][i] = true;
-				colCheck[blanks[depth][1]][i] = true;
-				nineCheck[blanks[depth][0] / 3][blanks[depth][1] / 3][i] = true;
-				map[blanks[depth][0]][blanks[depth][1]] = i;
-				combination(depth + 1);
-				if(flag) return;
-				rowCheck[blanks[depth][0]][i] = false;
-				colCheck[blanks[depth][1]][i] = false;
-				nineCheck[blanks[depth][0] / 3][blanks[depth][1] / 3][i] = false;
-			}
-		}
-	}
+        // 입력받기
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 0) {
+                    blanks.add(new int[]{i, j});
+                } else {
+                    int num = map[i][j];
+                    updateState(i, j, num, true); // 초기 상태 업데이트
+                }
+            }
+        }
 
-	private static void checkPossible() {
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				rowCheck[i][map[i][j]] = true;
-				colCheck[j][map[i][j]] = true;
-				nineCheck[i / 3][j / 3][map[i][j]] = true;
-			}
-		}
-	}
+        // 백트래킹 시작
+        solve(0);
+
+        // 출력
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sb.append(map[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static void solve(int depth) {
+        if (solved) return; // 이미 해결된 경우 탐색 중지
+        if (depth == blanks.size()) {
+            solved = true;
+            return;
+        }
+
+        int[] blank = blanks.get(depth);
+        int r = blank[0], c = blank[1];
+        int grid = (r / 3) * 3 + (c / 3);
+
+        // 가능한 숫자 탐색
+        for (int num = 1; num <= 9; num++) {
+            if (isValid(r, c, grid, num)) {
+                updateState(r, c, num, true); // 상태 업데이트
+                map[r][c] = num;
+
+                solve(depth + 1);
+
+                if (solved) return; // 해결된 경우 종료
+                updateState(r, c, num, false); // 상태 되돌리기
+                map[r][c] = 0;
+            }
+        }
+    }
+
+    static boolean isValid(int r, int c, int grid, int num) {
+        int bit = 1 << num;
+        return (rowCheck[r] & bit) == 0 &&
+               (colCheck[c] & bit) == 0 &&
+               (nineCheck[grid] & bit) == 0;
+    }
+
+    static void updateState(int r, int c, int num, boolean set) {
+        int bit = 1 << num;
+        int grid = (r / 3) * 3 + (c / 3);
+        if (set) {
+            rowCheck[r] |= bit;
+            colCheck[c] |= bit;
+            nineCheck[grid] |= bit;
+        } else {
+            rowCheck[r] &= ~bit;
+            colCheck[c] &= ~bit;
+            nineCheck[grid] &= ~bit;
+        }
+    }
 }
