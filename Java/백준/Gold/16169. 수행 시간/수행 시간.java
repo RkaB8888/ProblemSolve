3e669944df0f327c341a06a3077465cb9638c26d
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-01-07 14:26:20 +0900

    [Gold IV] Title: 수행 시간, Time: 64 ms, Memory: 11708 KB -BaekjoonHub

--- Java/백준/Gold/16169. 수행 시간/수행 시간.java
+++ Java/백준/Gold/16169. 수행 시간/수행 시간.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
  * @description 위상정렬
@@ -11,7 +8,7 @@ import java.util.StringTokenizer;
  */
 public class Main {
 	static int N, result;
-	static int[] whatIsProcessTime, whatIsTier, reachTime;
+	static int[] processTime, reachTime;
 	static int[][] transTime;
 	static List[] tiers;
 
@@ -19,48 +16,52 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
-		whatIsProcessTime = new int[N + 1];
-		transTime = new int[N + 1][N + 1];
-		whatIsTier = new int[N + 1];
-		reachTime = new int[N + 1];
+		
+		// 계급 인접리스트 생성
 		tiers = new List[N + 1];
 		for (int i = 0; i <= N; i++) {
 			tiers[i] = new ArrayList<Integer>();
 		}
+		
+		// 전송시간 전처리
+		transTime = new int[N + 1][N + 1];
 		for (int i = 1; i < N; i++) {
 			for (int j = i + 1; j <= N; j++) {
 				transTime[i][j] = transTime[j][i] = (i - j) * (i - j);
 			}
 		}
+		
+		// 입력 처리
+		processTime = new int[N + 1];
 		int lastTier = 1;
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int tier = Integer.parseInt(st.nextToken());
 			int process = Integer.parseInt(st.nextToken());
 			tiers[tier].add(i);
-			whatIsTier[i] = tier;
-			whatIsProcessTime[i] = process;
+			processTime[i] = process;
 			lastTier = Math.max(lastTier, tier);
 		}
 		
-//		for(int i = 1 ; i <= N ; i++) {
-//			System.out.println(tiers[i].toString());
-//		}
-		
+		// 해당 번호까지의 도달 시간 계산
+		reachTime = new int[N + 1];
 		for (int i = 1; i < lastTier; i++) {
 			List<Integer> curTier = tiers[i];
 			List<Integer> nextTier = tiers[i + 1];
 			for (int curNum : curTier) {
 				for (int nextNum : nextTier) {
 					reachTime[nextNum] = Math.max(reachTime[nextNum],
-							reachTime[curNum]+transTime[curNum][nextNum] + whatIsProcessTime[curNum]);
+							reachTime[curNum]+transTime[curNum][nextNum] + processTime[curNum]);
 				}
 			}
 		}
+		
+		// 최상위 계급의 동작 시간까지 계산
 		List<Integer> last = tiers[lastTier];
 		for (int num : last) {
-			result = Math.max(result, reachTime[num]+whatIsProcessTime[num]);
+			result = Math.max(result, reachTime[num]+processTime[num]);
 		}
+		
 		System.out.print(result);
 	}
 }
