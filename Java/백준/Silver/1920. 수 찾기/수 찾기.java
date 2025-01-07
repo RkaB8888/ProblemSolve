85fdaa63d76648fcaee2b5e412897afbf1cb2dbf
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-01-08 08:42:36 +0900

    [Silver IV] Title: 수 찾기, Time: 496 ms, Memory: 60584 KB -BaekjoonHub

--- Java/백준/Silver/1920. 수 찾기/수 찾기.java
+++ Java/백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,47 +2,36 @@ import java.io.*;
 import java.util.*;
 
 /**
- * @description 이분 탐색
- * @performance 메모리: ? KB, 동작시간: ? ms
+ * @description Set
+ * @performance 메모리: 57,100 KB, 동작시간: 500 ms
  * @author python98
  */
 public class Main {
 	static int N, M;
-	static int[] arr;
+	static Set<Integer> set;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
+		set = new HashSet<>();
+
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			set.add(Integer.parseInt(st.nextToken()));
 		}
-		Arrays.sort(arr);
 
 		M = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < M; i++) {
-			sb.append(bSearch(Integer.parseInt(st.nextToken()))).append("\n");
+			if (set.contains(Integer.parseInt(st.nextToken()))) {
+				sb.append(1).append("\n");
+			} else {
+				sb.append(0).append("\n");
+			}
 		}
-
 		System.out.print(sb);
 	}
 
-	private static int bSearch(int target) {
-		int left = 0, right = arr.length - 1;
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			if (arr[mid] == target) {
-				return 1;
-			} else if (arr[mid] < target) {
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
-		return 0;
-	}
 }
