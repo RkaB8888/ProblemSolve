43eecb84921c49bb6dd3b926712c1340e540418e
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-01-07 11:37:31 +0900

    [Bronze V] Title: 홍익대학교, Time: 64 ms, Memory: 11480 KB -BaekjoonHub

---
+++ Java/백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+/**
+ * @description 수학
+ * @performance 메모리: ? KB, 동작시간: ? ms
+ * @author python98
+ */
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print(Integer.parseInt(br.readLine())-1946);
+	}
+}
