19ed0ecbae077c8e912a58b6754e7d7e3ddd85ae
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-01-09 09:24:33 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

--- Java/백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ Java/백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,49 +1,43 @@
 import java.io.*;
 
 /**
- * @description 구현
- * @performance 메모리: 11,520 KB, 동작시간: 68 ms
+ * @description 대규모 문자열에 대한 크로아티아 알파벳 변환
+ * @performance 메모리: ? KB, 동작시간: ? ms
  * @author python98
  */
 public class Main {
-	static int cnt;
-	static char[] input;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		input = br.readLine().toCharArray();
-		int c = 0;
-		cnt = input.length;
-		while (c < input.length - 1) {
-			if (input[c] == 'c' && (input[c + 1] == '=' || input[c + 1] == '-')) {
-				c++;
-				cnt--;
-			} else if (input[c] == 'd') {
-				if (input[c + 1] == '-') {
-					c++;
-					cnt--;
-				} else if (c < input.length - 2 && input[c + 1] == 'z' && input[c + 2] == '=') {
-					c += 2;
-					cnt -= 2;
-				}
-			} else if (input[c] == 'l' && input[c + 1] == 'j') {
-				c++;
-				cnt--;
-			}
-			else if ( input[c] == 'n' && input[c + 1] == 'j') {
-				c++;
-				cnt--;
-			}
-			else if (input[c] == 's' && input[c + 1] == '=') {
-				c++;
-				cnt--;
-			}
-			else if (input[c] == 'z' && input[c + 1] == '=') {
-				c++;
-				cnt--;
-			}
-			c++;
-		}
-		System.out.print(cnt);
-	}
+        // 크로아티아 알파벳 패턴 정의
+        String input = br.readLine();
+        int length = input.length();
+        int count = 0;
+
+        for (int i = 0; i < length; i++) {
+            count++; // 기본적으로 한 글자
+            if (i < length - 1) {
+                // 두 글자 패턴 확인
+                if (input.charAt(i) == 'c' && (input.charAt(i + 1) == '=' || input.charAt(i + 1) == '-')) {
+                    i++;
+                } else if (input.charAt(i) == 'd') {
+                    if (i < length - 2 && input.charAt(i + 1) == 'z' && input.charAt(i + 2) == '=') {
+                        i += 2;
+                    } else if (input.charAt(i + 1) == '-') {
+                        i++;
+                    }
+                } else if (input.charAt(i) == 'l' && input.charAt(i + 1) == 'j') {
+                    i++;
+                } else if (input.charAt(i) == 'n' && input.charAt(i + 1) == 'j') {
+                    i++;
+                } else if (input.charAt(i) == 's' && input.charAt(i + 1) == '=') {
+                    i++;
+                } else if (input.charAt(i) == 'z' && input.charAt(i + 1) == '=') {
+                    i++;
+                }
+            }
+        }
+
+        System.out.print(count);
+    }
 }
