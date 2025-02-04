ea8da271c6517672eba765c2bf5db756c0ab65ab
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-02-04 10:00:05 +0900

    [Gold V] Title: 회문은 회문아니야!!, Time: 128 ms, Memory: 18556 KB -BaekjoonHub

--- Java/백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.java
+++ Java/백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.java
@@ -1,38 +1,37 @@
 import java.io.*;
-import java.util.*;
 
 /**
- * @description 그리디
+ * @description 그리디 최적화
  * @performance 메모리: ? KB, 동작시간: ? ms
  * @author python98
  */
 public class Main {
-	static char[] input, temp;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		input = br.readLine().toCharArray();
-		int len = input.length;
-		temp = new char[2*len+1];
-		Arrays.fill(temp, input[0]);
-		for(int i = 0 ; i < len ; i++) {
-			temp[2*i+1] = input[i]; 
-		}
-		boolean isPal = true;
-		boolean isAllSame = true;
-		for(int i = 0 ; i <= len ; i++) {
-			if(temp[i]!=temp[len+len-i]) {
-				isPal = false;
-				isAllSame = false;
-			} else if(temp[i]!=input[0]) {
-				isAllSame = false;
-			}
-		}
-		if(isAllSame) {
-			System.out.print(-1);
-		} else if(isPal) {
-			System.out.print(len-1);
-		} else {
-			System.out.print(len);
-		}
-	}
+    static char[] input;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        input = br.readLine().toCharArray();
+        int len = input.length;
+
+        boolean isPal = true;
+        boolean isAllSame = true;
+
+        for (int i = 0; i <= len / 2; i++) {
+            if (input[i] != input[len - 1 - i]) {
+                isPal = false;
+                isAllSame = false;
+                break;
+            } else if (input[i] != input[0]) {
+                isAllSame = false;
+            }
+        }
+
+        if (isAllSame) {
+            System.out.print(-1);
+        } else if (isPal) {
+            System.out.print(len - 1);
+        } else {
+            System.out.print(len);
+        }
+    }
 }
