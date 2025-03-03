c85701f481785ac5c7baca00c5aa36baa733974f
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-03-03 19:26:44 +0900

    [Platinum I] Title: 빅 픽쳐, Time: 360 ms, Memory: 57236 KB -BaekjoonHub

--- Java/백준/Platinum/10538. 빅 픽쳐/빅 픽쳐.java
+++ Java/백준/Platinum/10538. 빅 픽쳐/빅 픽쳐.java
@@ -2,22 +2,121 @@ import java.io.*;
 import java.util.*;
 
 /**
- * @description 라빈카프
+ * @description 아호코라식
  * @performance 메모리: ? KB, 동작시간: ? ms
  * @author python98
  */
 public class Main {
 	
-	static final long BASE = 5381;
-	static final long MOD = 1000000007;
-	
 	static int hp, wp, hm, wm, result;
-	static long patternCode;
-	static char[][] p, m;
-	static long[] powC;
-	static long[] powR;
-	static long x;
-	static long[][] prefix;
+	static String[] p, m;
+	static Trie root;
+	
+	static int[] hpNum;
+	static int[][] convert;
+	
+	static class Trie{
+		int end;
+		Trie fail;
+		Trie[] next;
+		
+		Trie() {
+			next = new Trie[2];
+		}
+		
+		void add (String s, int id) {
+			Trie cur = this;
+			for(char c : s.toCharArray()) {
+				int idx = charToIdx(c);
+				if(cur.next[idx]==null) {
+					cur.next[idx] = new Trie();
+				}
+				cur = cur.next[idx];
+			}
+			if(cur.end==0) {
+				cur.end = id+1;
+			}
+		}
+		
+		void build() {
+			Queue<Trie> q = new ArrayDeque<>();
+			this.fail = this;
+			
+			for (int i = 0; i < 2; i++) {
+				if(next[i]!=null) {
+					next[i].fail = this;
+					q.add(next[i]);
+				} else {
+					next[i] = this;
+				}
+			}
+			
+			while(!q.isEmpty()) {
+				Trie cur = q.poll();
+				for(int i = 0 ; i < 2 ; i++) {
+					if(cur.next[i] != null) {
+						Trie child = cur.next[i];
+						child.fail = cur.fail.next[i];
+						q.add(child);
+					} else {
+						cur.next[i] = cur.fail.next[i];
+					}
+				}
+			}
+		}
+		
+		int[] convertRow(String text) {
+			int len = text.length();
+			int[] arr = new int[len];
+			Trie cur = this;
+			for(int i = 0 ; i < len ; i++) {
+				int idx = charToIdx(text.charAt(i));
+				cur = cur.next[idx];
+				arr[i] = cur.end;
+			}
+			return arr;
+		}
+	}
+	
+	static int[] getLps(int[] pat) {
+		int n = pat.length;
+		int[] lps = new int[n];
+		int idx = 0;
+		int i = 1;
+		while(i<n) {
+			if(pat[i] == pat[idx]) {
+				lps[i++] = ++idx; // 같다면 i의 lps는 idx의 다음 인덱스를 가리킴
+			} else if(idx > 0) {
+				idx = lps[idx-1];
+			} else {
+				lps[i++] = 0;
+			}
+		}
+		return lps;
+	}
+	
+	static int kmp(int[] pat, int[] txt) {
+		int[] lps = getLps(pat);
+		int i = 0, j = 0, cnt = 0;
+		while(i < txt.length) {
+			if(txt[i]==pat[j]) {
+				i++;j++;
+				if(j==pat.length) {
+					cnt++;
+					j = lps[j-1];
+				}
+			} else if(j > 0) {
+				j = lps[j-1];
+			} else {
+				i++;
+			}
+		}
+		return cnt;
+	}
+	
+	static int charToIdx(char c) {
+		return c=='o'? 1 : 0;
+	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,71 +127,43 @@ public class Main {
 		hm = Integer.parseInt(st.nextToken());
 		wm = Integer.parseInt(st.nextToken());
 		
-		p = new char[hp][];
-		m = new char[hm][];
-		
+		p = new String[hp];
+		m = new String[hm];
 		for(int i = 0 ; i < hp ; i++) {
-			p[i] = br.readLine().toCharArray();
+			p[i] = br.readLine();
 		}
 		for(int i = 0 ; i < hm ; i++) {
-			m[i] = br.readLine().toCharArray();
+			m[i] = br.readLine();
 		}
 		
-		//해당 열에 곱할 값 세팅
-		powC = new long[wm+1];
-		powC[0] = 1;
-		for(int i = 1 ; i <= wm ; i++) {
-			powC[i] = (powC[i-1]*BASE)%MOD;
+		Map<String, Integer> map = new HashMap<>();
+		hpNum = new int[hp];
+		root = new Trie();
+		int uniqCnt = 0;
+		for(int i = 0 ; i < hp ; i++) {
+			if(!map.containsKey(p[i])) {
+				map.put(p[i], uniqCnt);
+				uniqCnt++;
+				root.add(p[i], map.get(p[i]));
+			}
+			hpNum[i] = map.get(p[i]) + 1;
 		}
 		
-		//행 단위의 해시 누적합 전처리
-		prefix = new long[hm][wm + 1];
-        for (int i = 0; i < hm; i++) {
-            prefix[i][0] = 0;
-            for (int j = 0; j < wm; j++) {
-                prefix[i][j + 1] = (prefix[i][j] * BASE + m[i][j]) % MOD;
-            }
-        }
-        
-        patternCode = computeFlatHash(p, hp, wp);
+		root.build();
 		
-		x = powC[wp];
-		powR = new long[hp+1];
-		powR[0] = 1;
-		for(int i = 1 ; i <= hp ; i++) {
-			powR[i] = (powR[i-1]*x)%MOD;
+		convert = new int[hm][];
+		for(int i = 0 ; i < hm ; i++) {
+			convert[i] = root.convertRow(m[i]);
 		}
 		
 		result = 0;
-		for(int j = 0 ; j <= wm-wp ; j++) {
-			long[] rowCombined  = new long[hm+1];
-			rowCombined [0] = 0;
+		for(int j = wp-1 ; j < wm ; j++) {
+			int[] colData = new int[hm];
 			for(int i = 0 ; i < hm ; i++) {
-				long rowHash = (prefix[i][j + wp] - prefix[i][j] * powC[wp] % MOD + MOD) % MOD;
-				rowCombined [i+1] = (rowCombined[i]*x+rowHash)%MOD;
+				colData[i] = convert[i][j];
 			}
-			for(int i = 0 ; i <= hm-hp ; i++) {
-				long candidate = (rowCombined[i+hp]-(rowCombined[i]*powR[hp])%MOD+MOD)%MOD;
-				if(patternCode==candidate) {
-					result++;
-				}
-			}
+			result += kmp(hpNum,colData);
 		}
 		System.out.print(result);
 	}
-	
-	//2차원 배열의 해시값 계산
-	static long computeFlatHash(char[][] M, int rows, int cols) {
-		long baseX = powC[cols];
-		long hash = 0;
-		for(int i = 0 ; i < rows ; i++) {
-			long rowHash = 0;
-			for(int j = 0 ; j < cols ; j++) {
-				rowHash = (rowHash*BASE+M[i][j])%MOD;
-			}
-			hash = (hash*baseX+rowHash)%MOD;
-		}
-		return hash;
-	}
-	
 }
