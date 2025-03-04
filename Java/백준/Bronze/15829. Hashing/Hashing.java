893939b5ad3f874b25255005c8d1dace0c0bc5df
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-03-04 13:54:25 +0900

    [Bronze II] Title: Hashing, Time: 64 ms, Memory: 11488 KB, Score: 50 point -BaekjoonHub

---
+++ Java/백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+/**
+ * @description 구현
+ * @performance 메모리: ? KB, 동작시간: ? ms
+ * @author python98
+ */
+public class Main {
+	static final int BASE = 31;
+	static final long MOD = 1234567891;
+	
+	static int L;
+	static char[] c;
+	static int[] conv;
+	static long result;
+	static long[] p;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        L = Integer.parseInt(br.readLine());
+        c = br.readLine().toCharArray();
+        
+        conv = new int[c.length];
+        for(int i = 0 ; i < c.length ; i++) {
+        	conv[i] = c[i]-'a' +1; // a:1부터 시작
+        }
+        
+        p = new long[c.length];
+        p[0] = 1;
+        for(int i = 1 ; i < c.length ; i++) {
+        	p[i] = p[i-1] * BASE % MOD; // 각 idx에 곱해줘야 하는 값 세팅
+        }
+        
+        result = 0;
+        for(int i = 0 ; i < c.length ; i++) {
+        	result += p[i] * conv[i] % MOD;
+        }
+        
+        System.out.print(result);
+	}
+}
