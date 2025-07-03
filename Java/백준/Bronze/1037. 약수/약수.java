02fa3cbdf5aa4b33132556a21e4b0745d5b892e0
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-07-04 03:13:22 +0900

    [Bronze I] Title: 약수, Time: 60 ms, Memory: 11564 KB -BaekjoonHub

--- Java/백준/Bronze/1037. 약수/약수.java
+++ Java/백준/Bronze/1037. 약수/약수.java
@@ -2,21 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int cnt = Integer.parseInt(br.readLine());
-        int[] list = new int[cnt];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < cnt; i++){
-            list[i] = Integer.parseInt(st.nextToken());
+
+        int min = 1000000, max = 2;
+
+        for(int i = 0 ; i < cnt ; i++){
+            int cur = Integer.parseInt(st.nextToken());
+            if(min > cur) min = cur;
+            if(max < cur) max = cur;
         }
-        int result;
-        if(cnt==1) {
-            result = list[0]*list[0];
-        } else {
-            Arrays.sort(list);
-            result = list[0] * list[cnt-1];
-        }
-        System.out.print(result);
+        System.out.print(min*max);
     }
 }
