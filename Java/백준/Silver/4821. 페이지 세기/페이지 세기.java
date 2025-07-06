87838c308e198664383512f980dc8de4f4a53ea4
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-07-06 15:34:35 +0900

    [Silver IV] Title: 페이지 세기, Time: 316 ms, Memory: 37208 KB -BaekjoonHub

--- Java/백준/Silver/4821. 페이지 세기/페이지 세기.java
+++ Java/백준/Silver/4821. 페이지 세기/페이지 세기.java
@@ -6,33 +6,39 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int page = Integer.parseInt(br.readLine());
         boolean[] print = new boolean[1001];
-
-        String line;
-        while ((line = br.readLine()) != null) {
-            int page = Integer.parseInt(line.trim());
-            if (page == 0) break;
-
-            Arrays.fill(print, false);
-            String[] tokens = br.readLine().split(",");
+        while(page > 0) {
+            st = new StringTokenizer(br.readLine());
             int result = 0;
-
-            for (String token : tokens) {
-                String[] range = token.split("-");
-                int start = Integer.parseInt(range[0]);
-                int end = (range.length == 2) ? Integer.parseInt(range[1]) : start;
-                end = Math.min(end, page);
-                if (start > end) continue;
-                for (int i = start; i <= end; i++) {
-                    if (!print[i]) {
-                        print[i] = true;
+            while(st.hasMoreTokens()){
+                StringTokenizer range = new StringTokenizer(st.nextToken(","));
+                int low = Integer.parseInt(range.nextToken("-"));
+                low = Math.max(low, 1);
+                if(low > page) continue;
+                if(range.hasMoreTokens()) {
+                    int high = Integer.parseInt(range.nextToken());
+                    high = Math.min(high, page);
+                    if(low > high) continue;
+                    while(low <= high) {
+                        if(!print[low]) {
+                            print[low] = true;
+                            result++;
+                        }
+                        low++;
+                    }
+                } else {
+                    if(!print[low]) {
+                        print[low] = true;
                         result++;
                     }
                 }
             }
             sb.append(result).append("\n");
+            Arrays.fill(print, false);
+            page = Integer.parseInt(br.readLine());
         }
         System.out.print(sb);
     }
 }
-
