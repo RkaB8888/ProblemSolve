a6efcab1772fb9ef89549c978913ff9bb58969d3
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-07-08 18:17:59 +0900

    [Silver I] Title: 보석 상자, Time: 272 ms, Memory: 35568 KB -BaekjoonHub

--- Java/백준/Silver/2792. 보석 상자/보석 상자.java
+++ Java/백준/Silver/2792. 보석 상자/보석 상자.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    // 가능한 최대 질투 = maxJewel, 최소 질투 = minJewel
+    // 가능한 최대 질투 = maxJewel, 최소 질투 = 1
     // 각 보석의 수 / checkNum 을 sum 했을 때
     // 인원 수 보다 작다면 질투를 내린다.
     // 인원 수 보다 크다면 질투를 올린다.
     static int N, M;
-    static int maxJewels, minJewels, result;
-    static int[] jewels; // 1울 제외한 색상 수
+    static int maxJewels, result;
+    static int[] jewels;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,27 +17,24 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         jewels = new int[M];
 
+        maxJewels = 1;
         for(int i = 0 ; i < M ; i++) {
             jewels[i] = Integer.parseInt(br.readLine());
-            minJewels = Math.min(minJewels, jewels[i]);
             maxJewels = Math.max(maxJewels, jewels[i]);
         }
 
-        while(minJewels < maxJewels) {
-            int half = (minJewels + maxJewels + 1)>>1;
+        int minJewels = 1;
+        while (minJewels <= maxJewels) {
+            int mid = (minJewels + maxJewels) >> 1;
             int checkNum = 0;
-            for(int i = 0 ; i < M ; i++) {
-                if (jewels[i] <= half) {
-                    checkNum++;
-                } else {
-                    checkNum+=(jewels[i] + half - 1) / half;
-                }
+            for (int jewel : jewels) {
+                checkNum+=(jewel + mid - 1) / mid;
             }
             if(checkNum <= N) {
-                result = half;
-                maxJewels = half - 1;
+                result = mid;
+                maxJewels = mid - 1;
             } else {
-                minJewels = half;
+                minJewels = mid + 1;
             }
         }
         System.out.print(result);
