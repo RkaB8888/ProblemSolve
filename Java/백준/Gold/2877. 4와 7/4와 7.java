16971da6c80d41e3ede982a76a1f6046c9f083fc
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-08-24 03:11:30 +0900

    [Gold V] Title: 4와 7, Time: 64 ms, Memory: 11488 KB -BaekjoonHub

--- Java/백준/Gold/2877. 4와 7/4와 7.java
+++ Java/백준/Gold/2877. 4와 7/4와 7.java
@@ -3,27 +3,22 @@ import java.util.*;
 
 /**
  * @author python98
- * @description
- * @performance 메모리: ? KB, 동작시간: ? ms
+ * @description Binary Representation + Mapping (4/7)
+ * @performance 메모리: 11,508 KB, 동작시간: 64 ms
  */
 public class Main {
 
-    static int K;
+    static int K, msb;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        K = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine())+1;
+        msb = 30-Integer.numberOfLeadingZeros(K);
+        StringBuilder sb = new StringBuilder(msb);
 
-        StringBuilder sb = new StringBuilder();
-
-        K+=1;
-        while(K>1) {
-            if((K&1)==1) {
-                sb.append('7');
-            } else {
-                sb.append('4');
-            }
-            K>>=1;
+        for(int i = msb ; i >= 0 ; i--) {
+            int bit = (K >> i) & 1;
+            sb.append(bit == 0 ? '4' : '7');
         }
-        System.out.print(sb.reverse());
+        System.out.print(sb);
     }
 }
