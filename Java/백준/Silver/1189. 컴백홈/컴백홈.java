c6b8f8d872cfe87a943d813e7416d75e458292c9
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-09-01 09:41:10 +0900

    [Silver I] Title: 컴백홈, Time: 68 ms, Memory: 11432 KB -BaekjoonHub

--- Java/백준/Silver/1189. 컴백홈/컴백홈.java
+++ Java/백준/Silver/1189. 컴백홈/컴백홈.java
@@ -3,16 +3,16 @@ import java.util.*;
 
 /**
  * @author python98
- * @description ?
- * @performance 메모리: ? KB, 동작시간: ? ms
+ * @description DFS + Backtracking + Visited Bitmask + Manhattan/Parity Pruning
+ * @performance 메모리: 12,344 KB, 동작시간: 96 ms
  */
 public class Main {
 
     static final int[][] DIRECTIONS = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     static int R, C, K, result;
-    static char[][] map;
-    static boolean[][] visit;
+    static int visit, block;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,27 +20,35 @@ public class Main {
         C = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        map = new char[R][];
-        visit = new boolean[R][C];
-        for(int i = 0; i < R; i++) map[i] = br.readLine().toCharArray();
-        visit[R-1][0] = true;
-        dfs(R-1, 0, 1);
+        for (int i = 0; i < R; i++) {
+            char[] row = br.readLine().toCharArray();
+            for (int j = 0; j < C; j++) {
+                if (row[j] == 'T') block |= 1 << (i * C + j);
+            }
+        }
+        visit = 1 << ((R - 1) * C);
+        dfs(R - 1, 0, 1);
         System.out.print(result);
     }
 
     private static void dfs(int r, int c, int cnt) {
-        if(r == 0 && c == C-1 && cnt == K) {
-            result++;
+        if (cnt == K) {
+            if (r == 0 && c == C - 1) result++;
             return;
         }
-        if(cnt > K) return;
-        for(int i = 0 ; i < 4 ; i++) {
-            int nr = r + DIRECTIONS[i][0];
-            int nc = c + DIRECTIONS[i][1];
-            if(nr < 0 || nr >= R || nc < 0 || nc >= C || map[nr][nc] == 'T' || visit[nr][nc]) continue;
-            visit[nr][nc] = true;
-            dfs(nr, nc, cnt+1);
-            visit[nr][nc] = false;
+
+        int rest = K - cnt;
+        int minCnt = r + (C - 1 - c);
+        if (rest < minCnt || ((rest - minCnt) & 1) == 1) return;
+
+        for (int[] d : DIRECTIONS) {
+            int nr = r + d[0], nc = c + d[1];
+            if (nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
+            int idx = nr * C + nc, bit = 1 << idx;
+            if ((block & bit) != 0 || (visit & bit) != 0) continue;
+            visit |= bit;
+            dfs(nr, nc, cnt + 1);
+            visit ^= bit;
         }
     }
 
