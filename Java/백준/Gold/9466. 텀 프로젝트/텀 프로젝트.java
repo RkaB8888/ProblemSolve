1271881f4356cf9865c2f8fc0647692390382f53
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-09-04 11:12:39 +0900

    [Gold III] Title: 텀 프로젝트, Time: 912 ms, Memory: 300232 KB -BaekjoonHub

--- Java/백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ Java/백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -3,70 +3,46 @@ import java.util.*;
 
 /**
  * @author python98
- * @description DFS (cycle detection) + Visiting/Finished Marking
- * @performance 메모리: 303,456 KB, 동작시간: 1160 ms
+ * @description ?
+ * @performance 메모리: ? KB, 동작시간: ? ms
  */
 public class Main {
 
-    static final int LEN = 100001;
-
     static int T, N, result;
-    static int[] arr, stack;
-    static boolean[] vis, fin;
+    static int[] next, cnt;
 
-    //-2:이미 그룹됨, -1:아무것도 못됨, 0:확인 안함, 1:검사시작번호, 2:순환시작번호
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-
-        arr = new int[LEN];
-        stack = new int[LEN];
-        vis = new boolean[LEN];
-        fin = new boolean[LEN];
-
         T = Integer.parseInt(br.readLine());
         for (int t = 1; t <= T; t++) {
             N = Integer.parseInt(br.readLine());
-            Arrays.fill(vis, false);
-            Arrays.fill(fin, false);
+            next = new int[N + 1];
+            cnt = new int[N + 1];
             result = 0;
             st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= N; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
+                next[i] = Integer.parseInt(st.nextToken());
+                cnt[next[i]]++;
             }
-            int len = 0;
-            for (int i = 1; i <= N; i++) {
-                if (fin[i]) continue;
-                int last = i;
-                //fin이거나 vis일 때 까지 stack에 넣기
-                while(!fin[last]&&!vis[last]) {
-                    stack[len++] = last;
-                    vis[last] = true;
-                    last = arr[last];
-                }
-                if(fin[last]) { // stack은 버려지는 숫자
-                    while(len > 0){
-                        fin[stack[--len]] = true;
-                        result++;
-                    }
-                } else { // 그룹되는 경우
-                    while(len >0){
-                        int now = stack[--len];
-                        fin[now] = true;
-                        if(now == last) {
-                            break;
-                        }
-                    }
-                    while(len > 0) {
-                        fin[stack[--len]] = true;
-                        result++;
-                    }
+            for(int i = 1; i <= N; i++) {
+                if(cnt[i]==0) {
+                    cnt[i]=-1;
+                    result++;
+                    count(next[i]);
                 }
             }
             sb.append(result).append("\n");
         }
         System.out.print(sb);
     }
-
+    static void count(int i){
+        cnt[i]--;
+        if(cnt[i]==0){
+            cnt[i]=-1;
+            result++;
+            count(next[i]);
+        }
+    }
 }
