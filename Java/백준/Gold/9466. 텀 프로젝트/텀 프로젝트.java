ba1636eaf6098432af916b7b6281816812a4d45c
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-09-04 09:53:00 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1052 ms, Memory: 305424 KB -BaekjoonHub

--- Java/백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ Java/백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -3,52 +3,70 @@ import java.util.*;
 
 /**
  * @author python98
- * @description ?
- * @performance 메모리: ? KB, 동작시간: ? ms
+ * @description DFS (cycle detection) + Visiting/Finished Marking
+ * @performance 메모리: 303,456 KB, 동작시간: 1160 ms
  */
 public class Main {
 
+    static final int LEN = 100001;
+
     static int T, N, result;
-    static int[] arr, gn;
+    static int[] arr, stack;
+    static boolean[] vis, fin;
+
     //-2:이미 그룹됨, -1:아무것도 못됨, 0:확인 안함, 1:검사시작번호, 2:순환시작번호
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+
+        arr = new int[LEN];
+        stack = new int[LEN];
+        vis = new boolean[LEN];
+        fin = new boolean[LEN];
+
         T = Integer.parseInt(br.readLine());
         for (int t = 1; t <= T; t++) {
             N = Integer.parseInt(br.readLine());
-            arr = new int[N + 1];
-            gn = new int[N + 1];
+            Arrays.fill(vis, false);
+            Arrays.fill(fin, false);
             result = 0;
             st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= N; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
-            for(int i = 1; i <= N; i++){
-                if(gn[i]!=0) continue;
-                gn[i] = 1;
-                gn[i] = check(arr[i]);
-                if(gn[i]==-1) result++;
+            int len = 0;
+            for (int i = 1; i <= N; i++) {
+                if (fin[i]) continue;
+                int last = i;
+                //fin이거나 vis일 때 까지 stack에 넣기
+                while(!fin[last]&&!vis[last]) {
+                    stack[len++] = last;
+                    vis[last] = true;
+                    last = arr[last];
+                }
+                if(fin[last]) { // stack은 버려지는 숫자
+                    while(len > 0){
+                        fin[stack[--len]] = true;
+                        result++;
+                    }
+                } else { // 그룹되는 경우
+                    while(len >0){
+                        int now = stack[--len];
+                        fin[now] = true;
+                        if(now == last) {
+                            break;
+                        }
+                    }
+                    while(len > 0) {
+                        fin[stack[--len]] = true;
+                        result++;
+                    }
+                }
             }
             sb.append(result).append("\n");
-//            System.out.print(result+":");
-//            System.out.println(Arrays.toString(gn));
         }
         System.out.print(sb);
-    }
-
-    private static int check(int i){
-        if(gn[i]<0) return -1; // 이미 그룹화 된 곳
-        gn[i]++; // 지나간 곳 표시
-        if(gn[i]==2) { // 만약 두 번 째 지나간 곳이면
-            return gn[i] = -2; // 이미 그룹 됨 표시
-        }
-
-        int temp = check(arr[i]);
-        if(temp==-1) result++;
-        else if(gn[i]==-2) return -1;
-        return gn[i] = temp;
     }
 
 }
