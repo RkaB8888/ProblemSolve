4780fafc3b6f174a41b99f9dac658d86d6754120
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-09-07 16:09:47 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 180 ms, Memory: 35400 KB -BaekjoonHub

--- Java/백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.java
+++ Java/백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 /**
  * @author python98
- * @description 이 클래스에 대한 동작 설명
- * @performance 메모리: ? KB, 동작시간: ? ms
+ * @description BFS + Parity (Even/Odd) + ArrayDeque
+ * @performance 메모리: 42,268 KB, 동작시간: 188 ms
  */
 public class Main {
 
@@ -13,50 +13,58 @@ public class Main {
     static int N, K;
     static int[][] reachTime;
 
-    static class TimePose {
-        int time;
-        int pose;
-
-        TimePose(int time, int pose) {
-            this.time = time;
-            this.pose = pose;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
+
+        if (N == K) {
+            System.out.print(0);
+            return;
+        }
+
         reachTime = new int[2][LIMIT + 1];
         Arrays.fill(reachTime[0], Integer.MAX_VALUE);
         Arrays.fill(reachTime[1], Integer.MAX_VALUE);
 
-        Queue<TimePose> q = new ArrayDeque<>();
+        int time = 0, kk = K;
+        while (kk <= LIMIT) {
+            time++;
+            kk += time;
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
         reachTime[0][N] = 0;
-        q.add(new TimePose(0, N));
+        q.add(N);
+
+        int t = 0;
         while (!q.isEmpty()) {
-            TimePose now = q.poll();
-            int nextTime = now.time + 1;
-            int nextPose = now.pose * 2;
-            if (nextPose <= LIMIT && reachTime[nextTime & 1][nextPose] > nextTime) {
-                reachTime[nextTime & 1][nextPose] = nextTime;
-                q.add(new TimePose(now.time + 1, nextPose));
-            }
-            nextPose = now.pose + 1;
-            if (nextPose <= LIMIT && reachTime[nextTime & 1][nextPose] > nextTime) {
-                reachTime[nextTime & 1][nextPose] = nextTime;
-                q.add(new TimePose(now.time + 1, nextPose));
-            }
-            nextPose = now.pose - 1;
-            if (nextPose >= 0 && reachTime[nextTime & 1][nextPose] > nextTime) {
-                reachTime[nextTime & 1][nextPose] = nextTime;
-                q.add(new TimePose(now.time + 1, nextPose));
+            t++;
+            if (t >= time) break;
+            int len = q.size();
+            while (--len >= 0) {
+                int pose = q.poll();
+                int nextPose = pose * 2;
+                if (nextPose <= LIMIT && reachTime[t & 1][nextPose] > t) {
+                    reachTime[t & 1][nextPose] = t;
+                    q.add(nextPose);
+                }
+                nextPose = pose + 1;
+                if (nextPose <= LIMIT && reachTime[t & 1][nextPose] > t) {
+                    reachTime[t & 1][nextPose] = t;
+                    q.add(nextPose);
+                }
+                nextPose = pose - 1;
+                if (nextPose >= 0 && reachTime[t & 1][nextPose] > t) {
+                    reachTime[t & 1][nextPose] = t;
+                    q.add(nextPose);
+                }
             }
         }
 
-        int time = 0;
+        time = 0;
         while (K <= LIMIT) {
             if (reachTime[time & 1][K] <= time) {
                 System.out.print(time);
