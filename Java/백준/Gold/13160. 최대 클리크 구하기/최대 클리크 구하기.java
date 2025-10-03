5e71e731c023f9c3bcd25e2425c78536a39985b5
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-10-03 18:39:40 +0900

    [Gold III] Title: 최대 클리크 구하기, Time: 704 ms, Memory: 116240 KB -BaekjoonHub

--- Java/백준/Gold/13160. 최대 클리크 구하기/최대 클리크 구하기.java
+++ Java/백준/Gold/13160. 최대 클리크 구하기/최대 클리크 구하기.java
@@ -3,44 +3,45 @@ import java.util.*;
 
 /**
  * @author python98
- * @description ?
- * @performance 메모리: ? KB, 동작시간: ? ms
+ * @description Line Sweep + Sorting
+ * @performance 메모리: 143,392 KB, 동작시간: 1,876 ms
  */
 /*
 동시에 겹치는 구간의 최대 갯수 찾기
  */
 public class Main {
     static int N;
-    static int[][] edges;
+    static int[] L, R, T;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
-        edges = new int[N][2];
-        int[][] times = new int[2*N][2];
-        for (int i = 0; i < N; i++) {
+        L = new int[N];
+        R = new int[N];
+        T = new int[N << 1];
+        for (int i = 0, idx = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            edges[i][0] = Integer.parseInt(st.nextToken());
-            edges[i][1] = Integer.parseInt(st.nextToken());
-            times[i<<1][0] = edges[i][0]-1;
-            times[i<<1][1] = 1;
-            times[(i<<1)+1][0] = edges[i][1];
-            times[(i<<1)+1][1] = -1;
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            L[i] = s;
+            R[i] = e;
+            T[idx++] = ((s - 1) << 1) | 1;
+            T[idx++] = (e << 1);
         }
-        Arrays.sort(times,(a, b)->a[0]-b[0]);
-        int cnt = 0, maxCnt = 0, t = 0;
-        for(int[] time : times) {
-            cnt+=time[1];
-            if(cnt>maxCnt) {
-                t = time[0]+1;
+        Arrays.sort(T);
+        int cnt = 0, maxCnt = 0, time = 0;
+        for (int t : T) {
+            cnt += ((t & 1) == 1) ? 1 : -1;
+            if (cnt > maxCnt) {
+                time = (t >> 1) + 1;
                 maxCnt = cnt;
             }
         }
         sb.append(maxCnt).append('\n');
-        for(int i = 0 ; i < N ; i++) {
-            if(edges[i][0]<=t && t<=edges[i][1]) {
-                sb.append(i+1).append(' ');
+        for (int i = 0; i < N; i++) {
+            if (L[i] <= time && time <= R[i]) {
+                sb.append(i + 1).append(' ');
             }
         }
         System.out.print(sb);
