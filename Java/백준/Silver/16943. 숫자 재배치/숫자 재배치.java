746824e80c214ea2eb43ca1e3f31186d6075a566
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-10-07 21:46:10 +0900

    [Silver I] Title: 숫자 재배치, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

--- Java/백준/Silver/16943. 숫자 재배치/숫자 재배치.java
+++ Java/백준/Silver/16943. 숫자 재배치/숫자 재배치.java
@@ -3,66 +3,103 @@ import java.util.*;
 
 /**
  * @author python98
- * @description 이 클래스에 대한 동작 설명
- * @performance 메모리: ? KB, 동작시간: ? ms
- */
-/*
-1의 자릿수부터 시작해서 처음으로 커지는 자릿수 찾기
-해당 자리보다 뒷 자리에서 해당 자릿수보다 작으면서 제일 큰 수와 교환
-뒷 자리의 수들을 뒤집어줌
+ * @description dfs + 넥퍼
+ * @performance 메모리: 12,632 KB, 동작시간: 108 ms
  */
 public class Main {
-    static int B, C;
-    static char[] A;
+    static char[] aChars, bChars, result;
+    static int aLen, bLen;
+    static int[] counts;
+    static boolean flag;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        A = st.nextToken().toCharArray();
-        B = Integer.parseInt(st.nextToken());
-        Arrays.sort(A);
-        reverse(A, 0, A.length - 1);
-        while (check()) {
-            int idx = A.length-1;
-            while(idx>0&&A[idx-1]<=A[idx]) idx--;
-            if(idx==0) {
-                C=-1;
-                break;
-            }
-            idx--;
-            int target = A.length-1;
-            while(A[idx]<=A[target]) target--;
-            swap(idx,target);
-            if(A[0]=='0') {
-                C=-1;
-                break;
-            }
-            reverse(A,idx+1,A.length-1);
+        String aStr = st.nextToken();
+        String bStr = st.nextToken();
+
+        aLen = aStr.length();
+        aChars = aStr.toCharArray();
+        bLen = bStr.length();
+        bChars = bStr.toCharArray();
+
+        if (aLen < bLen) {
+            Arrays.sort(aChars);
+            reverse(aChars, 0, aLen - 1);
+            System.out.print(new String(aChars));
+            return;
         }
-        System.out.print(C);
-    }
-
-    private static boolean check() {
-        C = 0;
-        for (int a : A) {
-            C = (C << 3) + (C << 1) + (a & 15);
+        if (aLen > bLen) {
+            System.out.print(-1);
+            return;
         }
-//        System.out.println(B+", "+C);
-        return B <= C;
+        counts = new int[10];
+        for (char c : aChars) {
+            counts[c & 15]++;
+        }
+
+        result = new char[aLen];
+        dfs(0, true);
+
+        if(!flag || result[0]==48) {
+            System.out.print(-1);
+            return;
+        }
+
+        String resultStr = new String(result);
+        if(resultStr.equals(bStr)) {
+            if(!nextPer(result)) {
+                System.out.print(-1);
+            } else {
+                System.out.print(new String(result));
+            }
+        } else {
+            System.out.print(resultStr);
+        }
     }
 
-    private static void swap(int a,int b){
-        char temp = A[a];
-        A[a] = A[b];
-        A[b] = temp;
+    private static void dfs(int k, boolean isTight) {
+        if (flag) return;
+        if (k == aLen) {
+            flag = true;
+            return;
+        }
+
+        int limit = isTight ? (bChars[k] & 15) : 9; // b의 자릿수 보다 작아야 함.
+        for (int d = limit; d >= 0; d--) {
+            if (counts[d] > 0) {
+                result[k] = (char) (d + 48);
+                counts[d]--;
+                dfs(k + 1, isTight && (d == limit));
+
+                if(flag) return;
+                counts[d]++;
+            }
+        }
     }
 
-    private static void reverse(char[] c, int start, int end) {
-        for (int s = start, e = end; s < e; s++, e--) {
-            char temp = c[s];
-            c[s] = c[e];
-            c[e] = temp;
+    private static boolean nextPer(char[] arr) {
+        int i = arr.length - 1;
+        while (i > 0 && arr[i - 1] <= arr[i]) i--;
+        if (i == 0) return false;
+
+        int j = arr.length - 1;
+        while (arr[i - 1] <= arr[j]) j--;
+
+        swap(arr, i - 1, j);
+        reverse(arr, i, arr.length - 1);
+        return arr[0] != '0';
+    }
+
+    private static void swap(char[] arr, int a, int b) {
+        char temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    private static void reverse(char[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start++, end--);
         }
     }
 }
