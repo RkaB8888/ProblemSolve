25cc8e288b8aa137af7911addbbd11a16fa837c6
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-10-11 20:35:51 +0900

    [Silver I] Title: 부동산 다툼, Time: 228 ms, Memory: 29796 KB -BaekjoonHub

--- Java/백준/Silver/20364. 부동산 다툼/부동산 다툼.java
+++ Java/백준/Silver/20364. 부동산 다툼/부동산 다툼.java
@@ -2,31 +2,38 @@ import java.io.*;
 import java.util.*;
 
 /**
- * @description 이 클래스에 대한 동작 설명
- * @performance 메모리: ? KB, 동작시간: ? ms
  * @author python98
+ * @description 이 클래스에 대한 동작 설명
+ * @performance 메모리: 45,644 KB, 동작시간: 352 ms
  */
 public class Main {
     static int N, Q;
     static int[] est;
+
+    private static int nestInt() throws IOException {
+        int c, n;
+        while ((n = System.in.read()) <= 32) ;
+        n &= 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        Q = Integer.parseInt(st.nextToken());
-        est = new int[N+1];
-        for(int i = 1 ; i <= Q ; i++) {
+        N = nestInt();
+        Q = nestInt();
+        est = new int[N + 1];
+        for (int i = 1; i <= Q; i++) {
             int result = 0;
-            int goal = Integer.parseInt(br.readLine());
+            int goal = nestInt();
             int estCheck = goal;
-            while(estCheck>1) {
-                if(est[estCheck]!=0) {
+            while (estCheck > 1) {
+                if (est[estCheck] != 0) {
                     result = estCheck;
                 }
-                estCheck>>=1;
+                estCheck >>= 1;
             }
-            if(result==0) est[goal] = i;
+            if (result == 0) est[goal] = i;
             sb.append(result).append('\n');
         }
         System.out.print(sb);
