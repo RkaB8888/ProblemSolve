dff4054f2157ecd4b521658da072552e2edee958
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-10-11 16:02:23 +0900

    [Silver I] Title: 나무 탈출, Time: 280 ms, Memory: 27820 KB -BaekjoonHub

--- Java/백준/Silver/15900. 나무 탈출/나무 탈출.java
+++ Java/백준/Silver/15900. 나무 탈출/나무 탈출.java
@@ -3,29 +3,28 @@ import java.util.*;
 
 /**
  * @author python98
- * @description 이 클래스에 대한 동작 설명
- * @performance 메모리: 143,712 KB, 동작시간: 528 ms
+ * @description BFS + 인접 리스트(전방성 배열) + 배열 큐
+ * @performance 메모리: 26,324 KB, 동작시간: 292 ms
  */
 public class Main {
-    static int N, totalStep;
-    static int[] step, link, next, v;
-    static boolean[] leaf;
+    static int N, sum;
+    static int[] step, deg, link, next, v;
 
     private static int nextInt() throws IOException {
         int n, c;
-        while((n=System.in.read())<=32);
-        n&=15;
-        while((c=System.in.read())>32) n = (n<<3) + (n<<1) + (c&15);
+        while ((n = System.in.read()) <= 32) ;
+        n &= 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
         return n;
     }
+
     public static void main(String[] args) throws IOException {
         N = nextInt();
         step = new int[N + 1];
-        Arrays.fill(step, -1);
+        deg = new int[N + 1];
         link = new int[N + 1];
         next = new int[N << 1];
         v = new int[N << 1];
-        leaf = new boolean[N + 1];
         for (int i = 1; i < N; i++) {
             int a = nextInt(), b = nextInt();
             next[(i << 1)] = link[a];
@@ -35,30 +34,29 @@ public class Main {
             next[(i << 1) + 1] = link[b];
             link[b] = (i << 1) + 1;
             v[(i << 1) + 1] = a;
+
+            deg[a]++;
+            deg[b]++;
         }
 
-        dfs();
-        for (int i = 1; i <= N; i++) {
-            if (leaf[i]) totalStep += step[i];
-        }
-        System.out.print((totalStep & 1) == 1 ? "Yes" : "No");
+        bfs();
+        System.out.print((sum & 1) == 1 ? "Yes" : "No");
     }
 
-    private static void dfs() {
+    private static void bfs() {
         int[] q = new int[N];
         int b = 0, t = 0;
-        leaf[1] = true;
+        Arrays.fill(step, -1);
         step[1] = 0;
         q[t++] = 1;
-        while (b<t) {
+        while (b < t) {
             int curV = q[b++];
             for (int e = link[curV]; e > 0; e = next[e]) {
                 int nextV = v[e];
                 if (step[nextV] >= 0) continue;
-                leaf[curV] = false;
-                leaf[nextV] = true;
                 step[nextV] = step[curV] + 1;
                 q[t++] = nextV;
+                if (deg[nextV] == 1) sum += step[nextV];
             }
         }
     }
