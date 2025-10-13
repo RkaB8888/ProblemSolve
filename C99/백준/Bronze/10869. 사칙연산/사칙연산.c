c5b8f9ce412ee2757c6ef26ad6e272f669f2b4e4
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-10-14 08:26:54 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/10869. 사칙연산/사칙연산.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * @description ? 
+ * @performance 메모리: ? KB, 동작시간: ? ms
+ * @author java08
+ */
+
+int main(void) {
+    int a, b;
+    scanf("%d%d",&a, &b);
+    printf("%d\n%d\n%d\n%d\n%d", a+b, a-b,a*b,a/b,a%b);
+    return 0;
+}
