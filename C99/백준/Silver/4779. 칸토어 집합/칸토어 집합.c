de1fe15f9c55bce8cbe654b137fa5d9aba9fa97f
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-10-16 00:36:32 +0900

    [Silver III] Title: 칸토어 집합, Time: 0 ms, Memory: 1636 KB -BaekjoonHub

--- C99/백준/Silver/4779. 칸토어 집합/칸토어 집합.c
+++ C99/백준/Silver/4779. 칸토어 집합/칸토어 집합.c
@@ -1,43 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include <stdint.h>
 
 /**
- * @description ?
- * @performance 메모리: 1,112 KB, 동작시간: 0 ms
+ * @description 칸토어 집합 생성 (분할정복) + 문자 배열
+ * @performance 메모리: 1,812 KB, 동작시간: 0 ms
  * @author java08
  */
 
+int idx;
+char str[531442];
+int pow3[13] = {1};
+
+void cantor(int N)
+{
+    if (N == 0)
+    {
+        str[idx++] = '-';
+    }
+    else
+    {
+        cantor(N - 1);
+        idx += pow3[N - 1];
+        cantor(N - 1);
+    }
+}
+
 int main(void)
 {
-    char *str[13] = {0};
-    int len[13] = {0};
-
-    len[0] = 1;
-    str[0] = (char *)malloc(len[0] + 1);
-    str[0][0] = '-';
-    str[0][1] = '\0';
-
-    for (int i = 1; i < 13; ++i)
+    for (int i = 1; i < 13; i++)
     {
-        len[i] = len[i - 1] * 3;
-        str[i] = (char *)malloc(len[i] + 1);
-
-        memcpy(str[i], str[i - 1], len[i - 1]);
-        memset(str[i] + len[i - 1], ' ', len[i - 1]);
-        memcpy(str[i] + 2 * len[i - 1], str[i - 1], len[i - 1]);
-        str[i][len[i]] = '\0';
+        pow3[i] = pow3[i - 1] * 3;
     }
-
     int N;
     while (scanf("%d", &N) == 1)
     {
-        printf("%s\n", str[N]);
-    }
+        if (N == 0)
+        {
+            printf("-\n");
+            continue;
+        }
+        int len = pow3[N];
+        memset(str, ' ', len);
+        str[len] = '\0';
 
-    for (int i = 0; i < 13; i++)
-        free(str[i]);
+        idx = 0;
+        cantor(N);
+        printf("%s\n", str);
+    }
     return 0;
 }
