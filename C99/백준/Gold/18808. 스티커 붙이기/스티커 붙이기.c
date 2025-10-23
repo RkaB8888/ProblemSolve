1efa3cc9a75551a768244c412a4380a47d2cf808
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-10-23 09:49:47 +0900

    [Gold III] Title: 스티커 붙이기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- C99/백준/Gold/18808. 스티커 붙이기/스티커 붙이기.c
+++ C99/백준/Gold/18808. 스티커 붙이기/스티커 붙이기.c
@@ -5,47 +5,30 @@
 #include <stdint.h>
 
 /**
- * @description ?
- * @performance 메모리: 1,112 KB, 동작시간: 0 ms
+ * @description 완전탐색(Brute Force) + 2차원 배열(Matrix)
+ * @performance 메모리: 1,116 KB, 동작시간: 8 ms
  * @author java08
  */
 static int N, M, r, c;
-static int **map, **arr;
+static long *map, *arr;
 
-static int stitch(int map_r, int map_c, int arr_r, int arr_c)
+static int stitch(int map_r, int map_c, long piece_bits)
 {
-    if (map[map_r][map_c] == 1 && arr[arr_r][arr_c] == 1)
-    {
-        return 0;
-    }
-    map[map_r][map_c] |= arr[arr_r][arr_c];
 
-    if (arr_r == r - 1 && arr_c == c - 1)
+    for (int i = 0; i < r; i++)
     {
-        return 1;
+        long map_bits = map[map_r + i] >> (M - c - map_c);
+        map_bits = map_bits & piece_bits;
+        if (map_bits & arr[i])
+        {
+            return 0;
+        }
     }
-
-    int next_arr_r = arr_r;
-    int next_arr_c = arr_c + 1;
-    int next_map_r = map_r;
-    int next_map_c = map_c + 1;
-    if (next_arr_c == c)
+    for (int i = 0; i < r; i++)
     {
-        next_arr_c = 0;
-        next_arr_r++;
-        next_map_c = map_c - arr_c;
-        next_map_r++;
+        map[map_r + i] |= (arr[i] << (M - c - map_c));
     }
-
-    if (stitch(next_map_r, next_map_c, next_arr_r, next_arr_c))
-    {
-        return 1;
-    }
-    else
-    {
-        map[map_r][map_c] = arr[arr_r][arr_c] == 1 ? 0 : map[map_r][map_c];
-        return 0;
-    }
+    return 1;
 }
 
 static int dfs()
@@ -54,11 +37,13 @@ static int dfs()
     {
         return 0;
     }
+    long piece_bits = 1 << c;
+    piece_bits--;
     for (int i = 0; i <= N - r; i++)
     {
         for (int j = 0; j <= M - c; j++)
         {
-            if (stitch(i, j, 0, 0))
+            if (stitch(i, j, piece_bits))
             {
                 return 1;
             }
@@ -69,21 +54,16 @@ static int dfs()
 
 void rotate()
 {
-    int **new_arr = malloc(c * sizeof(int *));
-    for (int i = 0; i < c; i++)
-    {
-        new_arr[i] = malloc(r * sizeof(int));
-    }
+    long *new_arr = calloc(c, sizeof(long));
     for (int i = 0; i < r; i++)
     {
         for (int j = 0; j < c; j++)
         {
-            new_arr[j][r - 1 - i] = arr[i][j];
+            if (arr[i] & (1 << (c - j - 1)))
+            {
+                new_arr[j] |= (1 << i);
+            }
         }
-    }
-    for (int i = 0; i < r; i++)
-    {
-        free(arr[i]);
     }
     free(arr);
     arr = new_arr;
@@ -99,11 +79,7 @@ int main(void)
     {
         return 1;
     }
-    map = malloc(N * sizeof(int *));
-    for (int i = 0; i < N; i++)
-    {
-        map[i] = calloc(M, sizeof(int));
-    }
+    map = calloc(N, sizeof(long));
 
     for (int i = 0; i < K; i++)
     {
@@ -112,13 +88,10 @@ int main(void)
             return 1;
         }
         int cnt = 0;
-        arr = malloc(r * sizeof(int *));
+        arr = calloc(r, sizeof(long));
         for (int j = 0; j < r; j++)
         {
-            arr[j] = calloc(c, sizeof(int));
-        }
-        for (int j = 0; j < r; j++)
-        {
+            long col = 0;
             for (int k = 0; k < c; k++)
             {
                 int cell;
@@ -126,12 +99,13 @@ int main(void)
                 {
                     return 1;
                 }
-                arr[j][k] = cell;
+                col = (col << 1) | cell;
                 if (cell == 1)
                 {
                     cnt++;
                 }
             }
+            arr[j] = col;
         }
         for (int j = 0; j < 4; j++)
         {
@@ -146,17 +120,9 @@ int main(void)
                 rotate();
             }
         }
-        for (int j = 0; j < r; j++)
-        {
-            free(arr[j]);
-        }
         free(arr);
     }
     printf("%d\n", result);
-    for (int i = 0; i < N; i++)
-    {
-        free(map[i]);
-    }
     free(map);
     return 0;
 }
