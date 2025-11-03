bc30a91e00c12f23bbb8761ffe876fa3c50dfa79
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2025-11-04 00:16:23 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 68 ms, Memory: 3556 KB -BaekjoonHub

--- C99/백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/README.md
+++ C99/백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 5916 KB, 시간: 232 ms
+메모리: 3556 KB, 시간: 68 ms
 
 ### 분류
 
@@ -12,7 +12,7 @@
 
 ### 제출 일자
 
-2025년 11월 3일 17:29:13
+2025년 11월 4일 00:16:17
 
 ### 문제 설명
 
@@ -102,19 +102,19 @@
 
 <p><img alt="" src="https://www.acmicpc.net/upload/201004/pb.PNG" style="height:181px; width:274px"></p>
 
-<p><img alt="" src="" style="height:183px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/pc.PNG" style="height:183px; width:274px"></p>
 
-<p><img alt="" src="" style="height:182px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/pd.PNG" style="height:182px; width:274px"></p>
 
 <p><img alt="" src="https://www.acmicpc.net/upload/201004/pe.PNG" style="height:183px; width:274px"></p>
 
-<p><img alt="" src="" style="height:183px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/pf.PNG" style="height:183px; width:274px"></p>
 
-<p><img alt="" src="" style="height:181px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/pg.PNG" style="height:181px; width:274px"></p>
 
-<p><img alt="" src="" style="height:184px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/ph.PNG" style="height:184px; width:274px"></p>
 
-<p><img alt="" src="" style="height:184px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/pi.PNG" style="height:184px; width:274px"></p>
 
 <p>이다솜 : 휴... 이겼다.</p>
 
--- C99/백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.c
+++ C99/백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.c
@@ -5,29 +5,19 @@
 #include <stdint.h>
 
 /**
- * @description ?
- * @performance 메모리: 1,112 KB, 동작시간: 0 ms
+ * @description djb2 해시
+ * @performance 메모리: 5,916 KB, 동작시간: 232 ms
  * @author java08
  */
-
 #define HASH_SIZE 100003
-#define BASE1 31
-#define BASE2 37
-#define MOD1 1000000007
-#define MOD2 1000000009
+static char (*arr)[21];
 
-typedef struct Node
-{
-    char name[21];
-    int index;
-} Node;
+static int hash_table[HASH_SIZE];
 
 static inline int is_number(const char *str)
 {
     return str[0] >= '0' && str[0] <= '9';
 }
-
-static Node hash_table[HASH_SIZE];
 
 static inline int char2int(char c)
 {
@@ -39,46 +29,45 @@ static inline int char2int(char c)
     return -1;
 }
 
+static unsigned long long djb2(const char *str)
+{
+    unsigned long long hash = 5381;
+    for (int i = 0; str[i]; i++)
+    {
+        hash = ((hash << 5) + hash) + str[i];
+    }
+    return hash;
+}
+
 static void add_map(const char *name, int index)
 {
-    long long hash1 = 0;
-    long long hash2 = 0;
-    for (int i = 0; name[i]; i++)
+    unsigned long long temp = djb2(name);
+    unsigned long long hash1 = temp % HASH_SIZE;
+    unsigned long long hash2 = 1 + (temp % (HASH_SIZE - 1));
+    int hash_index = hash1;
+    while (hash_table[hash_index] != 0)
     {
-        hash1 = (hash1 * BASE1 + char2int(name[i])) % MOD1;
-        hash2 = (hash2 * BASE2 + char2int(name[i])) % MOD2;
+        hash_index = (hash_index + hash2) % HASH_SIZE;
     }
-    int hash_index = (hash1 + hash2) % HASH_SIZE;
-    while (hash_table[hash_index].index != 0)
-    {
-        hash_index = (hash_index + 1) % HASH_SIZE;
-    }
-    strncpy(hash_table[hash_index].name, name, 21);
-    hash_table[hash_index].index = index;
+    hash_table[hash_index] = index;
 }
 
 static int get_index(const char *name)
 {
-    long long hash1 = 0;
-    long long hash2 = 0;
-    for (int i = 0; name[i]; i++)
+    unsigned long long temp = djb2(name);
+    unsigned long long hash1 = temp % HASH_SIZE;
+    unsigned long long hash2 = 1 + (temp % (HASH_SIZE - 1));
+    int hash_index = hash1;
+    while (hash_table[hash_index] != 0)
     {
-        hash1 = (hash1 * BASE1 + char2int(name[i])) % MOD1;
-        hash2 = (hash2 * BASE2 + char2int(name[i])) % MOD2;
-    }
-    int hash_index = (hash1 + hash2) % HASH_SIZE;
-    while (true)
-    {
-        if (hash_table[hash_index].index == 0)
+        int index = hash_table[hash_index];
+        if (strcmp(arr[index], name) == 0)
         {
-            return -1; // 도감에 없는 경우
+            return index;
         }
-        if (strncmp(hash_table[hash_index].name, name, 21) == 0)
-        {
-            return hash_table[hash_index].index;
-        }
-        hash_index = (hash_index + 1) % HASH_SIZE;
+        hash_index = (hash_index + hash2) % HASH_SIZE;
     }
+    return -1;
 }
 
 int main(void)
@@ -89,27 +78,27 @@ int main(void)
         return 1;
     }
 
-    char (*arr)[21] = malloc(N * sizeof(char[21])); // 최소 2, 최대 20
-    for (int i = 0; i < N; i++)
+    arr = malloc((N + 1) * sizeof(char[21])); // 최소 2, 최대 20
+    for (int i = 1; i <= N; i++)
     {
         if (scanf("%20s", arr[i]) != 1)
         {
             free(arr);
             return 1;
         }
-        add_map(arr[i], i + 1);
+        add_map(arr[i], i);
     }
+    char input[21];
     for (int i = 0; i < M; i++)
     {
-        char input[21];
-        if (scanf("%s", input) != 1)
+        if (scanf("%20s", input) != 1)
         {
             free(arr);
             return 1;
         }
         if (is_number(input)) // 번호를 받은 경우
         {
-            int index = atoi(input) - 1;
+            int index = atoi(input);
             printf("%s\n", arr[index]);
         }
         else // 이름을 받은 경우
