175bb878d4e267c8f900b52a445fdb1bc177eafc
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2026-03-04 14:27:50 +0900

    [Silver I] Title: 접두사, Time: 68 ms, Memory: 11632 KB -BaekjoonHub

--- Java/백준/Silver/1141. 접두사/접두사.java
+++ Java/백준/Silver/1141. 접두사/접두사.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 /**
- * @description 정렬 + Trie
+ * @description 정렬
  * @performance 메모리: ? KB, 동작시간: ? ms
  * @author python98
  */
@@ -10,28 +10,6 @@ public class Main {
 	static int N, result;
 	static String[] words;
 
-	static class Trie{
-		Trie[] next;
-		void init(){
-			next = new Trie[26];
-		}
-		boolean add(String s){ // 새로운 단어라면 true 반환
-			Trie[] cur = this.next;
-			boolean flag = false;
-			for(char c : s.toCharArray()){
-				if(cur[c-'a'] == null) {
-					cur[c-'a'] = new Trie();
-					cur[c-'a'].init();
-					flag = true;
-					cur = cur[c-'a'].next;
-				}
-				else{
-					cur = cur[c-'a'].next;
-				}
-			}
-			return flag;
-		}
-	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -42,16 +20,14 @@ public class Main {
 			words[i] = br.readLine();
 		}
 
-		Trie trie = new Trie();
-		trie.init();
-
-		Arrays.sort(words,(a,b)-> b.length()-a.length());
-
-		for(String s : words){
-			if(!trie.add(s)){
+		Arrays.sort(words);
+		int result = N;
+		for(int i = 1 ; i < N ; i++){
+			if(words[i].startsWith(words[i-1])){
 				result--;
 			}
 		}
+
 		System.out.print(result);
 	}
 }
