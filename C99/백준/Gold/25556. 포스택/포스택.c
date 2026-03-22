cefbc10aef3072f8d40fe7f67b5c3000d70577aa
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2026-03-22 14:06:14 +0900

    [Gold V] Title: 포스택, Time: 12 ms, Memory: 1116 KB -BaekjoonHub

--- C99/백준/Gold/25556. 포스택/포스택.c
+++ C99/백준/Gold/25556. 포스택/포스택.c
@@ -5,16 +5,15 @@
 #include <stdint.h>
 
 /**
- * @description ?
- * @performance 메모리: 1,112 KB, 동작시간: 0 ms
+ * @description Greedy + Stack
+ * @performance 메모리: 1,116 KB, 동작시간: 12 ms
  * @author java08
  */
-// 스택에서 꺼낼 때 오름차순을 유지하려면 각 스택에 들어가는 수의 크기가 증가하는 방향으로 들어가야 함
-// 들어가는 값이 작아지면 다른 스택에 넣어야 함
-// 즉, 5번 연속 작아진다면 불가능함?
+
 int main(void)
 {
-    int N, s1 = 0, s2 = 0, s3 = 0, s4 = 0;
+    int N;
+    int s[4] = {0}, d[4] = {0};
     if (scanf("%d", &N) != 1)
         return 1;
     int cur = 0;
@@ -25,30 +24,18 @@ int main(void)
             return 1;
         if (flag)
             continue;
-        int d1 = cur - s1;
-        int d2 = cur - s2;
-        int d3 = cur - s3;
-        int d4 = cur - s4;
+        for (int j = 0; j < 4; j++)
+        {
+            d[j] = cur - s[j];
+        }
         int minS = 0, minD = 100001;
-        if (minD > d1 && d1 > 0)
+        for (int j = 0; j < 4; j++)
         {
-            minD = d1;
-            minS = 1;
-        }
-        if (minD > d2 && d2 > 0)
-        {
-            minD = d2;
-            minS = 2;
-        }
-        if (minD > d3 && d3 > 0)
-        {
-            minD = d3;
-            minS = 3;
-        }
-        if (minD > d4 && d4 > 0)
-        {
-            minD = d4;
-            minS = 4;
+            if (minD > d[j] && d[j] > 0)
+            {
+                minD = d[j];
+                minS = j;
+            }
         }
         if (minD == 100001)
         {
@@ -56,26 +43,7 @@ int main(void)
         }
         else
         {
-            if (minS == 1)
-            {
-                s1 = cur;
-            }
-            else if (minS == 2)
-            {
-                s2 = cur;
-            }
-            else if (minS == 3)
-            {
-                s3 = cur;
-            }
-            else if (minS == 4)
-            {
-                s4 = cur;
-            }
-            else
-            {
-                return 2;
-            }
+            s[minS] = cur;
         }
     }
     if (flag)
