f0938aee354504fe3f83555ce7f61a6f230084a3
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2026-03-29 20:53:16 +0900

    [Gold II] Title: 통나무 옮기기, Time: 68 ms, Memory: 11624 KB -BaekjoonHub

--- Java/백준/Gold/1938. 통나무 옮기기/통나무 옮기기.java
+++ Java/백준/Gold/1938. 통나무 옮기기/통나무 옮기기.java
@@ -2,123 +2,116 @@ import java.io.*;
 import java.util.*;
 
 /**
- * @description BFS + 시뮬레이션
- * @performance 메모리: ? KB, 동작시간: ? ms
+ * @description BFS + 시뮬레이션 (최적화)
+ * @performance 메모리: 12,328 KB, 동작시간: 76 ms
  * @author python98
  */
 public class Main {
 
-	private static final int[][] DIR = {{-1,0},{1,0},{0,-1},{0,1}}; // 위 아래 좌 우
-	private static final int[][][] HORI_VERT = {{{0,-1},{0,0},{0,1}},{{-1,0},{0,0},{1,0}}};
 	private static int N;
 	private static long[] map; // 64bit N개로 비트마스킹
 	private static long[][] dp; // 0:가로 1:세로
-	private static int[] B, E;
 
-	static class Pos{
-		int r, c, d;
-		Pos(int r, int c, int d){
-			this.r = r;
-			this.c = c;
-			this.d = d;
-		}
-	}
-
-	private static boolean isPossible(Pos cur, int idx){
-		if(idx==4) { // 회전
-			if(cur.r-1<0||cur.r+1>=N||cur.c-1<0||cur.c+1>=N) return false;
-			for(int i = cur.r-1 ; i <= cur.r+1 ; i++) {
-				for(int j = cur.c-1 ; j <= cur.c+1 ; j++) {
-					if(((map[i]>>j)&1)==1) return false;
-				}
-			}
-			return true;
-		}else {
-			for(int i = 0 ; i < 3 ; i++){
-				int nr = cur.r + HORI_VERT[cur.d][i][0] + DIR[idx][0]; // 막대 3개 중 위치 + 이동 방향
-				int nc = cur.c + HORI_VERT[cur.d][i][1] + DIR[idx][1];
-				if(nr<0||nc<0||nr>=N||nc>=N) return false;
-				if(((map[nr]>>nc)&1)==1) return false;
-			}
-			return true;
-		}
-	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		N = Integer.parseInt(br.readLine());
 		map = new long[N];
 		dp = new long[2][N];
 
-		int[][] b = new int[3][2];
-		int[][] e = new int[3][2];
+		int[] B = new int[3];
+        int[] E = new int[3];
+        int[][] bPos = new int[3][2];
+        int[][] ePos = new int[3][2];
 		int bCnt = 0, eCnt = 0;
+
 		for(int i = 0 ; i < N ; i++) {
 			char[] input = br.readLine().toCharArray();
 			for(int j = 0 ; j < N ; j++) {
-				long bit = 0l;
 				if(input[j]=='B') {
-					b[bCnt][0] = i;
-					b[bCnt][1] = j;
-					bCnt++;
+					bPos[bCnt][0] = i;
+					bPos[bCnt++][1] = j;
 				}else if(input[j]=='E') {
-					e[eCnt][0] = i;
-					e[eCnt][1] = j;
-					eCnt++;
-				}else {
-					bit = input[j]-'0';
+					ePos[eCnt][0] = i;
+					ePos[eCnt++][1] = j;
+				}else if(input[j] == '1'){
+					map[i]|=(1L<<j);
 				}
-				map[i]|=(bit<<j);
 			}
 		}
-		B = new int[3];
-		B[0] = b[1][0];
-		B[1] = b[1][1];
-		B[2] = b[0][0]==b[1][0] ? 0:1;
-		E = new int[3];
-		E[0] = e[1][0];
-		E[1] = e[1][1];
-		E[2] = e[0][0]==e[1][0] ? 0:1;
+		B[0] = bPos[1][0];
+		B[1] = bPos[1][1];
+		B[2] = bPos[0][0]==bPos[1][0] ? 0:1;
+		E[0] = ePos[1][0];
+		E[1] = ePos[1][1];
+		E[2] = ePos[0][0]==ePos[1][0] ? 0:1;
 
-		Pos[] q = new Pos[10000];
+		if (B[0] == E[0] && B[1] == E[1] && B[2] == E[2]) {
+            System.out.print(0);
+            return;
+        }
+
+		int[] q = new int[10000]; // 50은 6bit 이내, 0:방향, 1~7:c, 8~14:r 
 		int front = 0, rear = 0;
-		q[rear++] = new Pos(B[0], B[1], B[2]);
-		dp[B[2]][B[0]]|=(1l<<B[1]);
-		boolean flag = false;
+
+		q[rear++] = (B[0]<<8) | (B[1]<<1) | B[2];
+		dp[B[2]][B[0]] |=(1L << B[1]);
+
+		int[] dr = {-1, 1, 0, 0};
+		int[] dc = {0, 0, -1, 1};
 		int result = 0;
+
 		while(front<rear){
 			int cnt = rear-front;
-			while(cnt-->0){
-				Pos cur = q[front++];
-				if(cur.r==E[0]&&cur.c==E[1]&&cur.d==E[2]) {
-					flag = true;
-					break;
-				}
-				for(int i = 0 ; i < 5 ; i++) {
-					if(isPossible(cur,i)){
-						int nextR, nextC, nextD;
-						if(i==4) {
-							nextR = cur.r;
-							nextC = cur.c;
-							nextD = cur.d==0?1:0;
-						} else {
-							nextR = cur.r+DIR[i][0];
-							nextC = cur.c+DIR[i][1];
-							nextD = cur.d;
-						}
-						if(((dp[nextD][nextR]>>nextC)&1)==0) {
-							dp[nextD][nextR]|=(1l<<nextC);
-							q[rear++] = new Pos(nextR, nextC, nextD);
-						}
-					}
-				}
-			}
-			if(flag){
-				break;
-			}
 			result++;
+
+			while(cnt-- > 0){
+				int cur = q[front++];
+				int r = cur >> 8;
+				int c = (cur >> 1) & 0x7F;
+				int d = cur & 1;
+
+				// 상하좌우 이동
+				for (int i = 0; i < 4; i++) {
+                    int nr = r + dr[i];
+                    int nc = c + dc[i];
+
+                    if (d == 0) { // 가로
+                        if (nr < 0 || nr >= N || nc - 1 < 0 || nc + 1 >= N) continue;
+                        if ((map[nr] & (7L << (nc - 1))) != 0L) continue;
+                    } else { // 세로
+                        if (nr - 1 < 0 || nr + 1 >= N || nc < 0 || nc >= N) continue;
+                        long mask = 1L << nc;
+                        if ((map[nr - 1] & mask) != 0L || (map[nr] & mask) != 0L || (map[nr + 1] & mask) != 0L) continue;
+                    }
+					// 방문 체크
+                    if (((dp[d][nr] >> nc) & 1L) == 0L) {
+                        if (nr == E[0] && nc == E[1] && d == E[2]) {
+                            System.out.print(result);
+                            return;
+                        }
+                        dp[d][nr] |= (1L << nc);
+                        q[rear++] = (nr << 8) | (nc << 1) | d;
+                    }
+                }
+
+				// 회전
+                if (r - 1 >= 0 && r + 1 < N && c - 1 >= 0 && c + 1 < N) {
+                    long mask = 7L << (c - 1);
+                    if ((map[r - 1] & mask) == 0L && (map[r] & mask) == 0L && (map[r + 1] & mask) == 0L) {
+                        int nd = d ^ 1;
+						// 방문 체크
+                        if (((dp[nd][r] >> c) & 1L) == 0L) {
+                            if (r == E[0] && c == E[1] && nd == E[2]) {
+                                System.out.print(result);
+                                return;
+                            }
+                            dp[nd][r] |= (1L << c);
+                            q[rear++] = (r << 8) | (c << 1) | nd;
+                        }
+                    }
+                }
+			}
 		}
-		if(!flag) result = 0;
-		System.out.print(result);
+		System.out.print(0);
 	}
 }
