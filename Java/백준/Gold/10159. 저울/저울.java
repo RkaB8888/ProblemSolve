9207fcf443d9e2f1675699ca442e3142af7e896c
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2026-04-15 08:35:23 +0900

    [Gold IV] Title: 저울, Time: 76 ms, Memory: 11828 KB -BaekjoonHub

--- Java/백준/Gold/10159. 저울/저울.java
+++ Java/백준/Gold/10159. 저울/저울.java
@@ -3,12 +3,13 @@ import java.util.*;
 
 /**
  * @author python98
- * @description 그래프 관계 전파 + 인접 행렬 + 원형 Queue
- * @performance 메모리: 12,432 KB, 동작시간: 76 ms
+ * @description 전이 관계 전파 + BitSet 행렬 + row/col 카운트 + 원형 Queue
+ * @performance 메모리: 12,192 KB, 동작시간: 72 ms
  */
 public class Main {
-    static int N, M;
-    static int[][] left;
+    static int N, M, digit;
+    static int[] indeg, link, next, v, rowCnt, colCnt;
+    static BitSet[] left;
 
     private static int nextInt() throws IOException {
         int n = 0, s = 1, c;
@@ -24,21 +25,40 @@ public class Main {
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         N = nextInt();
+        digit = 1;
+        while (digit < N) digit <<= 1;
         M = nextInt();
-        left = new int[N + 1][N + 1];
+        indeg = new int[N];
+        left = new BitSet[N];
+        for (int i = 0; i < N; i++) left[i] = new BitSet(N);
+        rowCnt = new int[N];
+        colCnt = new int[N];
+
+        // 진출 차수를 찾기 위한 인접 리스트
+        link = new int[N];
+        Arrays.fill(link, -1);
+        next = new int[M];
+        v = new int[M];
+
         for (int i = 0; i < M; i++) {
-            int l = nextInt();
-            int r = nextInt();
-            if (left[r][l] == 0) {
-                left[r][l] = 1;
-                left[r][0]++;
+            int l = nextInt() - 1;
+            int r = nextInt() - 1;
+            if (!left[r].get(l)) {
+                left[r].set(l);
+                rowCnt[r]++;
+                colCnt[l]++;
+
+                next[i] = link[l];
+                link[l] = i;
+                v[i] = r;
+
+                indeg[r]++;
             }
         }
-        int digit = 1 << 7;
         int[] q = new int[digit--];
         int b = 0, t = 0;
-        for (int i = 1; i <= N; i++) {
-            if (left[i][0] == 0) {
+        for (int i = 0; i < N; i++) {
+            if (indeg[i] == 0) {
                 q[t++] = i;
                 t &= digit;
             }
@@ -46,26 +66,29 @@ public class Main {
         while (b != t) {
             int cur = q[b++];
             b &= digit;
-            for (int i = 1; i <= N; i++) {
-                if (left[i][cur] > 0) {
-                    for (int j = 1; j <= N; j++) {
-                        left[i][j] |= left[cur][j];
-                    }
-                    left[i][0]--;
-                    if (left[i][0] == 0) {
-                        q[t++] = i;
-                        t &= digit;
-                    }
+            BitSet rowC = left[cur];
+            for (int e = link[cur]; e >= 0; e = next[e]) {
+                int i = v[e];
+                BitSet rowI = left[i];
+                BitSet delta = (BitSet) rowC.clone();
+                delta.andNot(rowI); // rowC에서 rowI가 1인 부분을 지운다.
+                if (!delta.isEmpty()) {
+                    rowI.or(rowC);
+                    int add = delta.cardinality();
+                    rowCnt[i] += add;
+                }
+                for (int j = delta.nextSetBit(0); j >= 0; j = delta.nextSetBit(j + 1)) {
+                    colCnt[j]++;
+                }
+                if (--indeg[i] == 0) {
+                    q[t++] = i;
+                    t &= digit;
                 }
             }
         }
 
-        for (int i = 1; i <= N; i++) {
-            int cnt = 0;
-            for (int j = 1; j <= N; j++) {
-                if (left[i][j] > 0 || left[j][i] > 0) cnt++;
-            }
-            sb.append(N - cnt - 1).append('\n');
+        for (int i = 0; i < N; i++) {
+            sb.append(N - colCnt[i] - rowCnt[i] - 1).append('\n');
         }
         System.out.print(sb);
     }
