a310e5b3179e604d46ff51f522350f7036c90d96
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2026-05-07 18:39:13 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.44 ms, Memory: 75.6 MB -BaekjoonHub

--- Java/프로그래머스/3/17678. ［1차］ 셔틀버스/README.md
+++ Java/프로그래머스/3/17678. ［1차］ 셔틀버스/README.md
@@ -1,10 +1,10 @@
 # [level 3] [1차] 셔틀버스 - 17678 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17678) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17678?language=java) 
 
 ### 성능 요약
 
-메모리: 78.2 MB, 시간: 2.85 ms
+메모리: 75.6 MB, 시간: 1.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 07일 18:17:12
+2026년 05월 07일 18:39:07
 
 ### 문제 설명
 
--- Java/프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ Java/프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 /**
- * @description 이 클래스에 대한 동작 설명
+ * @description 그리디
  */
  
 // n: 셔틀 운행 횟수 0 초과 10 이하
@@ -13,51 +13,43 @@ import java.util.*;
 // 최대한 늦게 출근하기 위한 도착 시간 출력
 // 즉, 마지막 셔틀에 탑승할 수 있는 마지막 시간을 구한다.
 class Solution {
-    String[] TIME;
-    private void makeTimeArray(int n, int t){
-        this.TIME = new String[n];
-        int HH = 9;
-        int MM = 0;
-        
-        for(int i = 0 ; i < n ; i++) {
-            StringBuilder sb = new StringBuilder();
-            TIME[i] = sb.append(HH/10).append(HH%10).append(':').append(MM/10).append(MM%10).toString();
-            MM+=t;
-            while(MM>59) {
-                MM-=60;
-                HH++;
-            }
-        }
-    }
-    private String isSmall(String s) {
-        char[] tmp = s.toCharArray();
-        int m = 0;
-        m+=(tmp[0]-'0')*600;
-        m+=(tmp[1]-'0')*60;
-        m+=(tmp[3]-'0')*10;
-        m+=(tmp[4]-'0')-1;
-        return String.format("%02d:%02d",m/60,m%60);
-    }
-    public String solution(int n, int t, int m, String[] timetable) {
-        makeTimeArray(n,t);
-        Arrays.sort(timetable);
 
-        int startIdx = 0, endIdx = 0;
-        for(int i = 0 ; i < n ; i++) {
-            startIdx = endIdx;
-            String curTime = TIME[i];
-            for(; endIdx-startIdx < m && endIdx < timetable.length && timetable[endIdx].compareTo(curTime)<=0 ; endIdx++);
-        }
-        if(endIdx - startIdx < m) return TIME[n-1]; // 막차는 인원 부족
-        endIdx--;
-        return isSmall(timetable[endIdx]);
+    private int parseTime(String time) {
+        return (time.charAt(0) - '0') * 600 +
+               (time.charAt(1) - '0') * 60 +
+               (time.charAt(3) - '0') * 10 +
+               (time.charAt(4) - '0');
     }
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int n = 2;
-        int t = 10;
-        int m = 2;
-        String[] timetable = {"09:10", "09:09", "08:00"};
-        System.out.println(sol.solution(n,t,m,timetable));
+
+    private String formatTime(int time) {
+        return String.format("%02d:%02d", time / 60, time % 60);
+    }
+
+    public String solution(int n, int t, int m, String[] timetable) {
+        
+        int[] times = new int[timetable.length];
+        for(int i = 0 ; i < timetable.length ; i++) {
+            times[i] = parseTime(timetable[i]);
+        }
+        Arrays.sort(times);
+
+        int busTime = parseTime("09:00");
+        int timeIdx = 0;
+        int endTime = 0;
+
+        for(int i = 0 ; i < n ; i++) {
+            int cnt = 0;
+            while(cnt < m && timeIdx < times.length && times[timeIdx] <= busTime) {
+                endTime = times[timeIdx];
+                timeIdx++;
+                cnt++;
+            }
+            if(i==n-1) {
+                if(cnt==m) return formatTime(endTime-1);
+                else return formatTime(busTime);
+            }
+            busTime+=t;
+        }
+        return "";
     }
 }
