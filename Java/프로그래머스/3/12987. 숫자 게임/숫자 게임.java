d7af1aa83ca068ba78066c96e5c162729ecf0f30
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2026-06-10 21:42:51 +0900

    [level 3] Title: 숫자 게임, Time: 37.06 ms, Memory: 67.7 MB -BaekjoonHub

--- Java/프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ Java/프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -2,36 +2,33 @@ import java.io.*;
 import java.util.*;
 
 /**
- * @description 정렬 그리디
+ * @description 정렬 투포인터
  */
  
 // A와 B를 합치고 정렬한 후 (O(2Nlog2N))
 // 작은 것부터 A와 B를 카운팅 하며 B가 나왔을 때 A가 더 많으면 +1
 // AABABBAABAABBABABB
 class Solution {
-
     public int solution(int[] A, int[] B) {
-        int N = A.length;
-        int[] total = new int[N*2];
         int answer = 0;
 
-        for(int i = 0 ; i < N ; i++) {
-            total[i*2] = (A[i]<<1)|1;
-            total[i*2+1] = (B[i]<<1)|0;
-        }
-        Arrays.sort(total);
-
-        int aCnt = 0;
-        for(int i = 0 ; i < total.length ; i++){
-            if((total[i]&1)==1) {
-                aCnt++;
-            }else {
-                if(aCnt>0) {
-                    answer++;
-                    aCnt--;
-                }
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int aIdx = 0;
+        int bIdx = 0;
+        
+        while (bIdx < B.length) {
+            if (B[bIdx] > A[aIdx]) {
+                answer++;
+                aIdx++;
+                bIdx++;
+            } 
+            else {
+                bIdx++;
             }
         }
+        
         return answer;
     }
 }
