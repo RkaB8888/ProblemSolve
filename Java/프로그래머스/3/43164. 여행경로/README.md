55bc049c7c2de9814b1116e023d5f147cfc79d4b
RkaB8888 <57251314+RkaB8888@users.noreply.github.com>
2026-08-05 07:36:26 +0900

    [level 3] Title: 여행경로, Time: 0.25 ms, Memory: 83.8 MB -BaekjoonHub

--- Java/프로그래머스/3/43164. 여행경로/README.md
+++ Java/프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=java) 
 
 ### 성능 요약
 
-메모리: 73.8 MB, 시간: 0.80 ms
+메모리: 83.8 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 05일 05:18:08
+2026년 08월 05일 07:36:20
 
 ### 문제 설명
 
--- Java/프로그래머스/3/43164. 여행경로/여행경로.java
+++ Java/프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,89 +2,35 @@ import java.io.*;
 import java.util.*;
 
 /**
- * @description 정렬 + DFS + HashMap
+ * @description 한붓 그리기
  */
  
-// 모든 간선을 사용해서 거쳐가는 노드 반환
+// Hierholzer 알고리즘 활용
 class Solution {
-    int n;
-    int[] link, next, startNode, endNode;
-    boolean[] used;
-    int[] stack;
 
-    private boolean dfs(int nodeIdx, int step){
-        if(step == n) return true;
-
-        for(int edge = link[nodeIdx] ; edge != -1 ; edge = next[edge]) {
-            if(!used[edge]){
-                used[edge] = true;
-                stack[step] = edge;
-                if(dfs(endNode[edge], step+1)) {
-                    return true;
-                }
-                used[edge] = false;
-            }
+    private void dfs(String start, Map<String, List<String>> graph, Deque<String> route){
+        List<String> end = graph.get(start);
+        while(end != null && !end.isEmpty()) {
+            String next = end.remove(end.size()-1);
+            dfs(next, graph, route);
         }
-        return false;
+        route.addFirst(start);
     }
     public String[] solution(String[][] tickets) {
-        // 티켓 이름 오름차순 정렬
-        Arrays.sort(tickets,(a,b)->{
-            if(a[0].compareTo(b[0])==0) {
-                return a[1].compareTo(b[1]);
-            }
-            return a[0].compareTo(b[0]);
-        });
-
-        n = tickets.length;
-        link = new int[n+1]; // 공항의 갯수는 n+1이내임
-        Arrays.fill(link,-1);
-        next = new int[n];
-        startNode = new int[n];
-        endNode = new int[n]; // 도착 공항의 갯수는 n이내임
-
-        Map<String,Integer> index = new HashMap<>();
-        String[] name = new String[n+1]; // 티켓이 n개라면 지역은 최대 n+1개
-
-        int cnt = 0;
-        // 각 공항의 인덱스 저장
-        for(int i = 0 ; i < n ; i++) {
-            String from = tickets[i][0];
-            String to = tickets[i][1];
-
-            if(!index.containsKey(from)) {
-                index.put(from,cnt);
-                name[cnt] = from;
-                cnt++;
-            }
-            if(!index.containsKey(to)) {
-                index.put(to,cnt);
-                name[cnt] = to;
-                cnt++;
-            }
+        // 공항과 연결된 다음 공항들 저장
+        Map<String, List<String>> graph = new HashMap<>();
+        for(String[] ticket : tickets) {
+            graph.putIfAbsent(ticket[0],new ArrayList<>());
+            graph.get(ticket[0]).add(ticket[1]);
         }
 
-        for(int i = n-1 ; i >= 0 ; i--) { // 인접 리스트 역순으로 연결
-            int fromIdx = index.get(tickets[i][0]);
-            int toIdx = index.get(tickets[i][1]);
-
-            next[i] = link[fromIdx];
-            link[fromIdx] = i;
-            startNode[i] = fromIdx;
-            endNode[i] = toIdx;
+        for(List<String> list : graph.values()) {
+            list.sort(Collections.reverseOrder());
         }
 
-        used = new boolean[n];
-        stack = new int[n];
+        Deque<String> route = new ArrayDeque<>();
+        dfs("ICN", graph, route);
 
-        dfs(index.get("ICN"), 0);
-
-        String[] answer = new String[n+1];
-        answer[0] = "ICN";
-        for(int i = 1 ; i <= n ; i++) {
-            answer[i] = name[endNode[stack[i-1]]];
-        }
-
-        return answer;
+        return route.toArray(new String[0]);
     }
 }
